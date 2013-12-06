791692a8664a768c5001561bb3327a28a500ff95
Anubhav Gupta <anubhavg@codeaurora.org>
2013-12-06 17:11:11 +0530

    Restart sniff timer on AVDTP signalling channel establishment.
    VolksWagan UHV Preminum takes time to initiate AVDTP media channel
    after AVDTP signalling channel is up. If mentioned duration exceeds
    7 seconds then ACL link goes into sniff due to which A2dp Connection
    fails.
    
    With this change we make sure that when AVDTP signalling channel
    is up and media channel is not connected we do not go to sniff,
    which is achieved by restarting sniff timer on signalling channel up
    so that link does not go to sniff in between ongoing connection.
    
    Change-Id: Ic3f268703067155ffd55a43f320ce199641c6b3f

--- btif/src/btif_dm.c
+++ btif/src/btif_dm.c
@@ -214,6 +214,7 @@ extern bt_status_t btif_sdp_execute_service(BOOLEAN b_enable);
 extern int btif_hh_connect(bt_bdaddr_t *bd_addr);
 extern bt_status_t btif_hd_execute_service(BOOLEAN b_enable);
 extern void bta_gatt_convert_uuid16_to_uuid128(UINT8 uuid_128[LEN_UUID_128], UINT16 uuid_16);
+extern void btif_av_move_idle(bt_bdaddr_t bd_addr);
 
 
 /******************************************************************************
@@ -1841,6 +1842,7 @@ static void btif_dm_upstreams_evt(UINT16 event, char* p_param)
         case BTA_DM_LINK_DOWN_EVT:
             bdcpy(bd_addr.address, p_data->link_down.bd_addr);
             btm_set_bond_type_dev(p_data->link_down.bd_addr, BOND_TYPE_UNKNOWN);
+            btif_av_move_idle(bd_addr);
             BTIF_TRACE_DEBUG("BTA_DM_LINK_DOWN_EVT. Sending BT_ACL_STATE_DISCONNECTED");
             HAL_CBACK(bt_hal_cbacks, acl_state_changed_cb, BT_STATUS_SUCCESS,
                       &bd_addr, BT_ACL_STATE_DISCONNECTED);
