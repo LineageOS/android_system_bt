992fc3819961739fd663e1b2849f426e88fbeb40
Anubhav Gupta <anubhavg@codeaurora.org>
2014-12-11 17:56:02 +0530

    Do not initiate SDP for incoming AVDTP connection.
    Do not initiate SDP for incoming AVDTP connection.
    There are few instances when remote initiates L2cap
    channel for AVDTP signaling but keeps quite after that,
    and later on timeout DUT goes for rest of the signalling
    procedure but does sdp prior to the same.Few peer
    devices deletes a2dp sink sdp record when there is an
    ongoing connection, as a result sdp request fails and
    DUT fails to proceed with signalling procedure and
    connection setup fails.
    
    This patch makes sure that DUT does not go for redundant
    sdp request for AVDTP, provided remote has already
    established L2cap connection for the same. SDP is done
    explicitly only incase of outgoing AV connection.
    
    Change-Id: Id44368687d5b3f97bf0258f8d4ff75f185db23af

--- bta/av/bta_av_aact.c
+++ bta/av/bta_av_aact.c
@@ -1014,39 +1014,53 @@ void bta_av_do_disc_a2d (tBTA_AV_SCB *p_scb, tBTA_AV_DATA *p_data)
 
     bta_sys_conn_open(BTA_ID_AV, p_scb->app_id, p_scb->peer_addr);
 
-    /* allocate discovery database */
-    if (p_scb->p_disc_db == NULL)
+    if (p_scb->skip_sdp == TRUE)
     {
-        p_scb->p_disc_db = (tSDP_DISCOVERY_DB *) GKI_getbuf(BTA_AV_DISC_BUF_SIZE);
-    }
-
-    /* only one A2D find service is active at a time */
-    bta_av_cb.handle = p_scb->hndl;
-
-    if(p_scb->p_disc_db)
-    {
-        /* set up parameters */
-        db_params.db_len = BTA_AV_DISC_BUF_SIZE;
-        db_params.num_attr = 3;
-        db_params.p_db = p_scb->p_disc_db;
-        db_params.p_attrs = attr_list;
+        tA2D_Service a2d_ser;
+        a2d_ser.avdt_version = AVDT_VERSION;
+        p_scb->skip_sdp = FALSE;
         p_scb->uuid_int = p_data->api_open.uuid;
-        if (p_scb->uuid_int == UUID_SERVCLASS_AUDIO_SINK)
-            sdp_uuid = UUID_SERVCLASS_AUDIO_SOURCE;
-        else if (p_scb->uuid_int == UUID_SERVCLASS_AUDIO_SOURCE)
-            sdp_uuid = UUID_SERVCLASS_AUDIO_SINK;
-
-        APPL_TRACE_DEBUG("uuid_int 0x%x, Doing SDP For 0x%x", p_scb->uuid_int, sdp_uuid);
-        if(A2D_FindService(sdp_uuid, p_scb->peer_addr, &db_params,
-                        bta_av_a2d_sdp_cback) == A2D_SUCCESS)
-        {
-            return;
-        }
+        /* only one A2D find service is active at a time */
+        bta_av_cb.handle = p_scb->hndl;
+        APPL_TRACE_DEBUG("Skip Sdp for incoming A2dp connection");
+        bta_av_a2d_sdp_cback(TRUE, &a2d_ser);
     }
+    else
+    {
+        /* allocate discovery database */
+        if (p_scb->p_disc_db == NULL)
+        {
+            p_scb->p_disc_db = (tSDP_DISCOVERY_DB *) GKI_getbuf(BTA_AV_DISC_BUF_SIZE);
+        }
 
-    /* when the code reaches here, either the DB is NULL
-     * or A2D_FindService is not successful */
-    bta_av_a2d_sdp_cback(FALSE, NULL);
+        /* only one A2D find service is active at a time */
+        bta_av_cb.handle = p_scb->hndl;
+
+        if(p_scb->p_disc_db)
+        {
+            /* set up parameters */
+            db_params.db_len = BTA_AV_DISC_BUF_SIZE;
+            db_params.num_attr = 3;
+            db_params.p_db = p_scb->p_disc_db;
+            db_params.p_attrs = attr_list;
+            p_scb->uuid_int = p_data->api_open.uuid;
+            if (p_scb->uuid_int == UUID_SERVCLASS_AUDIO_SINK)
+                sdp_uuid = UUID_SERVCLASS_AUDIO_SOURCE;
+            else if (p_scb->uuid_int == UUID_SERVCLASS_AUDIO_SOURCE)
+                sdp_uuid = UUID_SERVCLASS_AUDIO_SINK;
+
+            APPL_TRACE_DEBUG("uuid_int 0x%x, Doing SDP For 0x%x", p_scb->uuid_int, sdp_uuid);
+            if(A2D_FindService(sdp_uuid, p_scb->peer_addr, &db_params,
+                            bta_av_a2d_sdp_cback) == A2D_SUCCESS)
+            {
+                return;
+            }
+        }
+
+        /* when the code reaches here, either the DB is NULL
+         * or A2D_FindService is not successful */
+        bta_av_a2d_sdp_cback(FALSE, NULL);
+    }
 }
 
 /*******************************************************************************
@@ -1083,6 +1097,7 @@ void bta_av_cleanup(tBTA_AV_SCB *p_scb, tBTA_AV_DATA *p_data)
     p_scb->cur_psc_mask = 0;
     p_scb->wait = 0;
     p_scb->num_disc_snks = 0;
+    p_scb->skip_sdp = FALSE;
     bta_sys_stop_timer(&p_scb->timer);
     if (p_scb->deregistring)
     {
@@ -3094,6 +3109,12 @@ void bta_av_open_at_inc (tBTA_AV_SCB *p_scb, tBTA_AV_DATA *p_data)
 {
     tBTA_AV_API_OPEN  *p_buf;
 
+    if (!p_scb)
+    {
+        APPL_TRACE_WARNING("scb is NULL, bailing out!");
+        return;
+    }
+
     memcpy (&(p_scb->open_api), &(p_data->api_open), sizeof(tBTA_AV_API_OPEN));
 
     if (p_scb->coll_mask & BTA_AV_COLL_INC_TMR)
@@ -3113,6 +3134,7 @@ void bta_av_open_at_inc (tBTA_AV_SCB *p_scb, tBTA_AV_DATA *p_data)
         if ((p_buf = (tBTA_AV_API_OPEN *) GKI_getbuf(sizeof(tBTA_AV_API_OPEN))) != NULL)
         {
             memcpy(p_buf, &(p_scb->open_api), sizeof(tBTA_AV_API_OPEN));
+            p_scb->skip_sdp = TRUE;
             bta_sys_sendmsg(p_buf);
         }
     }
