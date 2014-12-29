5c7d0331cf87893bc8891e5fd3f197dbb8ef76d1
repo Shareef Ali <shareefalis@gmail.com>
2014-12-29 00:48:01 -0600

    shut a2dp logspam.
    Change-Id: Id67b672e50f5cfc900622235497150c86f8a4938

--- audio_a2dp_hw/audio_a2dp_hw.c
+++ audio_a2dp_hw/audio_a2dp_hw.c
@@ -196,7 +196,7 @@ static void ts_error_log(char *tag, int val, int buff_size, struct a2dp_config c
     prev = now;
     if(diff_us > (calc_audiotime (cfg, buff_size) + 10000L))
     {
-       ERROR("[%s] ts %08lld, diff %08lld, val %d %d", tag, now_us, diff_us, val, buff_size);
+       DEBUG("[%s] ts %08lld, diff %08lld, val %d %d", tag, now_us, diff_us, val, buff_size);
     }
 }
 
