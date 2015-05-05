e9a660866e28f9b0c3ec34239fc992865d102970
Anubhav Gupta <anubhavg@codeaurora.org>
2015-05-05 17:48:24 +0530

    Enable AVRCP TG feature only when A2dp source is enabled
      - Drop Control, Meta and Browse commands when TG is not up.
      - Not to call AVRCP app callbacks when mentioned service is down.
      - Advertise AVRC CT category 2 support in CT SDP record.
    
    Change-Id: Ib54c104e87c735b55d6c6ec07c08d2149acc1a6d

--- btif/src/btif_rc.c
+++ btif/src/btif_rc.c
@@ -349,12 +349,14 @@ void close_uinput (void)
 
 void handle_rc_features()
 {
+    if (bt_rc_callbacks != NULL)
+    {
     btrc_remote_features_t rc_features = BTRC_FEAT_NONE;
     bt_bdaddr_t rc_addr;
     bt_bdaddr_t avdtp_addr;
     bdstr_t addr1, addr2;
 
-    bdcpy(rc_addr.address, btif_rc_cb.rc_addr);
+        bdcpy(rc_addr.address, btif_rc_cb.rc_addr);
     avdtp_addr = btif_av_get_addr();
 
     BTIF_TRACE_DEBUG("AVDTP Address : %s AVCTP address: %s",
@@ -363,51 +365,51 @@ void handle_rc_features()
 
     if (dev_blacklisted_for_absolute_volume(btif_rc_cb.rc_addr) ||
         bdcmp(avdtp_addr.address, rc_addr.address))
-    {
-        btif_rc_cb.rc_features &= ~BTA_AV_FEAT_ADV_CTRL;
-    }
+        {
+            btif_rc_cb.rc_features &= ~BTA_AV_FEAT_ADV_CTRL;
+        }
 
-    if (btif_rc_cb.rc_features & BTA_AV_FEAT_BROWSE)
-    {
-        rc_features |= BTRC_FEAT_BROWSE;
-    }
-    if ( (btif_rc_cb.rc_features & BTA_AV_FEAT_ADV_CTRL) &&
-         (btif_rc_cb.rc_features & BTA_AV_FEAT_RCTG))
-    {
-        rc_features |= BTRC_FEAT_ABSOLUTE_VOLUME;
-    }
-    if (btif_rc_cb.rc_features & BTA_AV_FEAT_METADATA)
-    {
-        rc_features |= BTRC_FEAT_METADATA;
-    }
-    BTIF_TRACE_DEBUG("%s: rc_features=0x%x", __FUNCTION__, rc_features);
-    HAL_CBACK(bt_rc_callbacks, remote_features_cb, &rc_addr, rc_features)
+        if (btif_rc_cb.rc_features & BTA_AV_FEAT_BROWSE)
+        {
+            rc_features |= BTRC_FEAT_BROWSE;
+        }
+        if ( (btif_rc_cb.rc_features & BTA_AV_FEAT_ADV_CTRL) &&
+               (btif_rc_cb.rc_features & BTA_AV_FEAT_RCTG))
+        {
+           rc_features |= BTRC_FEAT_ABSOLUTE_VOLUME;
+        }
+        if (btif_rc_cb.rc_features & BTA_AV_FEAT_METADATA)
+        {
+            rc_features |= BTRC_FEAT_METADATA;
+        }
+        BTIF_TRACE_DEBUG("%s: rc_features=0x%x", __FUNCTION__, rc_features);
+        HAL_CBACK(bt_rc_callbacks, remote_features_cb, &rc_addr, rc_features)
 
 #if (AVRC_ADV_CTRL_INCLUDED == TRUE)
-     BTIF_TRACE_DEBUG("Checking for feature flags in btif_rc_handler with label %d",
+         BTIF_TRACE_DEBUG("Checking for feature flags in btif_rc_handler with label %d",
                         btif_rc_cb.rc_vol_label);
-     // Register for volume change on connect
-      if(btif_rc_cb.rc_features & BTA_AV_FEAT_ADV_CTRL &&
-         btif_rc_cb.rc_features & BTA_AV_FEAT_RCTG)
-      {
-         rc_transaction_t *p_transaction=NULL;
-         bt_status_t status = BT_STATUS_NOT_READY;
-         if(MAX_LABEL==btif_rc_cb.rc_vol_label)
+         // Register for volume change on connect
+         if(btif_rc_cb.rc_features & BTA_AV_FEAT_ADV_CTRL &&
+             btif_rc_cb.rc_features & BTA_AV_FEAT_RCTG)
          {
-            status=get_transaction(&p_transaction);
-         }
-         else
-         {
-            p_transaction=get_transaction_by_lbl(btif_rc_cb.rc_vol_label);
-            if(NULL!=p_transaction)
+             rc_transaction_t *p_transaction=NULL;
+             bt_status_t status = BT_STATUS_NOT_READY;
+             if(MAX_LABEL==btif_rc_cb.rc_vol_label)
             {
-               BTIF_TRACE_DEBUG("register_volumechange already in progress for label %d",
-                                  btif_rc_cb.rc_vol_label);
-               return;
+                status=get_transaction(&p_transaction);
             }
             else
+            {
+                p_transaction=get_transaction_by_lbl(btif_rc_cb.rc_vol_label);
+                if(NULL!=p_transaction)
+                {
+                   BTIF_TRACE_DEBUG("register_volumechange already in progress for label %d",
+                                  btif_rc_cb.rc_vol_label);
+                 return;
+                }
+            else
               status=get_transaction(&p_transaction);
-         }
+            }
 
          if(BT_STATUS_SUCCESS == status && NULL!=p_transaction)
          {
@@ -416,6 +418,15 @@ void handle_rc_features()
          }
        }
 #endif
+    }
+    else
+    {
+    /*Disable all TG related bits if AVRCP TG feature is not enabled*/
+        BTIF_TRACE_WARNING("Avrcp TG role not enabled, disabling TG specific featuremask");
+        btif_rc_cb.rc_features &= ~BTA_AV_FEAT_ADV_CTRL;
+        btif_rc_cb.rc_features &= ~BTA_AV_FEAT_BROWSE;
+        btif_rc_cb.rc_features &= ~BTA_AV_FEAT_METADATA;
+    }
 }
 
 
@@ -459,13 +470,17 @@ void handle_rc_connect (tBTA_AV_RC_OPEN *p_rc_open)
         btif_rc_cb.rc_handle = p_rc_open->rc_handle;
 
         /* on locally initiated connection we will get remote features as part of connect */
-        if (btif_rc_cb.rc_features != 0)
-            handle_rc_features();
-
-        result = uinput_driver_check();
-        if(result == BT_STATUS_SUCCESS)
+        if (bt_rc_callbacks)
         {
-            init_uinput();
+            result = uinput_driver_check();
+            if(result == BT_STATUS_SUCCESS)
+            {
+                init_uinput();
+            }
+        }
+        else
+        {
+            BTIF_TRACE_WARNING("Avrcp TG role not enabled, not initializing UInput");
         }
 #if (AVRC_CTLR_INCLUDED == TRUE)
         bdcpy(rc_addr.address, btif_rc_cb.rc_addr);
@@ -518,7 +533,14 @@ void handle_rc_disconnect (tBTA_AV_RC_CLOSE *p_rc_close)
     btif_rc_cb.rc_vol_label=MAX_LABEL;
     btif_rc_cb.rc_volume=MAX_VOLUME;
     init_all_transactions();
-    close_uinput();
+    if (bt_rc_callbacks != NULL)
+    {
+        close_uinput();
+    }
+    else
+    {
+        BTIF_TRACE_WARNING("Avrcp TG role not enabled, not closing UInput");
+    }
 #if (AVRC_CTLR_INCLUDED == TRUE)
     bdcpy(rc_addr.address, btif_rc_cb.rc_addr);
 #endif
@@ -805,9 +827,27 @@ void btif_rc_handler(tBTA_AV_EVT event, tBTA_AV *p_data)
 
         case BTA_AV_REMOTE_CMD_EVT:
         {
-            BTIF_TRACE_DEBUG("rc_id:0x%x key_state:%d", p_data->remote_cmd.rc_id,
-                               p_data->remote_cmd.key_state);
-            handle_rc_passthrough_cmd( (&p_data->remote_cmd) );
+            if (bt_rc_callbacks != NULL)
+            {
+                BTIF_TRACE_DEBUG("rc_id:0x%x key_state:%d",
+                    p_data->remote_cmd.rc_id, p_data->remote_cmd.key_state);
+                /** In race conditions just after 2nd AVRCP is connected
+                 *  remote might send pass through commands, so check for
+                 *  Rc handle before processing pass through commands
+                 **/
+                if (btif_rc_cb.rc_handle == p_data->remote_cmd.rc_handle)
+                {
+                    handle_rc_passthrough_cmd( (&p_data->remote_cmd) );
+                }
+                else
+                {
+                    BTIF_TRACE_DEBUG("Pass-through command for Invalid rc handle");
+                }
+            }
+            else
+            {
+                BTIF_TRACE_ERROR("AVRCP TG role not up, drop passthrough commands");
+            }
         }
         break;
 #if (AVRC_CTLR_INCLUDED == TRUE)
@@ -828,12 +868,23 @@ void btif_rc_handler(tBTA_AV_EVT event, tBTA_AV *p_data)
         break;
         case BTA_AV_META_MSG_EVT:
         {
-            BTIF_TRACE_DEBUG("BTA_AV_META_MSG_EVT  code:%d label:%d", p_data->meta_msg.code,
-                p_data->meta_msg.label);
-            BTIF_TRACE_DEBUG("  company_id:0x%x len:%d handle:%d", p_data->meta_msg.company_id,
-                p_data->meta_msg.len, p_data->meta_msg.rc_handle);
-            /* handle the metamsg command */
-            handle_rc_metamsg_cmd(&(p_data->meta_msg));
+            if (bt_rc_callbacks != NULL)
+            {
+                BTIF_TRACE_DEBUG("BTA_AV_META_MSG_EVT  code:%d label:%d",
+                                                p_data->meta_msg.code,
+                                                p_data->meta_msg.label);
+                BTIF_TRACE_DEBUG("  company_id:0x%x len:%d handle:%d",
+                                            p_data->meta_msg.company_id,
+                                            p_data->meta_msg.len,
+                                            p_data->meta_msg.rc_handle);
+                /* handle the metamsg command */
+                handle_rc_metamsg_cmd(&(p_data->meta_msg));
+                /* Free the Memory allocated for tAVRC_MSG */
+            }
+            else
+            {
+                BTIF_TRACE_ERROR("AVRCP TG role not up, drop meta commands");
+            }
         }
         break;
         default:
