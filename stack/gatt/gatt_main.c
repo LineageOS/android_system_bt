add14031741a784c445f3ad4e40ca6f56a96ecfd
Satish kumar sugasi <ssugas@codeaurora.org>
2015-05-13 18:39:50 -0700

    Bluetooth: update gatt channel state to closing during disconnection
    This changes Gatt state to CLOSING from OPEN to avoid any farther
    gatt operations from that particular fixed channel CID during
    gatt disconnection process.
    
    Change-Id: I8511caa477881de4f4eaf18b84f23530e1958fe9

--- stack/gatt/gatt_main.c
+++ stack/gatt/gatt_main.c
@@ -227,9 +227,9 @@ BOOLEAN gatt_disconnect (tGATT_TCB *p_tcb)
                 }
                 else
                 {
-                    gatt_set_ch_state(p_tcb, GATT_CH_CLOSING);
                     ret = L2CA_CancelBleConnectReq (p_tcb->peer_bda);
                 }
+                gatt_set_ch_state(p_tcb, GATT_CH_CLOSING);
             }
             else
             {
