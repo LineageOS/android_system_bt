fb4bd9cb38319ddf556896945cb06b971ce8a8c9
Nitin Arora <niarora@codeaurora.org>
2015-07-24 12:30:39 -0700

    Bluetooth: Prevent LE connection request after SMp over BR
    This change performs LE specific discovery from LE auth cmpl
    event, only if the SMP has been performed over CID = 6. In
    case of cross key transport, discovery occurs in default way,
    over BR transport because the LE remote might not be available
    to connect back
    
    Change-Id: Ib6e94b77a72f643308ca0c8f60ee1417a7e6caec

--- bta/dm/bta_dm_act.c
+++ bta/dm/bta_dm_act.c
@@ -4579,6 +4579,7 @@ static UINT8 bta_dm_ble_smp_cback (tBTM_LE_EVT event, BD_ADDR bda, tBTM_LE_EVT_D
             else
             {
                 sec_event.auth_cmpl.success = TRUE;
+                sec_event.auth_cmpl.smp_over_br = p_data->complt.smp_over_br;
                 if (!p_data->complt.smp_over_br)
                     GATT_ConfigServiceChangeCCC(bda, TRUE, BT_TRANSPORT_LE);
             }
