59baf535a307dbb716c81909f43eba5bb8b98d7d
Srinu Jella <sjella@codeaurora.org>
2015-08-24 21:00:28 +0530

    Bluetooth: Read BLE vendor capability to proceed on Secure conn
    - Read BLE vendor capability to proceed on enablement of
      secure connection support from Host.
    
    - If BLE vendor capability is supported from the controller
      then secure connection is enabled from Host.
    
    CRs-Fixed: 1041642
    Change-Id: I64e79d4e6fff7d9f1b0ea7272250e8155f5888e2

--- hci/src/hci_layer.cc
+++ hci/src/hci_layer.cc
@@ -648,7 +648,26 @@ static waiting_command_t* get_waiting_command(command_opcode_t opcode) {
 
     return wait_entry;
   }
+  // look for any command complete with improper VS Opcode
+  for (const list_node_t *node = list_begin(commands_pending_response);
+      node != list_end(commands_pending_response);
+      node = list_next(node)) {
+    waiting_command_t *wait_entry =
+        reinterpret_cast<waiting_command_t*> (list_node(node));
 
+    if (wait_entry && (wait_entry->opcode != opcode) &&
+        (((wait_entry->opcode & HCI_GRP_VENDOR_SPECIFIC) == HCI_GRP_VENDOR_SPECIFIC) &&
+        ((opcode & HCI_GRP_VENDOR_SPECIFIC) == HCI_GRP_VENDOR_SPECIFIC))) {
+        LOG_DEBUG(LOG_TAG,"%s VS event found treat it as valid 0x%x", __func__, opcode);
+    }
+    else {
+        continue;
+    }
+
+    list_remove(commands_pending_response, wait_entry);
+
+    return wait_entry;
+  }
   return NULL;
 }
 
