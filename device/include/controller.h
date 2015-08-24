b6fb2784d080e158a53bf427d1c27cc3e3fa2611
Srinu Jella <sjella@codeaurora.org>
2015-08-24 21:00:28 +0530

    Bluetooth: Read BLE vendor capability to proceed on Secure conn
    - Read BLE vendor capability to proceed on enablement of
      secure connection support from Host.
    
    - If BLE vendor capability is supported from the controller
      then secure connection is enabled from Host.
    
    CRs-Fixed: 1041642
    Change-Id: I64e79d4e6fff7d9f1b0ea7272250e8155f5888e2

--- device/include/controller.h
+++ device/include/controller.h
@@ -84,6 +84,7 @@ typedef struct controller_t {
   uint8_t (*get_ble_resolving_list_max_size)(void);
   void (*set_ble_resolving_list_max_size)(int resolving_list_max_size);
   uint8_t* (*get_local_supported_codecs)(uint8_t* number_of_codecs);
+  bool (*supports_ble_offload_features)(void);
   uint8_t (*get_le_all_initiating_phys)(void);
 
 } controller_t;
