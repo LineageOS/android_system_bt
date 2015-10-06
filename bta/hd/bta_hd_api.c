codeaurora-unoffical/platform-external-bluetooth-bluedroid

HID: Improve Logging for Hid Host and Client
This patch adds support to enable logging for hid host's and client's
stack components dynamically. Also some logs for host and cleint
are added under verbose logging level which would help in latency
measurements.

Change-Id: I06de0ea89b8f5e40466519fe60aead6497b6cf11
CRs-Fixed: 608230

bta/hd/bta_hd_api.c
@@ -203,7 +203,7 @@ BTA_API extern void BTA_HdSendReport(tBTA_HD_REPORT *p_report)
 {
     tBTA_HD_SEND_REPORT *p_buf;
 
-    APPL_TRACE_API1("%s", __FUNCTION__);
+    APPL_TRACE_VERBOSE1("%s", __FUNCTION__);
 
     if ((p_buf = (tBTA_HD_SEND_REPORT *) GKI_getbuf(sizeof(tBTA_HD_SEND_REPORT))) != NULL)
     {

btif/co/bta_hh_co.c
@@ -206,7 +206,7 @@ void bta_hh_co_destroy(int fd)
 
 int bta_hh_co_write(int fd, UINT8* rpt, UINT16 len)
 {
-    APPL_TRACE_DEBUG0("bta_hh_co_data: UHID write");
+    APPL_TRACE_VERBOSE0("bta_hh_co_data: UHID write");
     struct uhid_event ev;
     memset(&ev, 0, sizeof(ev));
     ev.type = UHID_INPUT;
@@ -368,7 +368,7 @@ void bta_hh_co_data(UINT8 dev_handle, UINT8 *p_rpt, UINT16 len, tBTA_HH_PROTO_MO
 {
     btif_hh_device_t *p_dev;
 
-    APPL_TRACE_DEBUG6("%s: dev_handle = %d, subclass = 0x%02X, mode = %d, "
+    APPL_TRACE_VERBOSE6("%s: dev_handle = %d, subclass = 0x%02X, mode = %d, "
          "ctry_code = %d, app_id = %d",
          __FUNCTION__, dev_handle, sub_class, mode, ctry_code, app_id);
 

stack/hid/hidd_conn.c
@@ -44,9 +44,6 @@
 #include "hidd_api.h"
 #include "hidd_int.h"
 
-// uncomment following to enable log when report is sent to L2CAP (for measurements)
-//#define REPORT_TRANSFER_TIMESTAMP
-
 static void hidd_l2cif_connect_ind(BD_ADDR  bd_addr, UINT16 cid, UINT16 psm, UINT8 id);
 static void hidd_l2cif_connect_cfm(UINT16 cid, UINT16 result);
 static void hidd_l2cif_config_ind(UINT16 cid, tL2CAP_CFG_INFO *p_cfg);
@@ -91,7 +88,7 @@ static void hidd_check_config_done()
     {
         p_hcon->conn_state = HID_CONN_STATE_CONNECTED;
 
-        hd_cb.device.state = HID_DEV_CONNECTED;
+        hd_cb.device.state = HIDD_DEV_CONNECTED;
 
         hd_cb.callback(hd_cb.device.addr, HID_DHOST_EVT_OPEN, 0, NULL);
 
@@ -211,7 +208,7 @@ static void hidd_l2cif_connect_ind(BD_ADDR  bd_addr, UINT16 cid, UINT16 psm, UIN
     {
         p_dev->in_use = TRUE;
         memcpy(p_dev->addr, bd_addr, sizeof(BD_ADDR));
-        p_dev->state = HID_DEV_NO_CONN;
+        p_dev->state = HIDD_DEV_NO_CONN;
     }
 
     p_hcon = &hd_cb.device.conn;
@@ -567,7 +564,7 @@ static void hidd_l2cif_disconnect_ind(UINT16 cid, BOOLEAN ack_needed)
             hd_cb.pending_data = NULL;
         }
 
-        hd_cb.device.state = HID_DEV_NO_CONN;
+        hd_cb.device.state = HIDD_DEV_NO_CONN;
         p_hcon->conn_state = HID_CONN_STATE_UNUSED;
 
         hd_cb.callback(hd_cb.device.addr, HID_DHOST_EVT_CLOSE, p_hcon->disc_reason, NULL);
@@ -614,7 +611,7 @@ static void hidd_l2cif_disconnect_cfm(UINT16 cid, UINT16 result)
     {
         HIDD_TRACE_EVENT1("%s: INTR and CTRL disconnected", __FUNCTION__);
 
-        hd_cb.device.state = HID_DEV_NO_CONN;
+        hd_cb.device.state = HIDD_DEV_NO_CONN;
         p_hcon->conn_state = HID_CONN_STATE_UNUSED;
 
         if (hd_cb.pending_vc_unplug)
@@ -936,9 +933,9 @@ tHID_STATUS hidd_conn_send_data(UINT8 channel, UINT8 msg_type, UINT8 param,
     BOOLEAN   use_intr;
     UINT8     pool_id;
     UINT16    cid;
-#ifdef REPORT_TRANSFER_TIMESTAMP
-    BOOLEAN   report_transfer = FALSE;
-#endif
+
+    HIDD_TRACE_VERBOSE4("%s: channel(%d), msg_type(%d), len(%d)",
+        __FUNCTION__, channel, msg_type, len);
 
     p_hcon = &hd_cb.device.conn;
 
@@ -964,9 +961,6 @@ tHID_STATUS hidd_conn_send_data(UINT8 channel, UINT8 msg_type, UINT8 param,
         {
             cid = p_hcon->intr_cid;
             pool_id = HID_INTERRUPT_POOL_ID;
-#ifdef REPORT_TRANSFER_TIMESTAMP
-            report_transfer = TRUE;
-#endif
         }
         break;
     default:
@@ -1001,7 +995,7 @@ tHID_STATUS hidd_conn_send_data(UINT8 channel, UINT8 msg_type, UINT8 param,
     }
 
     // check if connected
-    if (hd_cb.device.state != HID_DEV_CONNECTED)
+    if (hd_cb.device.state != HIDD_DEV_CONNECTED)
     {
         // for DATA on intr we hold transfer and try to reconnect
         if (msg_type == HID_TRANS_DATA && cid == p_hcon->intr_cid)
@@ -1025,12 +1019,7 @@ tHID_STATUS hidd_conn_send_data(UINT8 channel, UINT8 msg_type, UINT8 param,
         return HID_ERR_NO_CONNECTION;
     }
 
-#ifdef REPORT_TRANSFER_TIMESTAMP
-    if (report_transfer)
-    {
-        HIDD_TRACE_ERROR1("%s: report sent", __FUNCTION__);
-    }
-#endif
+    HIDD_TRACE_VERBOSE1("%s: report sent", __FUNCTION__);
 
     if (!L2CA_DataWrite (cid, p_buf))
         return (HID_ERR_CONGESTED);

stack/hid/hidh_conn.c
@@ -843,6 +843,7 @@ static void hidh_l2cif_data_ind (UINT16 l2cap_cid, BT_HDR *p_msg)
 
 
     case HID_TRANS_DATA:
+        HIDH_TRACE_VERBOSE2 ("HID-Host hidh_l2cif_data_ind [l2cap_cid=0x%04x], ttype=%02x]", l2cap_cid, ttype);
         evt = (hh_cb.devices[dhandle].conn.intr_cid == l2cap_cid) ?
                     HID_HDEV_EVT_INTR_DATA : HID_HDEV_EVT_CTRL_DATA;
         hh_cb.callback(dhandle, hh_cb.devices[dhandle].addr, evt, rep_type, p_msg);

stack/l2cap/l2c_main.c
@@ -180,6 +180,7 @@ void l2c_rcv_acl_data (BT_HDR *p_msg)
     /* Extract the length and update the buffer header */
     STREAM_TO_UINT16 (hci_len, p);
     p_msg->offset += 4;
+    L2CAP_TRACE_VERBOSE3("%s: received packet from handle(%04x) of len (%d)", __FUNCTION__, handle, hci_len);
 
 #if (L2CAP_HOST_FLOW_CTRL == TRUE)
     /* Send ack if we hit the threshold */

LineageOS/android_system_bt

bta/hd/bta_hd_api.c
@@ -201,7 +201,7 @@ extern void BTA_HdSendReport(tBTA_HD_REPORT *p_report)
 {
     tBTA_HD_SEND_REPORT *p_buf;
 
-    APPL_TRACE_API("%s", __FUNCTION__);
+    APPL_TRACE_VERBOSE("%s", __FUNCTION__);
 
     if ((p_buf = (tBTA_HD_SEND_REPORT *) GKI_getbuf(sizeof(tBTA_HD_SEND_REPORT))) != NULL)
     {

btif/co/bta_hh_co.c
@@ -351,7 +351,7 @@ void bta_hh_co_destroy(int fd)
 
 int bta_hh_co_write(int fd, UINT8* rpt, UINT16 len)
 {
-    APPL_TRACE_DEBUG("bta_hh_co_data: UHID write");
+    APPL_TRACE_VERBOSE("bta_hh_co_data: UHID write");
     struct uhid_event ev;
     memset(&ev, 0, sizeof(ev));
     ev.type = UHID_INPUT;
@@ -515,7 +515,7 @@ void bta_hh_co_data(UINT8 dev_handle, UINT8 *p_rpt, UINT16 len, tBTA_HH_PROTO_MO
     btif_hh_device_t *p_dev;
     UNUSED(peer_addr);
 
-    APPL_TRACE_DEBUG("%s: dev_handle = %d, subclass = 0x%02X, mode = %d, "
+    APPL_TRACE_VERBOSE("%s: dev_handle = %d, subclass = 0x%02X, mode = %d, "
          "ctry_code = %d, app_id = %d",
          __FUNCTION__, dev_handle, sub_class, mode, ctry_code, app_id);
 

stack/hid/hidd_conn.c
@@ -45,9 +45,6 @@
 #include "hidd_int.h"
 #include "bt_utils.h"
 
-// uncomment following to enable log when report is sent to L2CAP (for measurements)
-//#define REPORT_TRANSFER_TIMESTAMP
-
 static void hidd_l2cif_connect_ind(BD_ADDR  bd_addr, UINT16 cid, UINT16 psm, UINT8 id);
 static void hidd_l2cif_connect_cfm(UINT16 cid, UINT16 result);
 static void hidd_l2cif_config_ind(UINT16 cid, tL2CAP_CFG_INFO *p_cfg);
@@ -92,7 +89,7 @@ static void hidd_check_config_done()
     {
         p_hcon->conn_state = HID_CONN_STATE_CONNECTED;
 
-        hd_cb.device.state = HID_DEV_CONNECTED;
+        hd_cb.device.state = HIDD_DEV_CONNECTED;
 
         hd_cb.callback(hd_cb.device.addr, HID_DHOST_EVT_OPEN, 0, NULL);
 
@@ -216,7 +213,7 @@ static void hidd_l2cif_connect_ind(BD_ADDR  bd_addr, UINT16 cid, UINT16 psm, UIN
     {
         p_dev->in_use = TRUE;
         memcpy(p_dev->addr, bd_addr, sizeof(BD_ADDR));
-        p_dev->state = HID_DEV_NO_CONN;
+        p_dev->state = HIDD_DEV_NO_CONN;
     }
 
     p_hcon = &hd_cb.device.conn;
@@ -570,7 +567,7 @@ static void hidd_l2cif_disconnect_ind(UINT16 cid, BOOLEAN ack_needed)
             hd_cb.pending_data = NULL;
         }
 
-        hd_cb.device.state = HID_DEV_NO_CONN;
+        hd_cb.device.state = HIDD_DEV_NO_CONN;
         p_hcon->conn_state = HID_CONN_STATE_UNUSED;
 
         hd_cb.callback(hd_cb.device.addr, HID_DHOST_EVT_CLOSE, p_hcon->disc_reason, NULL);
@@ -617,7 +614,7 @@ static void hidd_l2cif_disconnect_cfm(UINT16 cid, UINT16 result)
     {
         HIDD_TRACE_EVENT("%s: INTR and CTRL disconnected", __FUNCTION__);
 
-        hd_cb.device.state = HID_DEV_NO_CONN;
+        hd_cb.device.state = HIDD_DEV_NO_CONN;
         p_hcon->conn_state = HID_CONN_STATE_UNUSED;
 
         if (hd_cb.pending_vc_unplug)
@@ -984,9 +981,9 @@ tHID_STATUS hidd_conn_send_data(UINT8 channel, UINT8 msg_type, UINT8 param,
     BOOLEAN   use_intr;
     UINT8     pool_id;
     UINT16    cid;
-#ifdef REPORT_TRANSFER_TIMESTAMP
-    BOOLEAN   report_transfer = FALSE;
-#endif
+
+    HIDD_TRACE_VERBOSE("%s: channel(%d), msg_type(%d), len(%d)",
+        __FUNCTION__, channel, msg_type, len);
 
     p_hcon = &hd_cb.device.conn;
 
@@ -1012,9 +1009,6 @@ tHID_STATUS hidd_conn_send_data(UINT8 channel, UINT8 msg_type, UINT8 param,
         {
             cid = p_hcon->intr_cid;
             pool_id = HID_INTERRUPT_POOL_ID;
-#ifdef REPORT_TRANSFER_TIMESTAMP
-            report_transfer = TRUE;
-#endif
         }
         break;
     default:
@@ -1049,7 +1043,7 @@ tHID_STATUS hidd_conn_send_data(UINT8 channel, UINT8 msg_type, UINT8 param,
     }
 
     // check if connected
-    if (hd_cb.device.state != HID_DEV_CONNECTED)
+    if (hd_cb.device.state != HIDD_DEV_CONNECTED)
     {
         // for DATA on intr we hold transfer and try to reconnect
         if (msg_type == HID_TRANS_DATA && cid == p_hcon->intr_cid)
@@ -1079,6 +1073,7 @@ tHID_STATUS hidd_conn_send_data(UINT8 channel, UINT8 msg_type, UINT8 param,
         HIDD_TRACE_ERROR("%s: report sent", __FUNCTION__);
     }
 #endif
+    HIDD_TRACE_VERBOSE("%s: report sent", __FUNCTION__);
 
     if (!L2CA_DataWrite (cid, p_buf))
         return (HID_ERR_CONGESTED);

stack/hid/hidh_conn.c
@@ -856,6 +856,7 @@ static void hidh_l2cif_data_ind (UINT16 l2cap_cid, BT_HDR *p_msg)
 
 
     case HID_TRANS_DATA:
+        HIDH_TRACE_VERBOSE ("HID-Host hidh_l2cif_data_ind [l2cap_cid=0x%04x], ttype=%02x]", l2cap_cid, ttype);
         evt = (hh_cb.devices[dhandle].conn.intr_cid == l2cap_cid) ?
                     HID_HDEV_EVT_INTR_DATA : HID_HDEV_EVT_CTRL_DATA;
         hh_cb.callback(dhandle, hh_cb.devices[dhandle].addr, evt, rep_type, p_msg);

stack/l2cap/l2c_main.c
@@ -181,6 +181,7 @@ void l2c_rcv_acl_data (BT_HDR *p_msg)
     /* Extract the length and update the buffer header */
     STREAM_TO_UINT16 (hci_len, p);
     p_msg->offset += 4;
+    L2CAP_TRACE_VERBOSE("%s: received packet from handle(%04x) of len (%d)", __FUNCTION__, handle, hci_len);
 
     /* Extract the length and CID */
     STREAM_TO_UINT16 (l2cap_len, p);