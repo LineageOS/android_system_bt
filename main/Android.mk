codeaurora-unoffical/platform-external-bluetooth-bluedroid

QLogKit: Enable FW Logging and Set Stack Trace Level
   - Enables FW Logging during BT On and disables
     the FW Logging during BT Off in userdebug
     builds only.

   - Provides an interface to set trace level for
     all the stack layers of bluedroid.

CRs-Fixed: 866285
Change-Id: Ib12d220e0aa4845d04b05b7fbea079d30c308004

main/Android.mk
@@ -57,6 +57,7 @@ LOCAL_SRC_FILES += \
         ../btif/src/btif_smp.c \
         ../btif/src/btif_sdp.c\
         ../btif/src/btif_rfcomm.c \
+        ../btif/src/btif_stack_log.c \
 
 # callouts
 LOCAL_SRC_FILES += \

main/bte_logmsg.c
@@ -437,6 +437,48 @@ BT_API tBTTRC_LEVEL * BTA_SysSetTraceLevel(tBTTRC_LEVEL * p_levels)
     return p_levels;
 } /* BTA_SysSetTraceLevel() */
 
+
+
+/********************************************************************************
+ **
+ **    Function Name:    BTA_setStackLog
+ **
+ **    Purpose:          Set the trace level of the different layers of stack
+                         based on the stack layer and level as input
+ **
+ **    Input Parameters: const char* log_layer, int log_level
+                         Example : TRC_HCI, 5
+ **
+ **    Returns:          void
+ **
+ *********************************************************************************/
+
+void BTA_setStackLog( const char* log_layer, int log_level)
+{
+    const tBTTRC_FUNC_MAP *p_f_map;
+    int new_level = 0;
+    int layer_found = 0;
+    p_f_map = &bttrc_set_level_map[0];
+
+    while ( 0 != p_f_map->layer_id_start )
+    {
+
+        if( (NULL != p_f_map->p_f) && !strcmp( p_f_map->trc_name, log_layer))
+        {
+            new_level = p_f_map->p_f(log_level);
+            DBG_TRACE_DEBUG2("BTA_setStackLog: New trace level set for layer %s is %d", log_layer, new_level);
+            layer_found = 1;
+            break;
+        }
+        p_f_map++;
+    }
+
+    if ( layer_found == 0 )
+    {
+        DBG_TRACE_DEBUG2("BTA_setStackLog: Unable to set Layer %s with level %d. Layer not found", log_layer, log_level);
+    }
+}
+
 /* make sure list is order by increasing layer id!!! */
 tBTTRC_FUNC_MAP bttrc_set_level_map[] = {
     {BTTRC_ID_STK_BTU, BTTRC_ID_STK_HCI, BTU_SetTraceLevel, "TRC_HCI", DEFAULT_CONF_TRACE_LEVEL},

LineageOS/android_system_bt

main/Android.mk
@@ -58,7 +58,8 @@ LOCAL_SRC_FILES += \
     ../btif/src/stack_manager.c \
     ../btif/src/btif_rfcomm.c \
     ../btif/src/btif_mcap.c \
-    ../btif/src/btif_l2cap.c
+    ../btif/src/btif_l2cap.c \
+    ../btif/src/btif_stack_log.c
 
 ifeq ($(BOARD_USES_WIPOWER), true)
     LOCAL_SRC_FILES += \

main/bte_logmsg.c
@@ -268,11 +268,9 @@ static future_t *init(void) {
     LOG_INFO("[bttrc] using compile default trace settings");
     return NULL;
   }
-
   load_levels_from_config(stack_config->get_all());
   return NULL;
 }
-
 
 const module_t bte_logmsg_module = {
   .name = BTE_LOGMSG_MODULE,
@@ -285,3 +283,43 @@ const module_t bte_logmsg_module = {
     NULL
   }
 };
+
+/********************************************************************************
+ **
+ **    Function Name:    BTA_setStackLog
+ **
+ **    Purpose:          Set the trace level of the different layers of stack
+                         based on the stack layer and level as input
+ **
+ **    Input Parameters: const char* log_layer, int log_level
+                         Example : TRC_HCI, 5
+ **
+ **    Returns:          void
+ **
+ *********************************************************************************/
+
+void BTA_setStackLog( const char* log_layer, int log_level)
+{
+    const tBTTRC_FUNC_MAP *p_f_map;
+    int new_level = 0;
+    int layer_found = 0;
+    p_f_map = &bttrc_set_level_map[0];
+
+    while ( 0 != p_f_map->layer_id_start )
+    {
+
+        if( (NULL != p_f_map->p_f) && !strcmp( p_f_map->trc_name, log_layer))
+        {
+            new_level = p_f_map->p_f(log_level);
+            LOG_INFO("BTA_setStackLog: New trace level set for layer %s is %d", log_layer, new_level);
+            layer_found = 1;
+            break;
+        }
+        p_f_map++;
+    }
+
+    if ( layer_found == 0 )
+    {
+        LOG_INFO("BTA_setStackLog: Unable to set Layer %s with level %d. Layer not found", log_layer, log_level);
+    }
+}