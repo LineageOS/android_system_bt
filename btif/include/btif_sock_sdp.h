CyanogenMod/android_external_bluetooth_bluedroid

DUN: Add DUN SDP record to the Bluedroid stack.
Add DUN server SDP record in Bluedroid stack.

CRs-Fixed: 557180
Change-Id: I432496079ae0d7325805b1b1cf684629bbf51035

btif/include/btif_sock_sdp.h
@@ -28,6 +28,8 @@ static const UINT8  UUID_SPP[]              = {0x00, 0x00, 0x11, 0x01, 0x00, 0x0
                                                0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB};
 static const UINT8  UUID_FTP[]              = {0x00, 0x00, 0x11, 0x06, 0x00, 0x00, 0x10, 0x00,
                                                0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB};
+static const UINT8  UUID_DUN[]              = {0x00, 0x00, 0x11, 0x03, 0x00, 0x00, 0x10, 0x00,
+                                               0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB};
 #define RESERVED_SCN_FTP 20
 
 static inline BOOLEAN is_uuid_empty(const uint8_t* uuid)

LineageOS/android_system_bt

btif/include/btif_sock_sdp.h
@@ -33,6 +33,9 @@ static const uint8_t  UUID_SPP[]              = {0x00, 0x00, 0x11, 0x01, 0x00, 0
                                                  0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB};
 static const uint8_t  UUID_FTP[]              = {0x00, 0x00, 0x11, 0x06, 0x00, 0x00, 0x10, 0x00,
                                                  0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB};
+static const uint8_t  UUID_DUN[]              = {0x00, 0x00, 0x11, 0x03, 0x00, 0x00, 0x10, 0x00,
+                                                 0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB};
+
 static inline bool is_uuid_empty(const uint8_t* uuid)
 {
    static uint8_t empty_uuid[16];