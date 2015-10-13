Evervolv/android_external_bluetooth_bluedroid

fix up bluedroid compile errors
If
define BLE_INCLUDED FALSE
define BTA_GATT_INCLUDED FALSE
define SMP_INCLUDED FALSE

There would be compile errors.   This fixes that and gets BT
working on some devices that need these false

Change-Id: I6aae0debd2cc84aeeea0ac3f42b3ee2339580f6a

btif/src/bluetooth.c
@@ -544,6 +544,7 @@ static void bt_le_lpp_rssi_monitor_upstream_evt(uint16_t event, char* p_param)
     case BT_LE_LPP_RSSI_MONITOR_CMD_CPL_EVT:
         switch(p_cb->content.cmd_cpl_evt.subcmd)
         {
+#if BLE_INCLUDED == TRUE
         case WRITE_RSSI_MONITOR_THRESHOLD:
             HAL_CBACK(bt_hal_cbacks, le_lpp_write_rssi_thresh_cb,
                       &p_cb->remote_bda, p_cb->content.cmd_cpl_evt.status);
@@ -561,6 +562,7 @@ static void bt_le_lpp_rssi_monitor_upstream_evt(uint16_t event, char* p_param)
                       &p_cb->remote_bda, p_cb->content.cmd_cpl_evt.detail.enable,
                       p_cb->content.cmd_cpl_evt.status);
             break;
+#endif
         default:
             break;
         }
@@ -628,6 +630,7 @@ static void bt_le_handle_lpp_monitor_rssi(uint16_t event, char *p_param)
     switch(event)
     {
     case BT_LE_LPP_WRITE_RSSI_THRESH:
+#if BLE_INCLUDED == TRUE
         ALOGD("%s write rssi thrshold ", __FUNCTION__);
         status = BTM_Write_Rssi_Monitor_Threshold(p_cb->remote_bda.address, p_cb->min_thresh, p_cb->max_thresh);
 
@@ -637,8 +640,10 @@ static void bt_le_handle_lpp_monitor_rssi(uint16_t event, char *p_param)
             error.subcmd = WRITE_RSSI_MONITOR_THRESHOLD;
             error.status = 1; /* 0 indicate success*/
         }
+#endif
         break;
     case BT_LE_LPP_MONITOR_RSSI_START:
+#if BLE_INCLUDED == TRUE
         ALOGD("%s starts monitoring rssi", __FUNCTION__);
         status = BTM_Enable_Rssi_Monitor(p_cb->remote_bda.address, 1);
 
@@ -649,8 +654,10 @@ static void bt_le_handle_lpp_monitor_rssi(uint16_t event, char *p_param)
             error.detail.enable = 1;
             error.status = 1; /* 0 indicate success*/
         }
+#endif
         break;
     case BT_LE_LPP_MONITOR_RSSI_STOP:
+#if BLE_INCLUDED == TRUE
         ALOGD("%s stop monitoring rssi", __FUNCTION__);
         status = BTM_Enable_Rssi_Monitor(p_cb->remote_bda.address, 0);
 
@@ -661,8 +668,10 @@ static void bt_le_handle_lpp_monitor_rssi(uint16_t event, char *p_param)
             error.detail.enable = 0;
             error.status = 1; /* 0 indicate success*/
         }
+#endif
         break;
     case BT_LE_LPP_READ_RSSI_THRESH:
+#if BLE_INCLUDED == TRUE
         ALOGD("%s read rssi threshold", __FUNCTION__);
         status = BTM_Read_Rssi_Monitor_Threshold(p_cb->remote_bda.address);
 
@@ -672,6 +681,7 @@ static void bt_le_handle_lpp_monitor_rssi(uint16_t event, char *p_param)
             error.subcmd = READ_RSSI_MONITOR_THRESHOLD;
             error.status = 1; /* 0 indicate success*/
         }
+#endif
         break;
     default:
         error.status = 1;

LineageOS/android_system_bt

btif/src/bluetooth.c
@@ -127,7 +127,9 @@ extern const btl2cap_interface_t *btif_l2cap_get_interface(void);
 extern const btrfcomm_interface_t *btif_rfcomm_get_interface(void);
 extern const btmcap_interface_t *btif_mcap_get_interface(void);
 extern const btgatt_test_interface_t *btif_gatt_test_get_interface(void);
+#if SMP_INCLUDED == TRUE
 extern const btsmp_interface_t *btif_smp_get_interface(void);
+#endif
 extern const btgap_interface_t *btif_gap_get_interface(void);
 #endif
 extern const btstacklog_interface_t *btif_stack_log_interface(void);
@@ -428,7 +430,9 @@ static const void* get_testapp_interface(int test_app_profile)
         case TEST_APP_GATT:
            return btif_gatt_test_get_interface();
         case TEST_APP_SMP:
+#if SMP_INCLUDED == TRUE
            return btif_smp_get_interface();
+#endif
         case TEST_APP_GAP:
            return btif_gap_get_interface();
         default:
@@ -486,7 +490,6 @@ int le_test_mode(uint16_t opcode, uint8_t* buf, uint8_t len)
     return btif_le_test_mode(opcode, buf, len);
 }
 #endif
-
 int config_hci_snoop_log(uint8_t enable)
 {
     LOG_INFO("config_hci_snoop_log");