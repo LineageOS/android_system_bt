706b26755fadb2b85879aead142a9b65feca8505
Luke Zhang <lukez@qca.qualcomm.com>
2015-11-05 18:23:07 -0800

    Fix the command timeout issue with TX idle timer
    Start the idle timer after sending out each command to prevent power
    collapse.
    
    CRs-fixed:926763
    Change-Id: I98e649fa40a4622e3c6bce4ea5c53d51e25413b3

--- hci/src/hci_layer.c
+++ hci/src/hci_layer.c
@@ -467,9 +467,9 @@ static void event_command_ready(fixed_queue_t *queue, UNUSED_ATTR void *context)
     pthread_mutex_unlock(&commands_pending_response_lock);
 
     // Send it off
-    low_power_manager->wake_assert();
+    low_power_manager->stop_idle_timer();
     packet_fragmenter->fragment_and_dispatch(wait_entry->command);
-    low_power_manager->transmit_done();
+    low_power_manager->start_idle_timer(false);
 
     non_repeating_timer_restart_if(command_response_timer, !list_is_empty(commands_pending_response));
   }
