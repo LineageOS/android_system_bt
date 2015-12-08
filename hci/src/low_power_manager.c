4bf600fcdb9e7cf6fb32a45f3d6d9f93655c2b1d
Ethan Chen <intervigil@gmail.com>
2015-12-08 10:59:54 -0800

    Revert "Fix the command timeout issue with TX idle timer"
    * This breaks Broadcoms
    
    This reverts commit 706b26755fadb2b85879aead142a9b65feca8505.
    
    Change-Id: I586ecb86c74636d903a67f9da47745f9f38540bc

--- hci/src/low_power_manager.c
+++ hci/src/low_power_manager.c
@@ -55,8 +55,8 @@ static void event_allow_device_sleep(void *context);
 static void event_idle_timeout(void *context);
 
 static void reset_state();
-void start_idle_timer(bool check_LPM);
-void stop_idle_timer();
+static void start_idle_timer();
+static void stop_idle_timer();
 
 static thread_fn event_functions[] = {
   event_disable,
@@ -122,7 +122,7 @@ static void transmit_done() {
   transmit_is_done = true;
   if (wake_state == LPM_WAKE_W4_TX_DONE || wake_state == LPM_WAKE_ASSERTED) {
     wake_state = LPM_WAKE_W4_TIMEOUT;
-    start_idle_timer(true);
+    start_idle_timer();
   }
 }
 
@@ -156,7 +156,7 @@ static void allow_device_sleep() {
   if (state == LPM_ENABLED && wake_state == LPM_WAKE_ASSERTED) {
     if (transmit_is_done) {
       wake_state = LPM_WAKE_W4_TIMEOUT;
-      start_idle_timer(true);
+      start_idle_timer();
     } else {
       wake_state = LPM_WAKE_W4_TX_DONE;
     }
@@ -183,16 +183,14 @@ static void idle_timer_expired(UNUSED_ATTR void *context) {
     thread_post(thread, event_idle_timeout, NULL);
 }
 
-void start_idle_timer(bool check_LPM) {
-  if (state == LPM_ENABLED || !check_LPM) {
+static void start_idle_timer() {
+  if (state == LPM_ENABLED) {
     alarm_set(idle_alarm, idle_timeout_ms, idle_timer_expired, NULL);
-    LOG_DEBUG("%s check_LPM = %d", __func__, check_LPM);
   }
 }
 
-void stop_idle_timer() {
+static void stop_idle_timer() {
   alarm_cancel(idle_alarm);
-  LOG_DEBUG("%s", __func__);
 }
 
 static void event_disable(UNUSED_ATTR void *context) {
@@ -231,9 +229,7 @@ static const low_power_manager_t interface = {
   cleanup,
   post_command,
   wake_assert,
-  transmit_done,
-  start_idle_timer,
-  stop_idle_timer
+  transmit_done
 };
 
 const low_power_manager_t *low_power_manager_get_interface() {
