a788e57b70803e42fff322d74367e37812749a6c
Ethan Chen <intervigil@gmail.com>
2015-12-08 10:59:54 -0800

    Bluetooth: Don't use command TX idle timer Broadcoms
    * Bluesleep already takes care of this, doing this here results in TX
      timeouts on Broadcom devices using bluesleep.
    
    Change-Id: I586ecb86c74636d903a67f9da47745f9f38540bc

--- hci/Android.mk
+++ hci/Android.mk
@@ -19,6 +19,10 @@ LOCAL_SRC_FILES := \
     src/packet_fragmenter.c \
     src/vendor.c
 
+ifeq ($(BOARD_HAVE_BLUETOOTH_BCM),true)
+LOCAL_CFLAGS += -DBOARD_HAVE_BLUETOOTH_BCM
+endif
+
 ifeq ($(BLUETOOTH_HCI_USE_MCT),true)
 LOCAL_CFLAGS += -DHCI_USE_MCT
 endif
