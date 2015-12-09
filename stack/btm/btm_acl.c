98889ada28e200b56a153fbb9976cc9de9408f58
Ivan Grinko <iivanich@gmail.com>
2015-12-09 22:11:48 +0200

    btm: Fix compile with BLE disabled
    Change-Id: Idabd2e1d4b7dc1ea1ca7e8398ef519734230010e

--- stack/btm/btm_acl.c
+++ stack/btm/btm_acl.c
@@ -1029,8 +1029,10 @@ void btm_read_remote_version_complete (UINT8 *p)
                 btm_read_remote_features (p_acl_cb->hci_handle);
             }
 
+#if BLE_INCLUDED == TRUE
             if (p_acl_cb->transport == BT_TRANSPORT_LE)
                 l2cble_notify_le_connection (p_acl_cb->remote_addr);
+#endif
             break;
         }
     }
