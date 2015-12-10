3476a72ce64678bedd89d0f095a21e2e4a2307bb
Mallikarjuna GB <gbmalli@codeaurora.org>
2015-12-10 18:36:48 +0530

    btsnoop: Remove the ext snoop dependency on snoop socket
    Snoop socket cannot be enabled by default to be CTS compliant.
    External snoop dump uses same thread, external dump related
    functionality is moved out of TCP socket enabling
    condition check
    
    CRs-Fixed: 951828
    Change-Id: Icf51b27531a42ce257704d2c00b6e6b394cad3af

--- hci/src/btsnoop_net.c
+++ hci/src/btsnoop_net.c
@@ -100,9 +100,6 @@ void btsnoop_net_close() {
 
 void btsnoop_net_write(const void *data, size_t length) {
   ssize_t ret;
-#if (!defined(BT_NET_DEBUG) || (BT_NET_DEBUG != TRUE))
-  return;               // Disable using network sockets for security reasons
-#endif
 
   pthread_mutex_lock(&client_socket_lock_);
   if (client_socket_btsnoop != -1) {
