0b4dec79909301c0f0a86f1c377af311c15b8f0a
Gurpreet Ghai <gghai@codeaurora.org>
2016-02-03 15:01:41 +0530

    Bluetooth: Increasing the size of security service record and Adding null check to avoid exception after l2cap psm assigned
    With the number of supported services increased,
    larger service record array is required. Smaller array
    is resulting in out of record error causing l2cap
    deregister.
    When l2cap deregister happens, it cause socks to
    cleanup, If another thread gets psm during this time
    and tries to get sock by id, null is returned. This,
    causes null pointer exception.
    
    Change-Id: I2e77127262119cabf2c372f894427dcdf3350e80
    CRs-Fixed: 968880

--- btif/src/btif_sock_l2cap.c
+++ btif/src/btif_sock_l2cap.c
@@ -832,11 +832,14 @@ void on_l2cap_psm_assigned(int id, int psm) {
      *  mtu will be set below */
     pthread_mutex_lock(&state_lock);
     sock = btsock_l2cap_find_by_id_l(id);
-    sock->channel = psm;
+    if(sock) {
+        sock->channel = psm;
 
-    if(btSock_start_l2cap_server_l(sock) != BT_STATUS_SUCCESS) {
-        btsock_l2cap_free_l(sock);
-    }
+        if(btSock_start_l2cap_server_l(sock) != BT_STATUS_SUCCESS) {
+            btsock_l2cap_free_l(sock);
+        }
+    } else
+        APPL_TRACE_ERROR("Error: sock is null");
 
     pthread_mutex_unlock(&state_lock);
 
