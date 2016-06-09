CyanogenMod/android_external_bluetooth_bluedroid

bluetooth: a2dp: Property account pcm.counter
Every tick we account that there is now pcm_bytes_per_tick *
number_of_ticks (since last accounting).

The existing code would be right with infinite precision floating
point math but with integer math undercounts the amount of PCM
data that is available.

For example, if we have 20ms ticks and we have two ticks that occur
at intervals of 20543 us and 39457 us (a total of 3 ticks) and we
generate 3528 bytes of PCM data per tick (44100 * 2 channels * 16bit)
then we should determine that there is exactly 10584 new bytes of
PCM data available.  It actually computes that there are

3528 * 20543 / 20000 = 3623.7852 vs. int 3623 bytes +
3528 * 39457 / 20000 = 6960.2148 vs. int 6960
                       ---------         -----
                       10584       vs.   10583

Similarly for 48K audio which generated 3840 bytes per frame

3840 * 20543 / 20000 = 3944.2560 vs. int 3944 bytes +
3840 * 39457 / 20000 = 7575.7440 vs. int 7575 bytes
                       ---------        -----
                       11520       vs.  11519

Which means that on average we are undercounting about half a byte
per actual tick that occurs.

In practice if you assume 25ms per tick when we are asking for
20ms then we are losing 1 byte of data about every 50ms, 20
bytes per second or 1200 bytes per minute.  1200 bytes is about
7ms of audio is lost every minute.

Change-Id: I413d764adcf0210a995aebde91908e3ed1d3b00d

btif/src/btif_media_task.c
@@ -2748,13 +2748,23 @@ static UINT8 check_for_max_number_of_frames_per_packet()
     return result;
 }
 
+static inline UINT64 now_us_rounded_to_nearest_tick(void)
+{
+    UINT32 tick_us = BTIF_MEDIA_TIME_TICK * 1000;
+    return (GKI_now_us() + tick_us/2) / tick_us * tick_us;
+}
+
 /*******************************************************************************
  **
  ** Function         btif_get_num_aa_frame
  **
  ** Description
  **
- ** Returns          The number of media frames in this time slice
+ ** Returns          The number of media frames in this time slice.  When a
+ **                  partial time slice has occurred, we round to the nearest
+ **                  total number of time slices.  This avoids all rounding errors
+ **                  that could occur in computing the amount of data that should
+ **                  be sent.
  **
  *******************************************************************************/
 static UINT8 btif_get_num_aa_frame(void)
@@ -2767,7 +2777,7 @@ static UINT8 btif_get_num_aa_frame(void)
         {
             UINT32 pcm_bytes_per_frame = compute_pcm_bytes_per_frame();
             UINT32 us_this_tick = BTIF_MEDIA_TIME_TICK * 1000;
-            UINT64 now_us = GKI_now_us();
+            UINT64 now_us = now_us_rounded_to_nearest_tick();
             if (last_frame_us != 0)
                 us_this_tick = (now_us - last_frame_us);
             last_frame_us = now_us;

LineageOS/android_system_bt

btif/src/btif_media_task.c
@@ -2725,6 +2725,12 @@ static UINT8 check_for_max_number_of_frames_per_packet()
     return result;
 }
 
+static inline UINT64 now_us_rounded_to_nearest_tick(void)
+{
+    UINT32 tick_us = BTIF_MEDIA_TIME_TICK * 1000;
+    return (time_now_us() + tick_us/2) / tick_us * tick_us;
+}
+
 /*******************************************************************************
  **
  ** Function         btif_get_num_aa_frame
@@ -2750,7 +2756,7 @@ static void btif_get_num_aa_frame(UINT8 *num_of_iterations, UINT8 *num_of_frames
             APPL_TRACE_DEBUG("pcm_bytes_per_frame %u", pcm_bytes_per_frame);
 
             UINT32 us_this_tick = BTIF_MEDIA_TIME_TICK * 1000;
-            UINT64 now_us = time_now_us();
+            UINT64 now_us = now_us_rounded_to_nearest_tick();
             if (last_frame_us != 0)
                 us_this_tick = (now_us - last_frame_us);
             last_frame_us = now_us;