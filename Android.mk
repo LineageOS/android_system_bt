CodeLinaro-mirror/la_platform_vendor_qcom-opensource_system_bt

Enbale Audio track for user builds
Enabled the audio track flag for user builds as well.

Change-Id: I8e41d118d1334db39f26eea730b6a6c8e2486c3a
CRs-Fixed: 1048502

Android.mk
@@ -25,8 +25,9 @@ endif
 
 ifneq ($(TARGET_BUILD_VARIANT),user)
 bdroid_CFLAGS += -DBLUEDROID_DEBUG
-bdroid_CFLAGS += -DUSE_AUDIO_TRACK
 endif
+
+bdroid_CFLAGS += -DUSE_AUDIO_TRACK
 
 ifeq ($(BOARD_USES_WIPOWER), true)
 bdroid_CFLAGS  += -DWIPOWER_SUPPORTED

LineageOS/android_system_bt

Android.mk
@@ -19,8 +19,9 @@ endif
 
 ifneq ($(TARGET_BUILD_VARIANT),user)
 bluetooth_CFLAGS += -DBLUEDROID_DEBUG
-bluetooth_CFLAGS += -DUSE_AUDIO_TRACK
 endif
+
+bluetooth_CFLAGS += -DUSE_AUDIO_TRACK
 
 bluetooth_CFLAGS += -DEXPORT_SYMBOL="__attribute__((visibility(\"default\")))"
 