12a925c50bfafd9c151c736807974c4bf1aa6330
Aaron Kling <webgeek1234@gmail.com>
2016-09-12 11:00:24 -0500

    Revert commits related to dynamic hci and layer selection.
    These break bt on non-qcom devices.
    
    This reverts the following commits:
    1987a39f742493ccbcbdfd52290a969a940c41ff
    3142c92d5e65ead7e206b1a326e0a5045176f2b9
    9eb80bf1c8cf24385375d76ff4453949a490edd4
    b3bfb154195109d4eddb2bb14c23f8cf2b122f98
    eb46fdb217ac117b83202d7471199e6042ed18a9
    
    Change-Id: Ia3305cca00118bdcf7e5ef72287caa77a8daefeb

--- hci/Android.mk
+++ hci/Android.mk
@@ -21,6 +21,10 @@ LOCAL_SRC_FILES := \
     src/packet_fragmenter.c \
     src/vendor.c
 
+ifeq ($(QCOM_BT_USE_SMD_TTY),true)
+LOCAL_CFLAGS += -DQCOM_WCN_SSR
+endif
+
 LOCAL_C_INCLUDES += \
     $(LOCAL_PATH)/include \
     $(LOCAL_PATH)/.. \
@@ -33,7 +37,11 @@ LOCAL_C_INCLUDES += \
 
 LOCAL_MODULE := libbt-hci
 
-ifeq ($(TARGET_BUILD_VARIANT), eng)
+ifeq ($(BLUETOOTH_HCI_USE_MCT),true)
+LOCAL_CFLAGS += -DHCI_USE_MCT
+endif
+
+ifeq ($(TARGET_BUILD_VARIANT),eng)
     LOCAL_CFLAGS += -DBTSNOOP_DEFAULT=TRUE
 endif
 
