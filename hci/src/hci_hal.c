868a0156002cd008c16fdc93e79da42ed12fe010
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-09-25 10:54:34 +0800

    bt: Select H4 HCI driver when QC bluetooth prop is unset
    This restores the default AOSP behaviour, which also happens
    to be the behaviour we had in CM 13.
    
    Change-Id: I5a23575467fa0f823f0d778b7cf127ad1565916e

--- hci/src/hci_hal.c
+++ hci/src/hci_hal.c
@@ -31,7 +31,8 @@ bt_soc_type soc_type;
 const hci_hal_t *hci_hal_get_interface() {
     soc_type = get_soc_type();
 
-    if (soc_type == BT_SOC_ROME || soc_type == BT_SOC_CHEROKEE) {
+    if (soc_type == BT_SOC_ROME || soc_type == BT_SOC_CHEROKEE
+            || soc_type == BT_SOC_DEFAULT) {
         return hci_hal_h4_get_interface();
     } else {
         return hci_hal_mct_get_interface();
