8ee6c0edc345ed1d32bb823abcc3a9daf4cf056c
Ivan Ivanich <iivanich@gmail.com>
2016-10-01 11:34:30 -0700

    Revert "Fix the command timeout issue with TX idle timer"
    This reverts commit 5a234ca06c185f336a6398efe09f78c4d0dacf0b.
    
    Change-Id: I2cec99e8cc119a9f8142f7370db1d4e1ff880e97

--- hci/src/hci_layer.c
+++ hci/src/hci_layer.c
@@ -534,9 +534,9 @@ static void event_command_ready(fixed_queue_t *queue, UNUSED_ATTR void *context)
     pthread_mutex_unlock(&commands_pending_response_lock);
 
     // Send it off
-    low_power_manager->stop_idle_timer();
+    low_power_manager->wake_assert();
     packet_fragmenter->fragment_and_dispatch(wait_entry->command);
-    low_power_manager->start_idle_timer(false);
+    low_power_manager->transmit_done();
 
     update_command_response_timer();
   }
