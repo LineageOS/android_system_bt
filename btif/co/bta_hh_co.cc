7bc0edda0fd10abb3ff73704b130428ac15ffbae
Kim Low <klow@gaikai.com>
2017-04-05 18:01:34 -0700

    Fix MAC address byte ordering in the uniq field
    The UNIQ field can be used in the driver to detect duplicate devices.
    For example, if a controller is connected via both Bluetooth and USB,
    the driver can use the UNIQ field, which typically contains the unique
    MAC address to identify that it's the same device.
    
    Test: Connect a Bluetooth device and check its MAC address using
          ioctl(EVIOCGUNIQ) call.
    Change-Id: I458608e845fcb24c0d615f6aef8d92ccb08d08ec

--- btif/co/bta_hh_co.cc
+++ btif/co/bta_hh_co.cc
@@ -474,11 +474,8 @@ void bta_hh_co_send_hid_info(btif_hh_device_t* p_dev, const char* dev_name,
   memset(&ev, 0, sizeof(ev));
   ev.type = UHID_CREATE;
   strncpy((char*)ev.u.create.name, dev_name, sizeof(ev.u.create.name) - 1);
-  snprintf((char*)ev.u.create.uniq, sizeof(ev.u.create.uniq),
-           "%2.2X:%2.2X:%2.2X:%2.2X:%2.2X:%2.2X", p_dev->bd_addr.address[5],
-           p_dev->bd_addr.address[4], p_dev->bd_addr.address[3],
-           p_dev->bd_addr.address[2], p_dev->bd_addr.address[1],
-           p_dev->bd_addr.address[0]);
+  snprintf((char*)ev.u.create.uniq, sizeof(ev.u.create.uniq), "%s",
+           p_dev->bd_addr.ToString().c_str());
   ev.u.create.rd_size = dscp_len;
   ev.u.create.rd_data = p_dscp;
   ev.u.create.bus = BUS_BLUETOOTH;
