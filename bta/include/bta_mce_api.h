5b790feeeb211c42bf78ca3ae9c26aa30e516765
Jakub Pawlowski <jpawlowski@google.com>
2017-09-18 09:00:20 -0700

    Make copyright headers consistent with Google template; remove "(C)"
    Test: Comment changes only; still compiles...
    Change-Id: Id699a8170112f06e4a2c9f2e0f0834d1817ace4e

--- bta/include/bta_mce_api.h
+++ bta/include/bta_mce_api.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
- *  Copyright (C) 2014 The Android Open Source Project
- *  Copyright (C) 2006-2012 Broadcom Corporation
+ *  Copyright 2014 The Android Open Source Project
+ *  Copyright 2006-2012 Broadcom Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
