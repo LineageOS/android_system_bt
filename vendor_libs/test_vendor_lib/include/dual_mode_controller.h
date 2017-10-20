154efdfd1fe99265888d4671e5b6db7c016a1180
Myles Watson <mylesgw@google.com>
2017-10-20 09:46:59 -0700

    test_vendor: Extract DeviceProperties
    Test: start/stop Bluetooth with rootcanal loaded
    Change-Id: Ic3dd1c87df9583c4b5957592bca3332f2daaa3da

--- vendor_libs/test_vendor_lib/include/dual_mode_controller.h
+++ vendor_libs/test_vendor_lib/include/dual_mode_controller.h
@@ -24,12 +24,12 @@
 
 #include "acl_packet.h"
 #include "async_manager.h"
-#include "base/json/json_value_converter.h"
 #include "base/time/time.h"
 #include "bt_address.h"
 #include "command_packet.h"
 #include "connection.h"
 #include "device.h"
+#include "device_properties.h"
 #include "event_packet.h"
 #include "sco_packet.h"
 #include "test_channel_transport.h"
@@ -49,120 +49,6 @@ namespace test_vendor_lib {
 // "Hci" to distinguish it as a controller command.
 class DualModeController {
  public:
-  class Properties {
-   public:
-    explicit Properties(const std::string& file_name);
-
-    // Access private configuration data
-
-    // Specification Version 4.2, Volume 2, Part E, Section 7.4.1
-    const std::vector<uint8_t>& GetLocalVersionInformation() const;
-
-    // Specification Version 4.2, Volume 2, Part E, Section 7.4.2
-    const std::vector<uint8_t>& GetLocalSupportedCommands() const {
-      return local_supported_commands_;
-    }
-
-    // Specification Version 4.2, Volume 2, Part E, Section 7.4.3
-    uint64_t GetLocalSupportedFeatures() const {
-      return local_extended_features_[0];
-    };
-
-    // Specification Version 4.2, Volume 2, Part E, Section 7.4.4
-    uint8_t GetLocalExtendedFeaturesMaximumPageNumber() const {
-      return local_extended_features_.size() - 1;
-    };
-
-    uint64_t GetLocalExtendedFeatures(uint8_t page_number) const {
-      CHECK(page_number < local_extended_features_.size());
-      return local_extended_features_[page_number];
-    };
-
-    // Specification Version 4.2, Volume 2, Part E, Section 7.4.5
-    uint16_t GetAclDataPacketSize() const { return acl_data_packet_size_; }
-
-    uint8_t GetSynchronousDataPacketSize() const {
-      return sco_data_packet_size_;
-    }
-
-    uint16_t GetTotalNumAclDataPackets() const { return num_acl_data_packets_; }
-
-    uint16_t GetTotalNumSynchronousDataPackets() const {
-      return num_sco_data_packets_;
-    }
-
-    const BtAddress& GetAddress() const { return address_; }
-
-    // Specification Version 4.2, Volume 2, Part E, Section 7.4.8
-    const std::vector<uint8_t>& GetSupportedCodecs() const {
-      return supported_codecs_;
-    }
-
-    const std::vector<uint32_t>& GetVendorSpecificCodecs() const {
-      return vendor_specific_codecs_;
-    }
-
-    const std::string& GetLocalName() const { return local_name_; }
-
-    uint8_t GetVersion() const { return version_; }
-
-    uint16_t GetRevision() const { return revision_; }
-
-    uint8_t GetLmpPalVersion() const { return lmp_pal_version_; }
-
-    uint16_t GetLmpPalSubversion() const { return lmp_pal_subversion_; }
-
-    uint16_t GetManufacturerName() const { return manufacturer_name_; }
-
-    // Specification Version 4.2, Volume 2, Part E, Section 7.8.2
-    uint16_t GetLeDataPacketLength() const { return le_data_packet_length_; }
-
-    uint8_t GetTotalNumLeDataPackets() const { return num_le_data_packets_; }
-
-    // Specification Version 4.2, Volume 2, Part E, Section 7.8.3
-    uint64_t GetLeLocalSupportedFeatures() const {
-      return le_supported_features_;
-    }
-
-    // Specification Version 4.2, Volume 2, Part E, Section 7.8.14
-    uint8_t GetLeWhiteListSize() const { return le_white_list_size_; }
-
-    // Specification Version 4.2, Volume 2, Part E, Section 7.8.27
-    uint64_t GetLeSupportedStates() const { return le_supported_states_; }
-
-    // Vendor-specific commands (see hcidefs.h)
-    const std::vector<uint8_t>& GetLeVendorCap() const {
-      return le_vendor_cap_;
-    }
-
-    static void RegisterJSONConverter(
-        base::JSONValueConverter<Properties>* converter);
-
-   private:
-    uint16_t acl_data_packet_size_;
-    uint8_t sco_data_packet_size_;
-    uint16_t num_acl_data_packets_;
-    uint16_t num_sco_data_packets_;
-    uint8_t version_;
-    uint16_t revision_;
-    uint8_t lmp_pal_version_;
-    uint16_t manufacturer_name_;
-    uint16_t lmp_pal_subversion_;
-    std::vector<uint8_t> supported_codecs_;
-    std::vector<uint32_t> vendor_specific_codecs_;
-    std::vector<uint8_t> local_supported_commands_;
-    std::string local_name_;
-    std::vector<uint64_t> local_extended_features_;
-    BtAddress address_;
-
-    uint16_t le_data_packet_length_;
-    uint8_t num_le_data_packets_;
-    uint8_t le_white_list_size_;
-    uint64_t le_supported_features_;
-    uint64_t le_supported_states_;
-    std::vector<uint8_t> le_vendor_cap_;
-  };
-
   // Sets all of the methods to be used as callbacks in the HciHandler.
   DualModeController();
 
@@ -579,7 +465,7 @@ class DualModeController {
 
   State state_;
 
-  Properties properties_;
+  DeviceProperties properties_;
 
   std::vector<std::shared_ptr<Device>> devices_;
 
