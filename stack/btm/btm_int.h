325abcfa4a039aa6cb0c7a350c11daacd3cc9b62
Vishwath Mohan <vishwath@google.com>
2017-11-08 22:45:47 -0800

    Fix BTM callbacks so callee and caller types match
    This CL fixes inconsistencies between the actual type of a callback
    function, and the type assumed at the call site. This respects the
    intended function signature.
    
    For P, we plan to enable control-flow integrity (CFI) across multiple
    platform components, including BT. This mismatch will break CFI and
    cause runtime errors when enabled - so this is also a preemptive
    measure to get BT compatible in preparation.
    
    Bug: 30227045
    Test: Builds and runs without errors when CFI is enabled.
    Change-Id: I8970e6866d8b37a9adf5e040db993d30f1b5103a

--- stack/btm/btm_int.h
+++ stack/btm/btm_int.h
@@ -193,7 +193,7 @@ extern void btm_ble_resolving_list_remove_dev(tBTM_SEC_DEV_REC* p_dev_rec);
 
 /* Vendor Specific Command complete evt handler */
 extern void btm_vsc_complete(uint8_t* p, uint16_t cc_opcode, uint16_t evt_len,
-                             tBTM_CMPL_CB* p_vsc_cplt_cback);
+                             tBTM_VSC_CMPL_CB* p_vsc_cplt_cback);
 extern void btm_inq_db_reset(void);
 extern void btm_vendor_specific_evt(uint8_t* p, uint8_t evt_len);
 extern void btm_delete_stored_link_key_complete(uint8_t* p);
