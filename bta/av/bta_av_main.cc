1b548bb34a0bfc300b8aa03c271148141673922f
Guang Xie <gregxie@google.com>
2018-02-15 12:42:15 -0800

    Enable HCI Sniff mode and disable subrating for A2DP
    Enabling the HCI Sniff mode could help headsets to save power.
    
    Note that subrating is disabled implicitly by NOT sending the
    "Sent Sniff Subrating" HCI command to the BT controller,
    and effectively using the default Sniff Subrating parameters.
    The Headset shouldn't go into Subrating Mode to prevent
    potential stuttering if the Headset chooses to use subrating
    with the wrong parameters.
    
    NOTE: This should affect only Pixel Buds.
    
    Also:
     - Added some extra log messages
     - Minor cleanup (argument/variable renaming)
    
    Bug: 74344711
    Bug: 74361002
    Test: Manual - audio streaming; examine btsnoop logs
    Change-Id: Iec5086dba6f9003c21bcb41eddb3bc83ac7dddd1

--- bta/av/bta_av_main.cc
+++ bta/av/bta_av_main.cc
@@ -835,13 +835,14 @@ void bta_av_restore_switch(void) {
   tBTA_AV_CB* p_cb = &bta_av_cb;
   int i;
   uint8_t mask;
+  uint8_t set_policy = (HCI_ENABLE_SNIFF_MODE | HCI_ENABLE_MASTER_SLAVE_SWITCH);
 
   APPL_TRACE_DEBUG("%s: reg_audio: 0x%x", __func__, bta_av_cb.reg_audio);
   for (i = 0; i < BTA_AV_NUM_STRS; i++) {
     mask = BTA_AV_HNDL_TO_MSK(i);
     if (p_cb->conn_audio == mask) {
       if (p_cb->p_scb[i]) {
-        bta_sys_set_policy(BTA_ID_AV, HCI_ENABLE_MASTER_SLAVE_SWITCH,
+        bta_sys_set_policy(BTA_ID_AV, set_policy,
                            p_cb->p_scb[i]->PeerAddress());
       }
       break;
@@ -865,6 +866,7 @@ static void bta_av_sys_rs_cback(UNUSED_ATTR tBTA_SYS_CONN_STATUS status,
   tBTA_AV_SCB* p_scb = NULL;
   uint8_t cur_role;
   uint8_t peer_idx = 0;
+  uint8_t set_policy = (HCI_ENABLE_SNIFF_MODE | HCI_ENABLE_MASTER_SLAVE_SWITCH);
 
   APPL_TRACE_DEBUG(
       "%s: peer %s new_role:%d hci_status:0x%x bta_av_cb.rs_idx:%d", __func__,
@@ -884,9 +886,7 @@ static void bta_av_sys_rs_cback(UNUSED_ATTR tBTA_SYS_CONN_STATUS status,
       /*
       if ((id != BTM_ROLE_MASTER) && (app_id != HCI_SUCCESS))
       {
-          bta_sys_set_policy(BTA_ID_AV,
-      (HCI_ENABLE_MASTER_SLAVE_SWITCH|HCI_ENABLE_SNIFF_MODE),
-      p_scb->PeerAddress());
+          bta_sys_set_policy(BTA_ID_AV, set_policy, p_scb->PeerAddress());
       }
       */
       p_buf->hdr.event = BTA_AV_ROLE_CHANGE_EVT;
@@ -903,7 +903,7 @@ static void bta_av_sys_rs_cback(UNUSED_ATTR tBTA_SYS_CONN_STATUS status,
   if ((HCI_SUCCESS != app_id) &&
       (BTM_GetRole(peer_addr, &cur_role) == BTM_SUCCESS) &&
       (cur_role == BTM_ROLE_SLAVE)) {
-    bta_sys_set_policy(BTA_ID_AV, HCI_ENABLE_MASTER_SLAVE_SWITCH, peer_addr);
+    bta_sys_set_policy(BTA_ID_AV, set_policy, peer_addr);
   }
 
   /* if BTA_AvOpen() was called for other device, which caused the role switch
