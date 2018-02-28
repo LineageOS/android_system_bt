8e521034ded85c26152a18dd591192df3b80d958
Ajay Panicker <apanicke@google.com>
2018-02-27 20:51:47 -0800

    Add the AVRCP Connection Handler
    This class handles connecting and disconnecting AVRCP Devices as well as
    SDP lookups and feature selection.
    
    Bug: 68854188
    Test: run_host_unit_tests.py && run_unit_tests.sh net_test_avrcp
    Change-Id: I950587123d2875c4333b415c379c7c1fb0c29ca5

--- profile/avrcp/device.h
+++ profile/avrcp/device.h
@@ -36,6 +36,9 @@ namespace avrcp {
  * A class representing a connection with a remote AVRCP device. It holds all
  * the state and message handling for the device that it represents.
  */
+// TODO (apanicke): Once we move over to having the individual message
+// responders for Browse and Classic AVRCP Messages move the device around via a
+// weak pointer.
 class Device {
  public:
   /**
@@ -46,7 +49,7 @@ class Device {
 
   Device(
       const RawAddress& bdaddr, bool avrcp13_compatibility,
-      base::Callback<bool(uint8_t label, bool browse,
+      base::Callback<void(uint8_t label, bool browse,
                           std::unique_ptr<::bluetooth::PacketBuilder> message)>
           send_msg_cb);
   virtual ~Device() = default;
@@ -254,7 +257,7 @@ class Device {
   // such as browsing and playlists but has the highest chance of working.
   bool avrcp13_compatibility_ = false;
 
-  base::Callback<bool(uint8_t label, bool browse,
+  base::Callback<void(uint8_t label, bool browse,
                       std::unique_ptr<::bluetooth::PacketBuilder> message)>
       send_message_cb_;
 
@@ -262,6 +265,7 @@ class Device {
   std::set<uint8_t> active_labels_;
 
   int8_t volume_ = -1;
+  DISALLOW_COPY_AND_ASSIGN(Device);
 };
 
 }  // namespace avrcp
