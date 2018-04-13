046aee14539a173019f47dc4245f9c91f5309b90
Subramanian Srinivasan <subrsrin@codeaurora.org>
2018-04-12 17:19:34 -0700

    Add BLE Scan Phy parameter to scan API
    Add BLE Scan Phy parameter to set scan
    parameters API.
    
    CRs-Fixed: 2237643
    Change-Id: I6a21ff903aebccf559982e8ba1c4286db4c4c4ee

--- main/shim/le_scanning_manager.cc
+++ main/shim/le_scanning_manager.cc
@@ -159,12 +159,13 @@ class BleScannerInterfaceImpl : public BleScannerInterface,
   }
 
   /** Sets the LE scan interval and window in units of N*0.625 msec */
-  void SetScanParameters(int scan_interval, int scan_window, Callback cb) {
+  void SetScanParameters(int scan_phy, std::vector<uint32_t> scan_interval,
+                         std::vector<uint32_t> scan_window,
+                         Callback cb) {
     LOG(INFO) << __func__ << " in shim layer";
     // use active scan
     auto scan_type = static_cast<bluetooth::hci::LeScanType>(0x01);
-    bluetooth::shim::GetScanning()->SetScanParameters(scan_type, scan_interval,
-                                                      scan_window);
+    bluetooth::shim::GetScanning()->SetScanParameters(scan_type, scan_interval[0], scan_window[0]);
     do_in_jni_thread(FROM_HERE, base::Bind(cb, 0));
   }
 
