d473dd056879f4e651740b1883281508a01aacd2
Ajay Panicker <apanicke@google.com>
2018-04-13 11:49:34 -0700

    AVRCP: Inform upper layer if absolute volume isn't supported
    Before, the upper layers would be informed when a device connected that
    supported absolute volume. Now even devices that don't support absolute
    volume get reported.
    
    Bug: 33962015
    Test: Run host native test net-test-avrcp
    Change-Id: If083ccbb9ba9a170c5bca359e632585d826c6fa5
    (cherry picked from commit ca050dac60cef5f5eaffaeca9f7913fa00f77a0c)

--- profile/avrcp/connection_handler.cc
+++ profile/avrcp/connection_handler.cc
@@ -42,7 +42,8 @@ ConnectionHandler* ConnectionHandler::Get() {
 }
 
 bool ConnectionHandler::Initialize(const ConnectionCallback& callback,
-                                   AvrcpInterface* avrcp, SdpInterface* sdp) {
+                                   AvrcpInterface* avrcp, SdpInterface* sdp,
+                                   VolumeInterface* vol) {
   CHECK(instance_ == nullptr);
   CHECK(avrcp != nullptr);
   CHECK(sdp != nullptr);
@@ -53,6 +54,7 @@ bool ConnectionHandler::Initialize(const ConnectionCallback& callback,
   instance_->connection_cb_ = callback;
   instance_->avrc_ = avrcp;
   instance_->sdp_ = sdp;
+  instance_->vol_ = vol;
 
   // Set up the AVRCP acceptor connection
   if (!instance_->AvrcpConnect(false, RawAddress::kAny)) {
@@ -232,7 +234,10 @@ void ConnectionHandler::InitiatorControlCb(uint8_t handle, uint8_t event,
 
       if (feature_iter->second & BTA_AV_FEAT_ADV_CTRL) {
         newDevice->RegisterVolumeChanged();
+      } else if (instance_->vol_ != nullptr) {
+        instance_->vol_->DeviceConnected(newDevice->GetAddress());
       }
+
     } break;
 
     case AVRC_CLOSE_IND_EVT: {
@@ -308,6 +313,8 @@ void ConnectionHandler::AcceptorControlCb(uint8_t handle, uint8_t event,
         // connected that doesn't support absolute volume.
         if (features & BTA_AV_FEAT_ADV_CTRL) {
           device->RegisterVolumeChanged();
+        } else if (instance_->vol_ != nullptr) {
+          instance_->vol_->DeviceConnected(device->GetAddress());
         }
       };
 
