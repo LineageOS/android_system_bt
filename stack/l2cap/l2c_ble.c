4f2c83d2eceda3152edd8a665352be83ce7571c2
Ajay Panicker <apanicke@google.com>
2018-04-12 17:03:09 -0700

    Add bounds check to l2cble_process_sig_cmd L2CAP_CMD_DISC_REQ
    Bug: 74121659
    Test: Compiles
    Change-Id: Idf58e7b25b41ae1bd43cdd51de424b18e03cc7e8

--- stack/l2cap/l2c_ble.c
+++ stack/l2cap/l2c_ble.c
@@ -30,6 +30,7 @@
 #include "btu.h"
 #include "btm_int.h"
 #include "hcimsgs.h"
+#include "log/log.h"
 #include "device/include/controller.h"
 
 #if (BLE_INCLUDED == TRUE)
@@ -1233,6 +1234,10 @@ void l2cble_process_sig_cmd (tL2C_LCB *p_lcb, UINT8 *p, UINT16 pkt_len)
             }
             break;
         case L2CAP_CMD_DISC_REQ:
+            if (p + 4 > p_pkt_end) {
+                android_errorWriteLog(0x534e4554, "74121659");
+                return;
+            }
             STREAM_TO_UINT16 (lcid, p);
             STREAM_TO_UINT16 (rcid, p);
 
