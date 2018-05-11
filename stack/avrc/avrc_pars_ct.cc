0061dd6ae30ebcebce695c212c8bc0ceb276710e
Hansong Zhang <hsz@google.com>
2018-05-11 11:36:29 -0700

    DO NOT MERGE AVRC: Add bound check for AVRC_EVT_APP_SETTING_CHANGE
    Test: manual
    Bug: 73782082
    Change-Id: I4e384a2f8c0d8c4af03bd5865b2e907321419c86

--- stack/avrc/avrc_pars_ct.cc
+++ stack/avrc/avrc_pars_ct.cc
@@ -119,6 +119,10 @@ void avrc_parse_notification_rsp(uint8_t* p_stream,
 
     case AVRC_EVT_APP_SETTING_CHANGE:
       BE_STREAM_TO_UINT8(p_rsp->param.player_setting.num_attr, p_stream);
+      if (p_rsp->param.player_setting.num_attr > AVRC_MAX_APP_SETTINGS) {
+        android_errorWriteLog(0x534e4554, "73782082");
+        p_rsp->param.player_setting.num_attr = AVRC_MAX_APP_SETTINGS;
+      }
       for (int index = 0; index < p_rsp->param.player_setting.num_attr;
            index++) {
         BE_STREAM_TO_UINT8(p_rsp->param.player_setting.attr_id[index],
