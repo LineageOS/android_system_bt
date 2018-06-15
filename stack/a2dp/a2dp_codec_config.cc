ef3c0a46f326daf3fa8192ffd1bc7c12f7219b80
Gurpreet Ghai <gghai@codeaurora.org>
2018-06-15 15:36:05 +0530

    BT: Adding APTX-Adaptive codec to BT HAL
    Adding APTX-Adaptive codec entry to codec index.
    
    CRs-Fixed: 2280216
    
    Change-Id: I73ab0792a353b546338b2134d946ec500f7308f2

--- stack/a2dp/a2dp_codec_config.cc
+++ stack/a2dp/a2dp_codec_config.cc
@@ -133,6 +133,8 @@ A2dpCodecConfig* A2dpCodecConfig::createCodec(
     case BTAV_A2DP_CODEC_INDEX_SOURCE_APTX_HD:
       codec_config = new A2dpCodecConfigAptxHd(codec_priority);
       break;
+    case BTAV_A2DP_CODEC_INDEX_SOURCE_APTX_ADAPTIVE:
+      break;
     case BTAV_A2DP_CODEC_INDEX_SOURCE_LDAC:
       codec_config = new A2dpCodecConfigLdacSource(codec_priority);
       break;
