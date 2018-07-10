AgentFabulous/android_oreo_bacon

commit 645ee14ce2327fff61f54d208763eca1c143063e

Prevent multiple register of service change indication
Use Case:
Pair DUT to a remote LE device. The host registers for the service
change indication twice, once when the connection callback is
received and another time, when the SMP process completes

Failure:
There are a few issues caused by this.
First, the duplicate action of service change indication registeration
is unnecessary.
The registeration that follows the SMP process adds a gatt_if to the
apps holding the current link. This gatt_if (=1) never disconnects
after registeration failure or success. And hence the host can never
physically disconnect the link.

Fix:
This change removes the service change registeration at SMP
process completion

Test: manual
Change-Id: I0d7566e2b4a9d01aa7926cdbe9f528c3941fcfeb

bta/dm/bta_dm_act.cc
@@ -4413,8 +4413,6 @@ static uint8_t bta_dm_ble_smp_cback (tBTM_LE_EVT event, BD_ADDR bda, tBTM_LE_EVT
             else
             {
                 sec_event.auth_cmpl.success = true;
-                if (!p_data->complt.smp_over_br)
-                    GATT_ConfigServiceChangeCCC(bda, true, BT_TRANSPORT_LE);
             }
 
             if (bta_dm_cb.p_sec_cback)

LineageOS/android_system_bt

commit dc46396273b38ab637e7d82ec7306f4dc871d6c1

Register for Service Changed notification after encryption
This fixes issue introduced in commit 645ee14ce2327fff61f54d208763eca1c143063e
Registration for Service Changed notifications was
disabled, because there was no good way back then to have
"opportunistic" app. Since opportunistic apps were introduced, and the
code in GATT_ConfigServiceChangeCCC is already marking connection as
opportunistic, reenable notification registration.

Test: manual verification, that Service Changed descriptor is set
Change-Id: Iefaec3914829fa4a4a60dc3c3790c3daa9f4f4d8

bta/dm/bta_dm_act.cc
@@ -3849,6 +3849,8 @@ static uint8_t bta_dm_ble_smp_cback(tBTM_LE_EVT event, const RawAddress& bda,
         bta_dm_remove_sec_dev_entry(bda);
       } else {
         sec_event.auth_cmpl.success = true;
+        if (!p_data->complt.smp_over_br)
+          GATT_ConfigServiceChangeCCC(bda, true, BT_TRANSPORT_LE);
       }
 
       if (bta_dm_cb.p_sec_cback) {