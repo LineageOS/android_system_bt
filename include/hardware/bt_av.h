e1f2e78e348e6bd8045f702f4330e4235c7e4a7d
pramod kotreshappa <kpramod@codeaurora.org>
2018-10-04 03:14:41 -0700

    TWS-A2DP: Add TWS+ codec index
    Change-Id: I6f84643e152cfe9d9c2603e1fa7b257aca9d772a
    CRs-Fixed: 2318635

--- include/hardware/bt_av.h
+++ include/hardware/bt_av.h
@@ -55,6 +55,7 @@ typedef enum {
   BTAV_A2DP_CODEC_INDEX_SOURCE_APTX_HD,
   BTAV_A2DP_CODEC_INDEX_SOURCE_APTX_ADAPTIVE,
   BTAV_A2DP_CODEC_INDEX_SOURCE_LDAC,
+  BTAV_A2DP_CODEC_INDEX_SOURCE_APTX_TWS,
 
   BTAV_A2DP_CODEC_INDEX_SOURCE_MAX,
 
@@ -152,6 +153,9 @@ typedef struct {
         break;
       case BTAV_A2DP_CODEC_INDEX_SOURCE_LDAC:
         codec_name_str = "LDAC";
+        break;
+      case BTAV_A2DP_CODEC_INDEX_SOURCE_APTX_TWS:
+        codec_name_str = "aptX TWS";
         break;
       case BTAV_A2DP_CODEC_INDEX_SINK_SBC:
         codec_name_str = "SBC (Sink)";
