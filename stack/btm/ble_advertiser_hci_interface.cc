c74471dcacf7e2880a1e81236ab0054eaab01dbd
Jakub Pawlowski <jpawlowski@google.com>
2018-10-30 18:29:36 +0100

    HCI: accept OnceCallback instad of Callback
    Command complete is guaranteed to happen only once by HCI spec.
    
    Test: run sl4a tests around advertising, which use callback
    Change-Id: I3c40ed21c56b41649bbc9519d280339b1cbb285f

--- stack/btm/ble_advertiser_hci_interface.cc
+++ stack/btm/ble_advertiser_hci_interface.cc
@@ -45,8 +45,8 @@
 
 using status_cb = BleAdvertiserHciInterface::status_cb;
 
-using hci_cmd_cb = base::Callback<void(uint8_t* /* return_parameters */,
-                                       uint16_t /* return_parameters_length*/)>;
+using hci_cmd_cb = base::OnceCallback<void(
+    uint8_t* /* return_parameters */, uint16_t /* return_parameters_length*/)>;
 extern void btu_hcif_send_cmd_with_cb(const base::Location& posted_from,
                                       uint16_t opcode, uint8_t* params,
                                       uint8_t params_len, hci_cmd_cb cb);
