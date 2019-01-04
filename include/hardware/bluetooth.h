238f8359a9f3367151241c707596a6989fc5613b
Jack He <siyuanh@google.com>
2019-01-03 17:11:24 -0800

    Remove code related to Health Device Profile and MCAP Protocol
    * Health Device Profile (HDP) and MCAP protocol has been largely
      replaced by BLE. New applications should use Bluetooth Low Energy
      instead of legacy Bluetooth Health Device Profile
    
    Bug: 111562841
    Test: make, unit test, use Bluetooth
    Change-Id: I3da1173a8a928af4b26a29e71746241c22af2236
    Merged-In: I3da1173a8a928af4b26a29e71746241c22af2236
    (cherry picked from commit dc13609b58b2f6c6185951f650036f5098e19e92)

--- include/hardware/bluetooth.h
+++ include/hardware/bluetooth.h
@@ -38,7 +38,6 @@
 #define BT_PROFILE_HANDSFREE_CLIENT_ID "handsfree_client"
 #define BT_PROFILE_ADVANCED_AUDIO_ID "a2dp"
 #define BT_PROFILE_ADVANCED_AUDIO_SINK_ID "a2dp_sink"
-#define BT_PROFILE_HEALTH_ID "health"
 #define BT_PROFILE_SOCKETS_ID "socket"
 #define BT_PROFILE_HIDHOST_ID "hidhost"
 #define BT_PROFILE_HIDDEV_ID "hiddev"
@@ -49,9 +48,6 @@
 #define BT_PROFILE_AV_RC_ID "avrcp"
 #define BT_PROFILE_AV_RC_CTRL_ID "avrcp_ctrl"
 #define BT_PROFILE_HEARING_AID_ID "hearing_aid"
-
-/** Bluetooth test interface IDs */
-#define BT_TEST_INTERFACE_MCAP_ID "mcap_test"
 
 /** Bluetooth Device Name */
 typedef struct { uint8_t name[249]; } __attribute__((packed)) bt_bdname_t;
