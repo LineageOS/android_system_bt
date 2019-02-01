e0f1e9a32ce5d013aea911eabfc574093e40a7c0
Martin Brabham <optedoblivion@google.com>
2019-02-01 13:42:58 -0800

    [DO NOT MERGE] Implement key attestation using AndroidKeystore.
    Store SHA256 hash of the config file in an encrypted file that has been encrypted using the Android Keystore API.
    
    Bug: b/117993149
    Test: Manual
    Change-Id: I26de9ea05f515d6643a83d11628490fb49e10743
    Merged-In: I26de9ea05f515d6643a83d11628490fb49e10743

---
+++ btif/include/btif_keystore.h
@@ -0,0 +1,39 @@
+/******************************************************************************
+ *
+ *  Copyright 2019 Google, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at:
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+#include <base/logging.h>
+#include <keystore/keystore_client_impl.h>
+
+#include "osi/include/alarm.h"
+#include "osi/include/allocator.h"
+#include "osi/include/compat.h"
+#include "osi/include/config.h"
+#include "osi/include/log.h"
+#include "osi/include/osi.h"
+#include "osi/include/properties.h"
+
+using namespace keystore;
+
+class BtifKeystore {
+ public:
+  BtifKeystore();
+  ~BtifKeystore();
+  int Encrypt(const std::string& hash, const std::string& output_filename,
+              int32_t flags);
+  std::string Decrypt(const std::string& input_filename);
+};
