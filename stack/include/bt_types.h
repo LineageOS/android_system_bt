5ae23a59e89039becccc60934a3997a996134b32
Jakub Pawlowski <jpawlowski@google.com>
2019-03-11 19:22:01 +0100

    DO NOT MERGE Don't persist bonds using sample LTK
    Test: compilation, manual testing
    Bug: 128437297
    Change-Id: I52fd484d42bf87e96dbc9e6456090f231ed48111

--- stack/include/bt_types.h
+++ stack/include/bt_types.h
@@ -576,6 +576,14 @@ typedef std::array<uint8_t, OCTET16_LEN> Octet16;
 constexpr int LINK_KEY_LEN = OCTET16_LEN;
 typedef Octet16 LinkKey; /* Link Key */
 
+/* Sample LTK from BT Spec 5.1 | Vol 6, Part C 1
+ * 0x4C68384139F574D836BCF34E9DFB01BF */
+constexpr Octet16 SAMPLE_LTK = {0xbf, 0x01, 0xfb, 0x9d, 0x4e, 0xf3, 0xbc, 0x36,
+                                0xd8, 0x74, 0xf5, 0x39, 0x41, 0x38, 0x68, 0x4c};
+inline bool is_sample_ltk(const Octet16& ltk) {
+  return ltk == SAMPLE_LTK;
+}
+
 #endif
 
 #define PIN_CODE_LEN 16
