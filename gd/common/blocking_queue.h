9c2f4d0dbcb48cdbce1829d66f10b0b47b51c295
Chienyuan <chienyuanhuang@google.com>
2019-04-26 11:59:35 -0700

    Implement blocking queue
    Test: run unittest
    Change-Id: I93c59a2d8c9bbe9bee762412b368773c4b35456f

---
+++ gd/common/blocking_queue.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+namespace bluetooth {
+namespace common {
+
+template <typename T>
+class BlockingQueue {
+ public:
+  void push(T data) {
+    std::unique_lock<std::mutex> lock(mutex_);
+    queue_.push(std::move(data));
+    if (queue_.size() == 1) {
+      not_empty_.notify_all();
+    }
+  };
+
+  const T& take() {
+    std::unique_lock<std::mutex> lock(mutex_);
+    while (queue_.empty()) {
+      not_empty_.wait(lock);
+    }
+    const T& data = queue_.front();
+    queue_.pop();
+    return data;
+  };
+
+  bool empty() const {
+    std::unique_lock<std::mutex> lock(mutex_);
+    return queue_.empty();
+  };
+
+  void clear() {
+    std::unique_lock<std::mutex> lock(mutex_);
+    std::queue<T> empty;
+    std::swap(queue_, empty);
+  };
+
+ private:
+  std::queue<T> queue_;
+  mutable std::mutex mutex_;
+  std::condition_variable not_empty_;
+};
+
+}  // namespace common
+}  // namespace bluetooth
