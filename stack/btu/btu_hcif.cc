461f0b4739967d60a3c6ee2b03ad86fc5abb2e99
Jakub Pawlowski <jpawlowski@google.com>
2019-06-06 13:54:55 +0200

    DO NOT MERGE Fix for Bluetooth connection being dropped after HCI Read Encryption Key Size
    If remote device stop the encryption before we call "Read Encryption Key Size",
    we might receive Insufficient Security, which means that link is no longer
    encrypted.
    
    In such cases we should stay connected, rather than disconnecting the
    link.
    
    Test: Connect to device that stop encryption right after encryption is
    complete, i.e. to change roles.
    Bug: 124301137
    Bug: 132626699
    
    Change-Id: Iab0fd9f357d18a6b048b971d0393fbb47fd4d793
    Merged-In: Iab0fd9f357d18a6b048b971d0393fbb47fd4d793

--- stack/btu/btu_hcif.cc
+++ stack/btu/btu_hcif.cc
@@ -719,6 +719,15 @@ constexpr uint8_t MIN_KEY_SIZE = 7;
 
 static void read_encryption_key_size_complete_after_encryption_change(
     uint8_t status, uint16_t handle, uint8_t key_size) {
+  if (status == HCI_ERR_INSUFFCIENT_SECURITY) {
+    /* If remote device stop the encryption before we call "Read Encryption Key
+     * Size", we might receive Insufficient Security, which means that link is
+     * no longer encrypted. */
+    HCI_TRACE_WARNING("%s encryption stopped on link: 0x%02x", __func__,
+                      handle);
+    return;
+  }
+
   if (status != HCI_SUCCESS) {
     HCI_TRACE_WARNING("%s: disconnecting, status: 0x%02x", __func__, status);
     btsnd_hcic_disconnect(handle, HCI_ERR_PEER_USER);
@@ -1679,6 +1688,15 @@ static void btu_hcif_enhanced_flush_complete_evt(void) {
 
 static void read_encryption_key_size_complete_after_key_refresh(
     uint8_t status, uint16_t handle, uint8_t key_size) {
+  if (status == HCI_ERR_INSUFFCIENT_SECURITY) {
+    /* If remote device stop the encryption before we call "Read Encryption Key
+     * Size", we might receive Insufficient Security, which means that link is
+     * no longer encrypted. */
+    HCI_TRACE_WARNING("%s encryption stopped on link: 0x%02x", __func__,
+                      handle);
+    return;
+  }
+
   if (status != HCI_SUCCESS) {
     HCI_TRACE_WARNING("%s: disconnecting, status: 0x%02x", __func__, status);
     btsnd_hcic_disconnect(handle, HCI_ERR_PEER_USER);
