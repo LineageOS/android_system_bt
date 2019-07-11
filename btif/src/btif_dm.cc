098e2972201b454de0f95a72636ecd9009dc32de
Ugo Yu <ugoyu@google.com>
2019-07-11 20:12:42 +0800

    DO NOT MERGE Store BLE keys using the address from the ble_auth_cmpl_evt
    Reading the peer address from btif_dm_ble_auth_cmpl_evt, instead
    of using the value from the pairing control block in
    btif_dm_save_ble_bonding_keys, ensures that BLE keys are stored with
    the correct address.
    
    Bug: 133234174
    Bug: 79703832
    Test: 1. Initiate crosskey pairing from BLE
          2. Check whether BLE keys are stored correctly
    Change-Id: I18b4a1d8e2cdcd6dd4a300f1dc9e6d3892a3baff
    Merged-In: I18b4a1d8e2cdcd6dd4a300f1dc9e6d3892a3baff
    (cherry picked from commit 0d95651e8b22b1012f1ee103e4a0b8665a0c17d4)

--- btif/src/btif_dm.cc
+++ btif/src/btif_dm.cc
@@ -2918,7 +2918,7 @@ static void btif_dm_ble_auth_cmpl_evt(tBTA_DM_AUTH_CMPL* p_auth_cmpl) {
       btif_storage_remove_bonded_device(&bdaddr);
       state = BT_BOND_STATE_NONE;
     } else {
-      btif_dm_save_ble_bonding_keys();
+      btif_dm_save_ble_bonding_keys(bdaddr);
       BTA_GATTC_Refresh(bd_addr.address);
       btif_dm_get_remote_services_by_transport(&bd_addr, BTA_GATT_TRANSPORT_LE);
     }
@@ -2992,12 +2992,8 @@ void btif_dm_get_ble_local_keys(tBTA_DM_BLE_LOCAL_KEY_MASK* p_key_mask,
   BTIF_TRACE_DEBUG("%s  *p_key_mask=0x%02x", __func__, *p_key_mask);
 }
 
-void btif_dm_save_ble_bonding_keys(void) {
-  bt_bdaddr_t bd_addr;
-
+void btif_dm_save_ble_bonding_keys(bt_bdaddr_t bd_addr) {
   BTIF_TRACE_DEBUG("%s", __func__);
-
-  bdcpy(bd_addr.address, pairing_cb.bd_addr);
 
   if (pairing_cb.ble.is_penc_key_rcvd) {
     btif_storage_add_ble_bonding_key(&bd_addr, (char*)&pairing_cb.ble.penc_key,
