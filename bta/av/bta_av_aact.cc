b0401e9180fd53890c6d26df713f70e5f40d8e99
Cheney Ni <cheneyni@google.com>
2019-07-16 20:43:49 +0800

    AVDTP: Make sure the UUID of the SCB is initialized before discovering
    When parsing remote SEP capabilities, we check the UUID value in the AV
    SCB to choose the SEP, and this parameter was assigned when AVDTP was
    reconnecting from the phone side. This change also assigns the UUID to
    make sure the stack can find remote's SEPs correctly when reconnected
    by the remote.
    
    Bug: 137631786
    Test: make AVDTP reconnection from the peer side
    Change-Id: Ib2fa6e76de31bab1131c024e9f76433f3fd8b60a

--- bta/av/bta_av_aact.cc
+++ bta/av/bta_av_aact.cc
@@ -1167,6 +1167,7 @@ void bta_av_setconfig_rsp(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
     } else {
       /* we do not know the peer device and it is using non-SBC codec
        * we need to know all the SEPs on SNK */
+      if (p_scb->uuid_int == 0) p_scb->uuid_int = p_scb->open_api.uuid;
       bta_av_discover_req(p_scb, NULL);
       return;
     }
