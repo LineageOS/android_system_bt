5e078b3bb1b3ec89879be6a1e657c21c6f929b73
Łukasz Rymanowski <lukasz.rymanowski@codecoup.pl>
2019-07-30 13:59:14 +0200

    bluetooth/hci: Extend packet fragmenter for ISO
    Bug: 150670922
    Tag: #feature
    Test: atest net_test_hci
    Sponsor: jpawlowski
    Change-Id: I25fb0624e1c7b1f60ef09680304fc69af9a88a6d

--- hci/include/hci_internals.h
+++ hci/include/hci_internals.h
@@ -26,5 +26,33 @@
 #define HCI_SCO_PREAMBLE_SIZE 3
 // 1 byte for event code, 1 byte for parameter length (Volume 2, Part E, 5.4.4)
 #define HCI_EVENT_PREAMBLE_SIZE 2
+
+// ISO
 // 2 bytes for handle, 2 bytes for data length (Volume 2, Part E, 5.4.5)
 #define HCI_ISO_PREAMBLE_SIZE 4
+
+#define HCI_ISO_BF_FIRST_FRAGMENTED_PACKET (0)
+#define HCI_ISO_BF_CONTINUATION_FRAGMENT_PACKET (1)
+#define HCI_ISO_BF_COMPLETE_PACKET (2)
+#define HCI_ISO_BF_LAST_FRAGMENT_PACKET (3)
+
+#define HCI_ISO_HEADER_TIMESTAMP_SIZE (4)
+#define HCI_ISO_HEADER_ISO_LEN_SIZE (2)
+#define HCI_ISO_HEADER_PACKET_SEQ_SIZE (2)
+
+#define HCI_ISO_HEADER_LEN_WITHOUT_TS \
+  (HCI_ISO_HEADER_ISO_LEN_SIZE + HCI_ISO_HEADER_PACKET_SEQ_SIZE)
+#define HCI_ISO_HEADER_LEN_WITH_TS \
+  (HCI_ISO_HEADER_LEN_WITHOUT_TS + HCI_ISO_HEADER_TIMESTAMP_SIZE)
+
+#define HCI_ISO_SET_CONTINUATION_FLAG(handle) \
+  (((handle)&0x4FFF) | (0x0001 << 12))
+#define HCI_ISO_SET_COMPLETE_FLAG(handle) (((handle)&0x4FFF) | (0x0002 << 12))
+#define HCI_ISO_SET_END_FRAG_FLAG(handle) (((handle)&0x4FFF) | (0x0003 << 12))
+#define HCI_ISO_SET_TIMESTAMP_FLAG(handle) (((handle)&0x3FFF) | (0x0001 << 14))
+
+#define HCI_ISO_GET_TS_FLAG(handle) (((handle) >> 14) & 0x0001)
+#define HCI_ISO_GET_PACKET_STATUS_FLAGS(iso_sdu_length) \
+  (iso_sdu_length & 0xC000)
+
+#define HCI_ISO_SDU_LENGTH_MASK 0x0FFF
