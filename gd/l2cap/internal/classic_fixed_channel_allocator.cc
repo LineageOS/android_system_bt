6a9b4665f78b71d83742b0b92be87db244676f57
Jack He <siyuanh@google.com>
2019-08-20 22:07:26 -0700

    L2CAP: Classic fixed channel reference counting
    * Add a mechanism to disconnect ACL after a link is idle for kClassicLinkIdleDisconnectTimeout
      seconds. The current setting is 20.
    * Add Acquire() and Release() method to ClassicFixedChannel for user to
      indicate if a fixed channel should hold the current ACL link or should
      it be oportunistic
    * Implemented methods to register for disconnection callbacks
    * Add tests to verify fixed channel acquire/release and register
      disconnction callback methods
    * Add tests to verify classic link idle timeout disconnect methods
    
    Bug: 138261142
    Test: bluetooth_test_gd
    Change-Id: I78129afb1b988311c56019f87cbb3f78e9a08cc5

--- gd/l2cap/internal/classic_fixed_channel_allocator.cc
+++ gd/l2cap/internal/classic_fixed_channel_allocator.cc
@@ -19,6 +19,7 @@
 #include "classic_fixed_channel_allocator.h"
 #include "l2cap/cid.h"
 #include "l2cap/internal/classic_fixed_channel_allocator.h"
+#include "l2cap/internal/classic_link.h"
 #include "l2cap/security_policy.h"
 #include "os/handler.h"
 #include "os/log.h"
@@ -27,30 +28,54 @@ namespace bluetooth {
 namespace l2cap {
 namespace internal {
 
-ClassicFixedChannelImpl* ClassicFixedChannelAllocator::AllocateChannel(Cid cid, SecurityPolicy security_policy) {
-  ASSERT_LOG(!IsChannelInUse((cid)), "Cid %d is already in use", cid);
+std::shared_ptr<ClassicFixedChannelImpl> ClassicFixedChannelAllocator::AllocateChannel(Cid cid,
+                                                                                       SecurityPolicy security_policy) {
+  ASSERT_LOG(!IsChannelAllocated((cid)), "Cid 0x%x for device %s is already in use", cid,
+             link_->GetDevice().ToString().c_str());
   ASSERT_LOG(cid >= kFirstFixedChannel && cid <= kLastFixedChannel, "Cid %d out of bound", cid);
-  channels_.try_emplace(cid, cid, handler_);
-  return &channels_.find(cid)->second;
+  auto elem = channels_.try_emplace(cid, std::make_shared<ClassicFixedChannelImpl>(cid, link_, l2cap_handler_));
+  ASSERT_LOG(elem.second, "Failed to create channel for cid 0x%x device %s", cid,
+             link_->GetDevice().ToString().c_str());
+  ASSERT(elem.first->second != nullptr);
+  return elem.first->second;
 }
 
-bool ClassicFixedChannelAllocator::FreeChannel(Cid cid) {
-  ASSERT_LOG(IsChannelInUse(cid), "Channel is not in use: cid %d", cid);
+void ClassicFixedChannelAllocator::FreeChannel(Cid cid) {
+  ASSERT_LOG(IsChannelAllocated(cid), "Channel is not in use: cid %d, device %s", cid,
+             link_->GetDevice().ToString().c_str());
   channels_.erase(cid);
-  return true;
 }
 
-bool ClassicFixedChannelAllocator::IsChannelInUse(Cid cid) const {
+bool ClassicFixedChannelAllocator::IsChannelAllocated(Cid cid) const {
   return channels_.find(cid) != channels_.end();
 }
 
-ClassicFixedChannelImpl* ClassicFixedChannelAllocator::FindChannel(Cid cid) {
-  ASSERT_LOG(IsChannelInUse(cid), "Channel is not in use: cid %d", cid);
-  return &channels_.find(cid)->second;
+std::shared_ptr<ClassicFixedChannelImpl> ClassicFixedChannelAllocator::FindChannel(Cid cid) {
+  ASSERT_LOG(IsChannelAllocated(cid), "Channel is not in use: cid %d, device %s", cid,
+             link_->GetDevice().ToString().c_str());
+  return channels_.find(cid)->second;
 }
+
 size_t ClassicFixedChannelAllocator::NumberOfChannels() const {
   return channels_.size();
 }
+
+void ClassicFixedChannelAllocator::OnAclDisconnected(hci::ErrorCode reason) {
+  for (auto& elem : channels_) {
+    elem.second->OnClosed(reason);
+  }
+}
+
+int ClassicFixedChannelAllocator::GetRefCount() {
+  int ref_count = 0;
+  for (auto& elem : channels_) {
+    if (elem.second->IsAcquired()) {
+      ref_count++;
+    }
+  }
+  return ref_count;
+}
+
 }  // namespace internal
 }  // namespace l2cap
 }  // namespace bluetooth
