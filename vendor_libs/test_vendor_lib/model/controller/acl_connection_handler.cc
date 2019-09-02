967235304e9c96756b8dd7b90f554d8d69680977
Myles Watson <mylesgw@google.com>
2019-09-02 14:52:46 -0700

    RootCanal: Add LE connection packets
    - Add an event stream to the cert device
    - Add LinkLayer packets for LeConnections
    - Update AclConnection to track address types for LE
    - Fix serialization of LE meta events
    
    Bug: 138260116
    Test: cert/run_cert.sh
    Change-Id: I838ab86bb1b9cd45229126b06efceb017ccd4559

--- vendor_libs/test_vendor_lib/model/controller/acl_connection_handler.cc
+++ vendor_libs/test_vendor_lib/model/controller/acl_connection_handler.cc
@@ -35,40 +35,82 @@ bool AclConnectionHandler::HasHandle(uint16_t handle) const {
 }
 
 uint16_t AclConnectionHandler::GetUnusedHandle() {
-  static uint16_t sNextHandle = acl::kReservedHandle - 2;
-  while (acl_connections_.count(sNextHandle) == 1) {
-    sNextHandle = (sNextHandle + 1) % acl::kReservedHandle;
+  while (acl_connections_.count(last_handle_) == 1) {
+    last_handle_ = (last_handle_ + 1) % acl::kReservedHandle;
   }
-  uint16_t unused_handle = sNextHandle;
-  sNextHandle = (sNextHandle + 1) % acl::kReservedHandle;
+  uint16_t unused_handle = last_handle_;
+  last_handle_ = (last_handle_ + 1) % acl::kReservedHandle;
   return unused_handle;
 }
 
-bool AclConnectionHandler::CreatePendingConnection(const Address& addr) {
-  if ((pending_connections_.size() + 1 > max_pending_connections_) || HasPendingConnection(addr)) {
+bool AclConnectionHandler::CreatePendingConnection(Address addr) {
+  if (classic_connection_pending_) {
     return false;
   }
-  pending_connections_.insert(addr);
+  classic_connection_pending_ = true;
+  pending_connection_address_ = addr;
   return true;
 }
 
-bool AclConnectionHandler::HasPendingConnection(const Address& addr) {
-  return pending_connections_.count(addr) == 1;
+bool AclConnectionHandler::HasPendingConnection(Address addr) {
+  return classic_connection_pending_ && pending_connection_address_ == addr;
 }
 
-bool AclConnectionHandler::CancelPendingConnection(const Address& addr) {
-  if (!HasPendingConnection(addr)) {
+bool AclConnectionHandler::CancelPendingConnection(Address addr) {
+  if (!classic_connection_pending_ || pending_connection_address_ != addr) {
     return false;
   }
-  pending_connections_.erase(addr);
+  classic_connection_pending_ = false;
+  pending_connection_address_ = Address::kEmpty;
   return true;
 }
 
-uint16_t AclConnectionHandler::CreateConnection(const Address& addr) {
+bool AclConnectionHandler::CreatePendingLeConnection(Address addr, uint8_t address_type) {
+  if (IsDeviceConnected(addr, address_type)) {
+    LOG_INFO(LOG_TAG, "%s: %s (type %hhx) is already connected", __func__, addr.ToString().c_str(), address_type);
+    return false;
+  }
+  if (le_connection_pending_) {
+    LOG_INFO(LOG_TAG, "%s: connection already pending", __func__);
+    return false;
+  }
+  le_connection_pending_ = true;
+  pending_le_connection_address_ = addr;
+  pending_le_connection_address_type_ = address_type;
+  return true;
+}
+
+bool AclConnectionHandler::HasPendingLeConnection(Address addr, uint8_t address_type) {
+  return le_connection_pending_ && pending_le_connection_address_ == addr &&
+         pending_le_connection_address_type_ == address_type;
+}
+
+bool AclConnectionHandler::CancelPendingLeConnection(Address addr, uint8_t address_type) {
+  if (!le_connection_pending_ || pending_le_connection_address_ != addr ||
+      pending_le_connection_address_type_ != address_type) {
+    return false;
+  }
+  le_connection_pending_ = false;
+  pending_le_connection_address_ = Address::kEmpty;
+  pending_le_connection_address_type_ = 0xba;
+  return true;
+}
+
+uint16_t AclConnectionHandler::CreateConnection(Address addr) {
   if (CancelPendingConnection(addr)) {
     uint16_t handle = GetUnusedHandle();
     acl_connections_.emplace(handle, addr);
-    SetConnected(handle, true);
+    return handle;
+  }
+  return acl::kReservedHandle;
+}
+
+uint16_t AclConnectionHandler::CreateLeConnection(Address addr, uint8_t address_type, uint8_t own_address_type) {
+  if (CancelPendingLeConnection(addr, address_type)) {
+    uint16_t handle = GetUnusedHandle();
+    acl_connections_.emplace(handle, addr);
+    set_own_address_type(handle, own_address_type);
+    SetAddress(handle, addr, address_type);
     return handle;
   }
   return acl::kReservedHandle;
@@ -78,7 +120,7 @@ bool AclConnectionHandler::Disconnect(uint16_t handle) {
   return acl_connections_.erase(handle) > 0;
 }
 
-uint16_t AclConnectionHandler::GetHandle(const Address& addr) const {
+uint16_t AclConnectionHandler::GetHandle(Address addr) const {
   for (auto pair : acl_connections_) {
     if (std::get<AclConnection>(pair).GetAddress() == addr) {
       return std::get<0>(pair);
@@ -87,23 +129,41 @@ uint16_t AclConnectionHandler::GetHandle(const Address& addr) const {
   return acl::kReservedHandle;
 }
 
-const Address& AclConnectionHandler::GetAddress(uint16_t handle) const {
+Address AclConnectionHandler::GetAddress(uint16_t handle) const {
   CHECK(HasHandle(handle)) << "Handle unknown " << handle;
   return acl_connections_.at(handle).GetAddress();
 }
 
-void AclConnectionHandler::SetConnected(uint16_t handle, bool connected) {
-  if (!HasHandle(handle)) {
-    return;
-  }
-  acl_connections_.at(handle).SetConnected(connected);
+uint8_t AclConnectionHandler::GetAddressType(uint16_t handle) const {
+  CHECK(HasHandle(handle)) << "Handle unknown " << handle;
+  return acl_connections_.at(handle).GetAddressType();
+}
+
+void AclConnectionHandler::set_own_address_type(uint16_t handle, uint8_t address_type) {
+  CHECK(HasHandle(handle)) << "Handle unknown " << handle;
+  acl_connections_.at(handle).SetOwnAddressType(address_type);
+}
+
+uint8_t AclConnectionHandler::GetOwnAddressType(uint16_t handle) const {
+  CHECK(HasHandle(handle)) << "Handle unknown " << handle;
+  return acl_connections_.at(handle).GetOwnAddressType();
 }
 
 bool AclConnectionHandler::IsConnected(uint16_t handle) const {
   if (!HasHandle(handle)) {
     return false;
   }
-  return acl_connections_.at(handle).IsConnected();
+  return true;
+}
+
+bool AclConnectionHandler::IsDeviceConnected(Address addr, uint8_t address_type) const {
+  for (auto pair : acl_connections_) {
+    auto connection = std::get<AclConnection>(pair);
+    if (connection.GetAddress() == addr && connection.GetAddressType() == address_type) {
+      return true;
+    }
+  }
+  return false;
 }
 
 void AclConnectionHandler::Encrypt(uint16_t handle) {
@@ -120,11 +180,13 @@ bool AclConnectionHandler::IsEncrypted(uint16_t handle) const {
   return acl_connections_.at(handle).IsEncrypted();
 }
 
-void AclConnectionHandler::SetAddress(uint16_t handle, const Address& address) {
+void AclConnectionHandler::SetAddress(uint16_t handle, Address address, uint8_t address_type) {
   if (!HasHandle(handle)) {
     return;
   }
-  acl_connections_.at(handle).SetAddress(address);
+  auto connection = acl_connections_.at(handle);
+  connection.SetAddress(address);
+  connection.SetAddressType(address_type);
 }
 
 }  // namespace test_vendor_lib
