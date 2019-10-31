db55f31774621ef053cf5829d5cf603d6ed7fbcc
Chienyuan <chienyuanhuang@google.com>
2019-10-31 14:01:28 +0800

    RootCanal: Upgrade the LinkLayer in RootCanal to PDL
    Bug: 140384404
    Test: run_cert.sh
    Change-Id: I32f250c60cfc7aa32e4ccfdbbd408e1ee4e0a674

--- vendor_libs/test_vendor_lib/model/devices/remote_loopback_device.cc
+++ vendor_libs/test_vendor_lib/model/devices/remote_loopback_device.cc
@@ -18,16 +18,14 @@
 
 #include "model/setup/device_boutique.h"
 #include "os/log.h"
-#include "packets/link_layer/link_layer_packet_builder.h"
-#include "packets/link_layer/link_layer_packet_view.h"
 
 using std::vector;
 
 namespace test_vendor_lib {
 
-using packets::LinkLayerPacketBuilder;
-using packets::LinkLayerPacketView;
-using packets::PageResponseBuilder;
+using model::packets::LinkLayerPacketBuilder;
+using model::packets::LinkLayerPacketView;
+using model::packets::PageResponseBuilder;
 
 bool RemoteLoopbackDevice::registered_ = DeviceBoutique::Register("remote_loopback", &RemoteLoopbackDevice::Create);
 
@@ -44,27 +42,23 @@ void RemoteLoopbackDevice::Initialize(const std::vector<std::string>& args) {
   if (Address::FromString(args[1], addr)) properties_.SetAddress(addr);
 }
 
-void RemoteLoopbackDevice::IncomingPacket(LinkLayerPacketView packet) {
+void RemoteLoopbackDevice::IncomingPacket(
+    model::packets::LinkLayerPacketView packet) {
   // TODO: Check sender?
   // TODO: Handle other packet types
   Phy::Type phy_type = Phy::Type::BR_EDR;
 
-  Link::PacketType type = packet.GetType();
+  model::packets::PacketType type = packet.GetType();
   switch (type) {
-    case Link::PacketType::PAGE:
-      SendLinkLayerPacket(LinkLayerPacketBuilder::WrapPageResponse(
-                              PageResponseBuilder::Create(true), packet.GetSourceAddress(), packet.GetSourceAddress()),
-                          Phy::Type::BR_EDR);
+    case model::packets::PacketType::PAGE:
+      SendLinkLayerPacket(
+          PageResponseBuilder::Create(packet.GetSourceAddress(),
+                                      packet.GetSourceAddress(), true),
+          Phy::Type::BR_EDR);
       break;
     default: {
       LOG_WARN("Resend = %d", static_cast<int>(packet.size()));
-      std::shared_ptr<std::vector<uint8_t>> extracted_packet = std::make_shared<std::vector<uint8_t>>();
-      extracted_packet->reserve(packet.size());
-      for (const auto byte : packet) {
-        extracted_packet->push_back(byte);
-      }
-
-      SendLinkLayerPacket(LinkLayerPacketBuilder::ReWrap(extracted_packet), phy_type);
+      SendLinkLayerPacket(packet, phy_type);
     }
   }
 }
--- vendor_libs/test_vendor_lib/model/setup/phy_layer_factory.h
+++ vendor_libs/test_vendor_lib/model/setup/phy_layer_factory.h
@@ -20,8 +20,7 @@
 #include <vector>
 
 #include "include/phy.h"
-#include "packets/link_layer/link_layer_packet_builder.h"
-#include "packets/link_layer/link_layer_packet_view.h"
+#include "packets/link_layer_packets.h"
 #include "phy_layer.h"
 
 namespace test_vendor_lib {
@@ -38,7 +37,9 @@ class PhyLayerFactory {
 
   uint32_t GetFactoryId();
 
-  std::shared_ptr<PhyLayer> GetPhyLayer(const std::function<void(packets::LinkLayerPacketView)>& device_receive);
+  std::shared_ptr<PhyLayer> GetPhyLayer(
+      const std::function<void(model::packets::LinkLayerPacketView)>&
+          device_receive);
 
   void UnregisterPhyLayer(uint32_t id);
 
@@ -47,7 +48,10 @@ class PhyLayerFactory {
   virtual std::string ToString() const;
 
  protected:
-  virtual void Send(const std::shared_ptr<packets::LinkLayerPacketBuilder> packet, uint32_t id);
+  virtual void Send(
+      const std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet,
+      uint32_t id);
+  virtual void Send(model::packets::LinkLayerPacketView packet, uint32_t id);
 
  private:
   Phy::Type phy_type_;
@@ -58,12 +62,17 @@ class PhyLayerFactory {
 
 class PhyLayerImpl : public PhyLayer {
  public:
-  PhyLayerImpl(Phy::Type phy_type, uint32_t id, const std::function<void(packets::LinkLayerPacketView)>& device_receive,
+  PhyLayerImpl(Phy::Type phy_type, uint32_t id,
+               const std::function<void(model::packets::LinkLayerPacketView)>&
+                   device_receive,
                const std::shared_ptr<PhyLayerFactory>& factory);
   virtual ~PhyLayerImpl() override;
 
-  virtual void Send(const std::shared_ptr<packets::LinkLayerPacketBuilder> packet) override;
-  virtual void Receive(packets::LinkLayerPacketView packet) override;
+  virtual void Send(
+      const std::shared_ptr<model::packets::LinkLayerPacketBuilder> packet)
+      override;
+  virtual void Send(model::packets::LinkLayerPacketView packet) override;
+  virtual void Receive(model::packets::LinkLayerPacketView packet) override;
   virtual void Unregister() override;
   virtual bool IsFactoryId(uint32_t factory_id) override;
   virtual void TimerTick() override;
