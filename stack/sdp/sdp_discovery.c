1dc85bef62511da0d2d1dd5371e8b812d3dc3a7a
Zongheng Wang <wangzongheng@google.com>
2019-11-12 12:59:44 -0800

    SDP: add return after SDP disconnection
    A return is needed after sdp_disconnect(). It is the logic
    expected and it prevents the use of p_ccb after it's freed.
    
    Bug: 144177780
    Bug: 117105007
    Test: manual test
    Change-Id: I7a64382b36adca37a8ff0c7e361d89ecdc8f3b55
    (cherry picked from commit 30efc8c90a846460359a489e17e1461c725958b3)
    (cherry picked from commit 5edd605227af9a1b9eedf4fd9f02373a47fd49fb)

--- stack/sdp/sdp_discovery.c
+++ stack/sdp/sdp_discovery.c
@@ -511,6 +511,7 @@ static void process_service_attr_rsp (tCONN_CB* p_ccb, uint8_t* p_reply,
             if (!sdp_copy_raw_data(p_ccb, FALSE)) {
                 SDP_TRACE_ERROR("sdp_copy_raw_data failed");
                 sdp_disconnect(p_ccb, SDP_ILLEGAL_PARAMETER);
+                return;
             }
 #endif
 
@@ -755,6 +756,7 @@ static void process_service_search_attr_rsp (tCONN_CB* p_ccb, uint8_t* p_reply,
     if (!sdp_copy_raw_data (p_ccb, TRUE)) {
         SDP_TRACE_ERROR("sdp_copy_raw_data failed");
         sdp_disconnect(p_ccb, SDP_ILLEGAL_PARAMETER);
+        return;
     }
 #endif
 
