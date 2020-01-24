f617a523c628431999d0f7d533a7d56cc76b8797
Hansong Zhang <hsz@google.com>
2020-01-24 14:40:41 -0800

    Improve cert set up
    Test: cert/run_cert.sh and cert/run_cert_facade_only.sh
    Bug: 148147248
    Change-Id: Ie8bddde7a7fe3b8c7f213be254645549115ec866

--- gd/cert/run_cert_facade_only.sh
+++ gd/cert/run_cert_facade_only.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
-# For bluetooth_packets_python3
+
+if [[ -z "${ANDROID_BUILD_TOP}" ]]; then
+  echo "ANDROID_BUILD_TOP is not set"
+fi
+
+if [[ -z "${ANDROID_HOST_OUT}" ]]; then
+  echo "ANDROID_HOST_OUT is not set for host run"
+fi
+
 PYTHONPATH=$PYTHONPATH:$ANDROID_BUILD_TOP/out/host/linux-x86/lib64:$ANDROID_BUILD_TOP/system/bt/gd python3.8 `which act.py` -c $ANDROID_BUILD_TOP/system/bt/gd/cert/host_only_config_facade_only.json -tf $ANDROID_BUILD_TOP/system/bt/gd/cert/cert_testcases_facade_only -tp $ANDROID_BUILD_TOP/system/bt/gd
