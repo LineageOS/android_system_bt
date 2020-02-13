469ae986b0968a963c97a895e2d6484c0b563670
Hansong Zhang <hsz@google.com>
2020-02-13 11:40:44 -0800

    GattServcer: Check invalid offset
    Test: manual
    Bug: 143231677
    Change-Id: I0396380f431cdb7f91c78db6de9043ea0f373dfe
    Merged-In: I97e2c3ae15fccc482d07d8d621c455cc74900cfd
    Merged-In: I0ca22e7c60292d61c758120c1cd67f6e6edd8ae8
    (cherry picked from commit c14c1fb86420bb69b1d8d2ee9e83108331183acd)

--- service/gatt_server.cc
+++ service/gatt_server.cc
@@ -16,6 +16,7 @@
 
 #include "service/gatt_server.h"
 
+#include "osi/include/log.h"
 #include "service/logging_helpers.h"
 #include "stack/include/bt_types.h"
 
@@ -111,6 +112,12 @@ bool GattServer::SendResponse(const std::string& device_address, int request_id,
   RawAddress addr;
   if (!RawAddress::FromString(device_address, addr)) {
     LOG(ERROR) << "Invalid device address given: " << device_address;
+    return false;
+  }
+
+  if (offset < 0) {
+    android_errorWriteLog(0x534e4554, "143231677");
+    LOG(ERROR) << "Offset is less than 0 offset: " << offset;
     return false;
   }
 
