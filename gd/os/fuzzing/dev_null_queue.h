3e02b68dec7e0f524ae18dbb9679ec38386773c7
Zach Johnson <zachoverflow@google.com>
2020-04-09 16:03:12 -0700

    Add DevNullQueue, a fuzzing helper to drop stuff
    Hook it up to a queue, and it keeps taking all data
    you send it but never responds.
    
    Test: new fuzz test I'm working on
    Change-Id: Ie5614592a84a0ce933c76f3869ced980cfa365e8

---
+++ gd/os/fuzzing/dev_null_queue.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "os/queue.h"
+
+namespace bluetooth {
+namespace os {
+namespace fuzzing {
+
+// Drops stuff you send it, and banishes it into the void.
+template <typename T>
+class DevNullQueue {
+ public:
+  DevNullQueue(IQueueDequeue<T>* queue, Handler* handler) : queue_(queue), handler_(handler) {}
+  ~DevNullQueue() {}
+
+  void Start() {
+    queue_->RegisterDequeue(handler_, common::Bind(&DevNullQueue::Dump, common::Unretained(this)));
+  }
+
+  void Stop() {
+    queue_->UnregisterDequeue();
+  }
+
+  void Dump() {
+    queue_->TryDequeue();
+  }
+
+ private:
+  IQueueDequeue<T>* queue_;
+  Handler* handler_;
+};
+
+}  // namespace fuzzing
+}  // namespace os
+}  // namespace bluetooth
