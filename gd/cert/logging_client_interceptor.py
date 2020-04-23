d9baab4f0eef771a385b1b0620a9136a994a3a36
Jakub Pawlowski <jpawlowski@google.com>
2020-04-23 16:17:55 +0200

    GD Cert tests: log grpc communication in verbose mode
    Bug: 145007149
    Tag: #gd-refactor
    Test: gd/cert/run --verbose
    Change-Id: I0e81f5bf1b2d2320ac5569cc9e3ca13d2762e0da

---
+++ gd/cert/logging_client_interceptor.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#
+#   Copyright 2020 - The Android Open Source Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import logging
+
+import grpc
+
+from google.protobuf import text_format
+
+
+def pretty_print(request):
+    return '{} {}'.format(type(request).__name__, text_format.MessageToString(request, as_one_line=True))
+
+
+class LoggingClientInterceptor(grpc.UnaryUnaryClientInterceptor):
+
+    def __init__(self, name):
+        self.name = name
+
+    def _intercept_call(self, continuation, client_call_details, request_or_iterator):
+        return continuation(client_call_details, request_or_iterator)
+
+    def intercept_unary_unary(self, continuation, client_call_details, request):
+        print('[host ▶▶▶▶▶ ' + self.name + '] ' + client_call_details.method + ' ' + pretty_print(request))
+        return self._intercept_call(continuation, client_call_details, request)
