3b7fb36b4914fd3fdfbede287805bda58260e4fe
Myles Watson <mylesgw@google.com>
2020-04-30 17:13:23 -0700

    GD: Generate HTML coverage report
    Test: ./cert/run --host && ./cert/gen_html_coverage.sh
    Change-Id: Idd0a6c466cbb7967f1aacd4a66b0adff6d2c83ed

---
+++ gd/cert/gen_html_coverage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pushd $ANDROID_BUILD_TOP
+
+llvm-cov show --format=html --summary-only --show-line-counts-or-regions --show-instantiation-summary --instr-profile=/tmp/logs/HostOnlyCert/latest/GdDevice_stack_under_test_backing_process_coverage.profdata --output-dir=/tmp/logs/HostOnlyCert/latest/GdDevice_stack_under_test_backing_process_coverage/ out/dist/bluetooth_venv/lib/python3.8/site-packages/bluetooth_stack_with_facade 
+
+popd
+
+echo "point your browser to file:///tmp/logs/HostOnlyCert/latest/GdDevice_stack_under_test_backing_process_coverage/index.html"
+
