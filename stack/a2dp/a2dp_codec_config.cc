25b641af5509ee2a55018d8f3e0931df04ce3e54
Satheesh Kumar Pallemoni <spallemo@codeaurora.org>
2020-05-13 22:58:41 +0530

    A2dpSrc: Align legacy codec enumeration with AOSP(3/7)
    To avoid any future CTS failures, need to align our QC
    codec enumeration with AOSP. On top of that enumeraion
    extaned to QVA codecs.
    
    CRs-Fixed: 2708798
    Change-Id: I445f404e39680766d445ce47d9a1e2903c35b961

--- stack/a2dp/a2dp_codec_config.cc
+++ stack/a2dp/a2dp_codec_config.cc
@@ -623,7 +623,7 @@ bool A2dpCodecs::init() {
     }
 
     // In offload mode, disable the codecs based on the property
-    if ((codec_index < BTAV_A2DP_CODEC_INDEX_SOURCE_MAX) &&
+    if ((codec_index < BTAV_A2DP_QVA_CODEC_INDEX_SOURCE_MAX) &&
         a2dp_offload_status && (offload_codec_support[i] != true)) {
       codec_priority = BTAV_A2DP_CODEC_PRIORITY_DISABLED;
     }
@@ -645,7 +645,7 @@ bool A2dpCodecs::init() {
 
     indexed_codecs_.insert(std::make_pair(codec_index, codec_config));
 
-    if (codec_index < BTAV_A2DP_CODEC_INDEX_SOURCE_MAX) {
+    if (codec_index < BTAV_A2DP_QVA_CODEC_INDEX_SOURCE_MAX) {
       ordered_source_codecs_.push_back(codec_config);
       ordered_source_codecs_.sort(compare_codec_priority);
     } else {
