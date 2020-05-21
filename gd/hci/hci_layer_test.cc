7ae58794e427b87729d2f66804b29c553736e2ab
Myles Watson <mylesgw@google.com>
2020-05-20 21:57:10 -0700

    Remove ASSERT from tests
    ASSERT kills the test process instead of just
    failing the test.
    
    Tag: #gd-refactor
    Bug: 157507372
    Test: atest bluetooth_test_gd
    Change-Id: I96f3ff667c3604edb03d2c1eac9b83ee79f5c464

--- gd/hci/hci_layer_test.cc
+++ gd/hci/hci_layer_test.cc
@@ -286,7 +286,7 @@ class HciTest : public ::testing::Test {
     fake_registry_.Start<DependsOnHci>(&fake_registry_.GetTestThread());
     hci = static_cast<HciLayer*>(fake_registry_.GetModuleUnderTest(&HciLayer::Factory));
     upper = static_cast<DependsOnHci*>(fake_registry_.GetModuleUnderTest(&DependsOnHci::Factory));
-    ASSERT(fake_registry_.IsStarted<HciLayer>());
+    ASSERT_TRUE(fake_registry_.IsStarted<HciLayer>());
 
     auto reset_sent_status = command_future.wait_for(kTimeout);
     ASSERT_EQ(reset_sent_status, std::future_status::ready);
@@ -349,7 +349,7 @@ TEST_F(HciTest, leMetaEvent) {
   ASSERT_EQ(event_status, std::future_status::ready);
 
   auto event = upper->GetReceivedEvent();
-  ASSERT(LeConnectionCompleteView::Create(LeMetaEventView::Create(EventPacketView::Create(event))).IsValid());
+  ASSERT_TRUE(LeConnectionCompleteView::Create(LeMetaEventView::Create(EventPacketView::Create(event))).IsValid());
 }
 
 TEST_F(HciTest, noOpCredits) {
@@ -392,8 +392,9 @@ TEST_F(HciTest, noOpCredits) {
   ASSERT_EQ(event_status, std::future_status::ready);
 
   auto event = upper->GetReceivedEvent();
-  ASSERT(ReadLocalVersionInformationCompleteView::Create(CommandCompleteView::Create(EventPacketView::Create(event)))
-             .IsValid());
+  ASSERT_TRUE(
+      ReadLocalVersionInformationCompleteView::Create(CommandCompleteView::Create(EventPacketView::Create(event)))
+          .IsValid());
 }
 
 TEST_F(HciTest, creditsTest) {
@@ -439,8 +440,9 @@ TEST_F(HciTest, creditsTest) {
   ASSERT_EQ(event_status, std::future_status::ready);
 
   auto event = upper->GetReceivedEvent();
-  ASSERT(ReadLocalVersionInformationCompleteView::Create(CommandCompleteView::Create(EventPacketView::Create(event)))
-             .IsValid());
+  ASSERT_TRUE(
+      ReadLocalVersionInformationCompleteView::Create(CommandCompleteView::Create(EventPacketView::Create(event)))
+          .IsValid());
 
   // Verify that the second one is sent
   command_sent_status = command_future.wait_for(kTimeout);
@@ -468,8 +470,9 @@ TEST_F(HciTest, creditsTest) {
   ASSERT_EQ(event_status, std::future_status::ready);
 
   event = upper->GetReceivedEvent();
-  ASSERT(ReadLocalSupportedCommandsCompleteView::Create(CommandCompleteView::Create(EventPacketView::Create(event)))
-             .IsValid());
+  ASSERT_TRUE(
+      ReadLocalSupportedCommandsCompleteView::Create(CommandCompleteView::Create(EventPacketView::Create(event)))
+          .IsValid());
   // Verify that the third one is sent
   command_sent_status = command_future.wait_for(kTimeout);
   ASSERT_EQ(command_sent_status, std::future_status::ready);
@@ -492,8 +495,9 @@ TEST_F(HciTest, creditsTest) {
   event_status = event_future.wait_for(kTimeout);
   ASSERT_EQ(event_status, std::future_status::ready);
   event = upper->GetReceivedEvent();
-  ASSERT(ReadLocalSupportedFeaturesCompleteView::Create(CommandCompleteView::Create(EventPacketView::Create(event)))
-             .IsValid());
+  ASSERT_TRUE(
+      ReadLocalSupportedFeaturesCompleteView::Create(CommandCompleteView::Create(EventPacketView::Create(event)))
+          .IsValid());
 }
 
 TEST_F(HciTest, leSecurityInterfaceTest) {
