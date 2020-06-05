595878d9d83350b1b15da58ce99de123601964d3
Jack He <siyuanh@google.com>
2020-06-05 01:00:07 -0700

    GD-storage: Add LegacyConfigFile
    * LegacyConfigFile: Read, save, and delete legacy config on non-volatile storage
    * Add serialization function to ConfigCache
    * Add change callback input to ConfigCache so that it can notify a
      listener when a persistent device property changed
    
    Bug: 157533831
    Test: atest bluetooth_test_gd
    Tag: #gd-refactor
    Change-Id: Icbedb900b803508c60ee07041903ccb1b23b5544

---
+++ gd/storage/legacy_config_file.cc
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "storage/legacy_config_file.h"
+
+#include <cerrno>
+#include <fstream>
+#include <sstream>
+
+#include "common/strings.h"
+#include "os/files.h"
+#include "os/log.h"
+
+namespace bluetooth {
+namespace storage {
+
+LegacyConfigFile::LegacyConfigFile(std::string path) : path_(std::move(path)) {
+  ASSERT(!path_.empty());
+};
+
+std::optional<ConfigCache> LegacyConfigFile::Read(size_t temp_devices_capacity) {
+  ASSERT(!path_.empty());
+  std::ifstream config_file(path_);
+  if (!config_file || !config_file.is_open()) {
+    LOG_ERROR("unable to open file '%s', error: %s", path_.c_str(), strerror(errno));
+    return std::nullopt;
+  }
+  int line_num = 0;
+  ConfigCache cache(temp_devices_capacity);
+  std::string line;
+  std::string section(ConfigCache::kDefaultSectionName);
+  while (std::getline(config_file, line)) {
+    ++line_num;
+    line = common::StringTrim(std::move(line));
+    if (line.front() == '\0' || line.front() == '#') {
+      continue;
+    }
+    if (line.front() == '[') {
+      if (line.back() != ']') {
+        LOG_WARN("unterminated section name on line %d", line_num);
+        return std::nullopt;
+      }
+      // Read 'test' from '[text]', hence -2
+      section = line.substr(1, line.size() - 2);
+    } else {
+      auto tokens = common::StringSplit(line, "=", 2);
+      if (tokens.size() != 2) {
+        LOG_WARN("no key/value separator found on line %d", line_num);
+        return std::nullopt;
+      }
+      tokens[0] = common::StringTrim(std::move(tokens[0]));
+      tokens[1] = common::StringTrim(std::move(tokens[1]));
+      cache.SetProperty(section, tokens[0], std::move(tokens[1]));
+    }
+  }
+  return cache;
+}
+
+bool LegacyConfigFile::Write(const ConfigCache& cache) {
+  return os::WriteToFile(path_, cache.SerializeToLegacyFormat());
+}
+
+bool LegacyConfigFile::Delete() {
+  if (remove(path_.c_str()) != 0) {
+    LOG_WARN("unable to remove file '%s', error: %s", path_.c_str(), strerror(errno));
+    return false;
+  }
+  return true;
+}
+
+}  // namespace storage
+}  // namespace bluetooth
