7ae07ea3860dd5057dde657753957b89f8ed5798
Myles Watson <mylesgw@google.com>
2020-06-18 14:35:53 -0700

    DO NOT MERGE: Remove pairing on incoming bond request
    Bug: 150156492
    Tag: #security
    Test: Bond two devices, forget from one device and reconnect
    Change-Id: I048b7b142e3fe2096cf1a9aa2931c175fa52cd45
    (cherry picked from commit 13f409ad3a2423b06af7a7f1a9b06fb06c8820a7)
    Merged-In: I048b7b142e3fe2096cf1a9aa2931c175fa52cd45
    (cherry picked from commit e29c52cfdabd13c259f442ebaaf4ec876173d320)

--- stack/btm/btm_sec.cc
+++ stack/btm/btm_sec.cc
@@ -51,6 +51,8 @@
 bool(APPL_AUTH_WRITE_EXCEPTION)(const RawAddress& bd_addr);
 #endif
 
+extern void bta_dm_process_remove_device(const RawAddress& bd_addr);
+
 /*******************************************************************************
  *             L O C A L    F U N C T I O N     P R O T O T Y P E S            *
  ******************************************************************************/
@@ -3279,6 +3281,13 @@ void btm_io_capabilities_req(const RawAddress& p) {
 
   BTM_TRACE_EVENT("%s: State: %s", __func__,
                   btm_pair_state_descr(btm_cb.pairing_state));
+
+  if (btm_sec_is_a_bonded_dev(p)) {
+    BTM_TRACE_WARNING(
+        "%s: Incoming bond request, but %s is already bonded (removing)",
+        __func__, p.ToString().c_str());
+    bta_dm_process_remove_device(p);
+  }
 
   p_dev_rec = btm_find_or_alloc_dev(evt_data.bd_addr);
 
