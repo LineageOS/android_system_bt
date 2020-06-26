044ea0e531918daecddaea3209c48a63e2b176a7
Rahul Sabnis <rahulsabnis@google.com>
2020-06-26 10:27:07 -0700

    Shows a consent dialog on the local device when pairing a bluetooth low energy device if the local device has a display.
    Tag: #security
    Bug: 157038281
    Test: Manual
    Merged-In: I7de396230beb84bd0fa2b0cea346523b6824472a
    Change-Id: I7de396230beb84bd0fa2b0cea346523b6824472a
    (cherry picked from commit 8709264b5c18b53a0bc266b82d84ade06b04126b)

--- stack/btm/btm_ble.cc
+++ stack/btm/btm_ble.cc
@@ -2065,6 +2065,7 @@ uint8_t btm_proc_smp_cback(tSMP_EVT event, const RawAddress& bd_addr,
         /* fall through */
         p_dev_rec->sec_flags |= BTM_SEC_LE_AUTHENTICATED;
 
+      case SMP_CONSENT_REQ_EVT:
       case SMP_SEC_REQUEST_EVT:
         if (event == SMP_SEC_REQUEST_EVT &&
             btm_cb.pairing_state != BTM_PAIR_STATE_IDLE) {
@@ -2072,7 +2073,9 @@ uint8_t btm_proc_smp_cback(tSMP_EVT event, const RawAddress& bd_addr,
           break;
         }
         btm_cb.pairing_bda = bd_addr;
-        p_dev_rec->sec_state = BTM_SEC_STATE_AUTHENTICATING;
+        if (event != SMP_CONSENT_REQ_EVT) {
+          p_dev_rec->sec_state = BTM_SEC_STATE_AUTHENTICATING;
+        }
         btm_cb.pairing_flags |= BTM_PAIR_FLAGS_LE_ACTIVE;
       /* fall through */
 
