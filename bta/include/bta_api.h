b3f12befdc4def7d695b6f1049cd02238eb1e4a8
Rahul Sabnis <rahulsabnis@google.com>
2020-06-26 10:27:07 -0700

    Shows a consent dialog on the local device when pairing a bluetooth low energy device if the local device has a display.
    Tag: #security
    Bug: 157038281
    Test: Manual
    Merged-In: I7de396230beb84bd0fa2b0cea346523b6824472a
    Change-Id: I7de396230beb84bd0fa2b0cea346523b6824472a
    (cherry picked from commit 03580fc44067b4289d1d77bd2f3ddf1e5974cf30)

--- bta/include/bta_api.h
+++ bta/include/bta_api.h
@@ -380,6 +380,7 @@ typedef uint8_t tBTA_SIG_STRENGTH_MASK;
                                     */
 #define BTA_DM_ENER_INFO_READ 28 /* Energy info read */
 #define BTA_DM_BLE_SC_OOB_REQ_EVT 29 /* SMP SC OOB request event */
+#define BTA_DM_BLE_CONSENT_REQ_EVT 30 /* SMP consent request event */
 typedef uint8_t tBTA_DM_SEC_EVT;
 
 /* Structure associated with BTA_DM_ENABLE_EVT */
