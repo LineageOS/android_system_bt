99043ed7615a881673b7688d365ac5e79f730e43
Chris Manton <cmanton@google.com>
2020-07-23 10:27:17 -0700

    Add table parsing and tests
    Bug: 157647700
    Test: atest --host bluetooth_test_gd
    Tag: #gd-refactor
    
    Change-Id: I644e8dbe920eb7000a936388d7e5777da2ea9ec0

--- gd/dumpsys/test_data/baz.h
+++ gd/dumpsys/test_data/baz.h
@@ -7,25 +7,23 @@ namespace testing {
 
 class BazTestDataClass : public DumpsysTestDataClass {
  public:
-  TableAddFunction GetTable(flatbuffers::FlatBufferBuilder& builder) override {
-    int value = 0;
+  TableAddFunction GetTable(flatbuffers::FlatBufferBuilder& fb_builder) override {
+    auto sub_name_private = fb_builder.CreateString("Baz Subtable Private");
+    auto sub_name_opaque = fb_builder.CreateString("Baz Subtable Opaque");
+    auto sub_name_anonymized = fb_builder.CreateString("Baz Subtable Anonymized");
+    auto sub_name_any = fb_builder.CreateString("Baz Subtable Any");
 
-    auto sub_name_any = builder.CreateString("Baz Subtable Any");
-    auto sub_name_private = builder.CreateString("Baz Subtable Private");
-    auto sub_name_opaque = builder.CreateString("Baz Subtable Opaque");
-    auto sub_name_anonymized = builder.CreateString("Baz Subtable Anonymized");
+    auto private_subtable = CreateBazSubTablePrivate(fb_builder, 1, sub_name_private);
+    auto opaque_subtable = CreateBazSubTableOpaque(fb_builder, 1, sub_name_opaque);
+    auto anonymized_subtable = CreateBazSubTableAnonymized(fb_builder, 1, sub_name_anonymized);
+    auto any_subtable = CreateBazSubTableAny(fb_builder, 1, 2, 3, 4, sub_name_any);
 
-    auto any_subtable = CreateBazSubTableAny(builder, ++value, 1, 2, 3, sub_name_any);
-    auto private_subtable = CreateBazSubTablePrivate(builder, ++value, sub_name_private);
-    auto opaque_subtable = CreateBazSubTableOpaque(builder, ++value, sub_name_opaque);
-    auto anonymized_subtable = CreateBazSubTableAnonymized(builder, ++value, sub_name_anonymized);
-
-    BazTestSchemaBuilder baz_builder(builder);
-    baz_builder.add_sub_table_any(any_subtable);
-    baz_builder.add_sub_table_private(private_subtable);
-    baz_builder.add_sub_table_opaque(opaque_subtable);
-    baz_builder.add_sub_table_anonymized(anonymized_subtable);
-    auto baz_table = baz_builder.Finish();
+    BazTestSchemaBuilder builder(fb_builder);
+    builder.add_sub_table_private(private_subtable);
+    builder.add_sub_table_opaque(opaque_subtable);
+    builder.add_sub_table_anonymized(anonymized_subtable);
+    builder.add_sub_table_any(any_subtable);
+    auto baz_table = builder.Finish();
 
     return [baz_table](DumpsysTestDataRootBuilder* builder) { builder->add_baz_module_data(baz_table); };
   }
