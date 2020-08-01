73dea2b8eed124f5cb5d28896fb0c93ea012a3b6
Rahul Sabnis <rahulsabnis@google.com>
2020-07-31 19:44:27 -0700

    Check whether local device is an ATV device to determine whether to show the consent dialog for BLE pairing in JUSTWORKS and ENCRYPTION_ONLY mode
    Tag: #feature
    Bug: 157038281
    Test: Manual
    Merged-In: I6d06f5996da71e5a1407e544b0023d82924aa56f
    Change-Id: I6d06f5996da71e5a1407e544b0023d82924aa56f
    (cherry picked from commit ad8fba09accd6909e6678b8c9820732f36588401)

--- btif/src/bluetooth.cc
+++ btif/src/bluetooth.cc
@@ -82,6 +82,7 @@ using bluetooth::hearing_aid::HearingAidInterface;
 
 bt_callbacks_t* bt_hal_cbacks = NULL;
 bool restricted_mode = false;
+bool is_local_device_atv = false;
 
 /*******************************************************************************
  *  Externs
@@ -138,7 +139,7 @@ static bool is_profile(const char* p1, const char* p2) {
  *
  ****************************************************************************/
 
-static int init(bt_callbacks_t* callbacks) {
+static int init(bt_callbacks_t* callbacks, bool is_atv) {
   LOG_INFO(LOG_TAG, "%s", __func__);
 
   if (interface_ready()) return BT_STATUS_DONE;
@@ -148,6 +149,7 @@ static int init(bt_callbacks_t* callbacks) {
 #endif
 
   bt_hal_cbacks = callbacks;
+  is_local_device_atv = is_atv;
   stack_manager_get_interface()->init_stack();
   btif_debug_init();
   return BT_STATUS_SUCCESS;
@@ -174,6 +176,8 @@ static int disable(void) {
 static void cleanup(void) { stack_manager_get_interface()->clean_up_stack(); }
 
 bool is_restricted_mode() { return restricted_mode; }
+
+bool is_atv_device() { return is_local_device_atv; }
 
 static int get_adapter_properties(void) {
   /* sanity check */
