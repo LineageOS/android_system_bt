0c18cf38465582f9f51449cf042ef27568c10147
Chris Manton <cmanton@google.com>
2020-08-04 09:03:47 -0700

    Add init flags to dumpsys
    Bug: 162837468
    Test: atest --host bluetooth_test_gd
    Tag: #gd-refactor
    
    Change-Id: I541be13347613a72add23a61bf9fb3f092d2792a

---
+++ gd/dumpsys/init_flags.cc
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "common/init_flags.h"
+#include "dumpsys/init_flags.h"
+#include "init_flags_generated.h"
+
+flatbuffers::Offset<bluetooth::common::InitFlagsData> bluetooth::dumpsys::InitFlags::Dump(
+    flatbuffers::FlatBufferBuilder* fb_builder) {
+  auto title = fb_builder->CreateString("----- Init Flags -----");
+  common::InitFlagsDataBuilder builder(*fb_builder);
+  builder.add_title(title);
+  builder.add_gd_hci_enabled(bluetooth::common::InitFlags::GdHciEnabled());
+  builder.add_gd_controller_enabled(bluetooth::common::InitFlags::GdControllerEnabled());
+  builder.add_gd_core_enabled(bluetooth::common::InitFlags::GdCoreEnabled());
+  return builder.Finish();
+}
