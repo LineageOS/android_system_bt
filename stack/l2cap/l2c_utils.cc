828f76750e8875d7ceeb4ed970f55d18af1c8fb4
Zach Johnson <zachoverflow@google.com>
2020-08-07 11:55:29 -0700

    Remove unused echo response reporting
    Bug: 159815595
    Tag: #refactor
    Test: compile & verify basic functions working
    Change-Id: Ia83ad5d7ca7758076aecaa1ea840f049e87b579c

--- stack/l2cap/l2c_utils.cc
+++ stack/l2cap/l2c_utils.cc
@@ -189,16 +189,6 @@ void l2cu_release_lcb(tL2C_LCB* p_lcb) {
     l2c_link_adjust_allocation();
   }
 
-  /* Check for ping outstanding */
-  if (p_lcb->p_echo_rsp_cb) {
-    tL2CA_ECHO_RSP_CB* p_cb = p_lcb->p_echo_rsp_cb;
-
-    /* Zero out the callback in case app immediately calls us again */
-    p_lcb->p_echo_rsp_cb = NULL;
-
-    (*p_cb)(L2CAP_PING_RESULT_NO_LINK);
-  }
-
   /* Check and release all the LE COC connections waiting for security */
   if (p_lcb->le_sec_pending_q) {
     while (!fixed_queue_is_empty(p_lcb->le_sec_pending_q)) {
@@ -892,42 +882,6 @@ void l2cu_send_peer_disc_rsp(tL2C_LCB* p_lcb, uint8_t remote_id,
 
   UINT16_TO_STREAM(p, local_cid);
   UINT16_TO_STREAM(p, remote_cid);
-
-  l2c_link_check_send_pkts(p_lcb, NULL, p_buf);
-}
-
-/*******************************************************************************
- *
- * Function         l2cu_send_peer_echo_req
- *
- * Description      Build and send an L2CAP "echo request" message
- *                  to the peer. Note that we do not currently allow
- *                  data in the echo request.
- *
- * Returns          void
- *
- ******************************************************************************/
-void l2cu_send_peer_echo_req(tL2C_LCB* p_lcb, uint8_t* p_data,
-                             uint16_t data_len) {
-  BT_HDR* p_buf;
-  uint8_t* p;
-
-  p_lcb->id++;
-  l2cu_adj_id(p_lcb, L2CAP_ADJ_ZERO_ID); /* check for wrap to '0' */
-
-  p_buf = l2cu_build_header(p_lcb, (uint16_t)(L2CAP_ECHO_REQ_LEN + data_len),
-                            L2CAP_CMD_ECHO_REQ, p_lcb->id);
-  if (p_buf == NULL) {
-    L2CAP_TRACE_WARNING("L2CAP - no buffer for echo_req");
-    return;
-  }
-
-  p = (uint8_t*)(p_buf + 1) + L2CAP_SEND_CMD_OFFSET + HCI_DATA_PREAMBLE_SIZE +
-      L2CAP_PKT_OVERHEAD + L2CAP_CMD_OVERHEAD;
-
-  if (data_len) {
-    ARRAY_TO_STREAM(p, p_data, data_len);
-  }
 
   l2c_link_check_send_pkts(p_lcb, NULL, p_buf);
 }
