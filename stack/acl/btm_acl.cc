5eb6f560eb10894c9de38d3981810d6255dc204f
Chris Manton <cmanton@google.com>
2020-08-11 19:40:39 -0700

    Move BTM_ReadRemoteConnectionAddr
    From: stack/btm/btm_ble
    Into: stack/acl/btm_acl
    
    Working towards encapsulation of tACL_CONN
    
    Bug: 159815595
    Tag: #refactor
    Test: compile & verify basic functions working
    Change-Id: I07d157e849e7f2286d51fa148f68e66536e5149e

--- stack/acl/btm_acl.cc
+++ stack/acl/btm_acl.cc
@@ -2679,3 +2679,48 @@ bool acl_is_switch_role_idle(const RawAddress& bd_addr,
   }
   return p_acl->switch_role_state == BTM_ACL_SWKEY_STATE_IDLE;
 }
+
+/*******************************************************************************
+ *
+ * Function       BTM_ReadRemoteConnectionAddr
+ *
+ * Description    This function is read the remote device address currently used
+ *
+ * Parameters     pseudo_addr: pseudo random address available
+ *                conn_addr:connection address used
+ *                p_addr_type : BD Address type, Public or Random of the address
+ *                              used
+ *
+ * Returns        bool, true if connection to remote device exists, else false
+ *
+ ******************************************************************************/
+bool BTM_ReadRemoteConnectionAddr(const RawAddress& pseudo_addr,
+                                  RawAddress& conn_addr,
+                                  tBLE_ADDR_TYPE* p_addr_type) {
+  if (bluetooth::shim::is_gd_shim_enabled()) {
+    return bluetooth::shim::BTM_ReadRemoteConnectionAddr(pseudo_addr, conn_addr,
+                                                         p_addr_type);
+  }
+  bool st = true;
+#if (BLE_PRIVACY_SPT == TRUE)
+  tACL_CONN* p_acl = btm_bda_to_acl(pseudo_addr, BT_TRANSPORT_LE);
+
+  if (p_acl == NULL) {
+    BTM_TRACE_ERROR(
+        "BTM_ReadRemoteConnectionAddr can not find connection"
+        " with matching address");
+    return false;
+  }
+
+  conn_addr = p_acl->active_remote_addr;
+  *p_addr_type = p_acl->active_remote_addr_type;
+#else
+  tBTM_SEC_DEV_REC* p_dev_rec = btm_find_dev(pseudo_addr);
+
+  conn_addr = pseudo_addr;
+  if (p_dev_rec != NULL) {
+    *p_addr_type = p_dev_rec->ble.ble_addr_type;
+  }
+#endif
+  return st;
+}
