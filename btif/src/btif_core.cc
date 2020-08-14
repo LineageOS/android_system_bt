6159f23ac4f79c3a3473736fc09dee5a872a1edb
Zach Johnson <zachoverflow@google.com>
2020-08-14 10:27:18 -0700

    Flatten BTIF_CORE_STORAGE_ADAPTER_READ
    Bug: 159815595
    Tag: #refactor
    Test: compile & verify basic functions working
    Change-Id: I7876611d10807072124e6810f77f0e595f4c2c7a

--- btif/src/btif_core.cc
+++ btif/src/btif_core.cc
@@ -625,49 +625,6 @@ static void execute_storage_request(uint16_t event, char* p_param) {
       HAL_CBACK(bt_hal_cbacks, adapter_properties_cb, status, 1, p_prop);
     } break;
 
-    case BTIF_CORE_STORAGE_ADAPTER_READ: {
-      btif_storage_req_t* p_req = (btif_storage_req_t*)p_param;
-      char buf[512];
-      bt_property_t prop;
-      prop.type = p_req->read_req.type;
-      prop.val = (void*)buf;
-      prop.len = sizeof(buf);
-      if (prop.type == BT_PROPERTY_LOCAL_LE_FEATURES) {
-        tBTM_BLE_VSC_CB cmn_vsc_cb;
-        bt_local_le_features_t local_le_features;
-
-        /* LE features are not stored in storage. Should be retrived from stack
-         */
-        BTM_BleGetVendorCapabilities(&cmn_vsc_cb);
-        local_le_features.local_privacy_enabled = BTM_BleLocalPrivacyEnabled();
-
-        prop.len = sizeof(bt_local_le_features_t);
-        if (cmn_vsc_cb.filter_support == 1)
-          local_le_features.max_adv_filter_supported = cmn_vsc_cb.max_filter;
-        else
-          local_le_features.max_adv_filter_supported = 0;
-        local_le_features.max_adv_instance = cmn_vsc_cb.adv_inst_max;
-        local_le_features.max_irk_list_size = cmn_vsc_cb.max_irk_list_sz;
-        local_le_features.rpa_offload_supported = cmn_vsc_cb.rpa_offloading;
-        local_le_features.scan_result_storage_size =
-            cmn_vsc_cb.tot_scan_results_strg;
-        local_le_features.activity_energy_info_supported =
-            cmn_vsc_cb.energy_support;
-        local_le_features.version_supported = cmn_vsc_cb.version_supported;
-        local_le_features.total_trackable_advertisers =
-            cmn_vsc_cb.total_trackable_advertisers;
-
-        local_le_features.extended_scan_support =
-            cmn_vsc_cb.extended_scan_support > 0;
-        local_le_features.debug_logging_supported =
-            cmn_vsc_cb.debug_logging_supported > 0;
-        memcpy(prop.val, &local_le_features, prop.len);
-      } else {
-        status = btif_storage_get_adapter_property(&prop);
-      }
-      HAL_CBACK(bt_hal_cbacks, adapter_properties_cb, status, 1, &prop);
-    } break;
-
     case BTIF_CORE_STORAGE_NOTIFY_STATUS: {
       HAL_CBACK(bt_hal_cbacks, adapter_properties_cb, status, 0, NULL);
     } break;
@@ -776,10 +733,46 @@ void btif_get_adapter_properties(void) {
 void btif_get_adapter_property(bt_property_type_t type) {
   BTIF_TRACE_EVENT("%s %d", __func__, type);
 
-  btif_storage_req_t req;
-  req.read_req.bd_addr = RawAddress::kEmpty;
-  req.read_req.type = type;
-  execute_storage_request(BTIF_CORE_STORAGE_ADAPTER_READ, (char*)&req);
+  bt_status_t status = BT_STATUS_SUCCESS;
+  char buf[512];
+  bt_property_t prop;
+  prop.type = type;
+  prop.val = (void*)buf;
+  prop.len = sizeof(buf);
+  if (prop.type == BT_PROPERTY_LOCAL_LE_FEATURES) {
+    tBTM_BLE_VSC_CB cmn_vsc_cb;
+    bt_local_le_features_t local_le_features;
+
+    /* LE features are not stored in storage. Should be retrived from stack
+     */
+    BTM_BleGetVendorCapabilities(&cmn_vsc_cb);
+    local_le_features.local_privacy_enabled = BTM_BleLocalPrivacyEnabled();
+
+    prop.len = sizeof(bt_local_le_features_t);
+    if (cmn_vsc_cb.filter_support == 1)
+      local_le_features.max_adv_filter_supported = cmn_vsc_cb.max_filter;
+    else
+      local_le_features.max_adv_filter_supported = 0;
+    local_le_features.max_adv_instance = cmn_vsc_cb.adv_inst_max;
+    local_le_features.max_irk_list_size = cmn_vsc_cb.max_irk_list_sz;
+    local_le_features.rpa_offload_supported = cmn_vsc_cb.rpa_offloading;
+    local_le_features.scan_result_storage_size =
+        cmn_vsc_cb.tot_scan_results_strg;
+    local_le_features.activity_energy_info_supported =
+        cmn_vsc_cb.energy_support;
+    local_le_features.version_supported = cmn_vsc_cb.version_supported;
+    local_le_features.total_trackable_advertisers =
+        cmn_vsc_cb.total_trackable_advertisers;
+
+    local_le_features.extended_scan_support =
+        cmn_vsc_cb.extended_scan_support > 0;
+    local_le_features.debug_logging_supported =
+        cmn_vsc_cb.debug_logging_supported > 0;
+    memcpy(prop.val, &local_le_features, prop.len);
+  } else {
+    status = btif_storage_get_adapter_property(&prop);
+  }
+  HAL_CBACK(bt_hal_cbacks, adapter_properties_cb, status, 1, &prop);
 }
 
 /*******************************************************************************
