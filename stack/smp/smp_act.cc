36bebf2810c4083c3933865ab8ccacc67b62ee10
Chris Manton <cmanton@google.com>
2020-10-11 14:36:53 -0700

    Remove unused def SMP_DEBUG
    Toward readable code
    
    Bug: 163134718
    Tag: #refactor
    Test: compile & verify basic functions working
    
    Change-Id: Ic1411faf31fd6c4dc66282bfe42a0ec47c969bb1

--- stack/smp/smp_act.cc
+++ stack/smp/smp_act.cc
@@ -1891,28 +1891,6 @@ void smp_set_local_oob_random_commitment(tSMP_CB* p_cb, tSMP_INT_DATA* p_data) {
                          p_cb->sc_oob_data.loc_oob_data.publ_key_used.x,
                          p_cb->sc_oob_data.loc_oob_data.randomizer, 0);
 
-#if (SMP_DEBUG == TRUE)
-  uint8_t* p_print = NULL;
-  SMP_TRACE_DEBUG("local SC OOB data set:");
-  p_print = (uint8_t*)&p_cb->sc_oob_data.loc_oob_data.addr_sent_to;
-  smp_debug_print_nbyte_little_endian(p_print, "addr_sent_to",
-                                      sizeof(tBLE_BD_ADDR));
-  p_print = (uint8_t*)&p_cb->sc_oob_data.loc_oob_data.private_key_used;
-  smp_debug_print_nbyte_little_endian(p_print, "private_key_used",
-                                      BT_OCTET32_LEN);
-  p_print = (uint8_t*)&p_cb->sc_oob_data.loc_oob_data.publ_key_used.x;
-  smp_debug_print_nbyte_little_endian(p_print, "publ_key_used.x",
-                                      BT_OCTET32_LEN);
-  p_print = (uint8_t*)&p_cb->sc_oob_data.loc_oob_data.publ_key_used.y;
-  smp_debug_print_nbyte_little_endian(p_print, "publ_key_used.y",
-                                      BT_OCTET32_LEN);
-  p_print = (uint8_t*)&p_cb->sc_oob_data.loc_oob_data.randomizer;
-  smp_debug_print_nbyte_little_endian(p_print, "randomizer", OCTET16_LEN);
-  p_print = (uint8_t*)&p_cb->sc_oob_data.loc_oob_data.commitment;
-  smp_debug_print_nbyte_little_endian(p_print, "commitment", OCTET16_LEN);
-  SMP_TRACE_DEBUG("");
-#endif
-
   /* pass created OOB data up */
   p_cb->cb_evt = SMP_SC_LOC_OOB_DATA_UP_EVT;
   smp_send_app_cback(p_cb, NULL);
