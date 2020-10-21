5d004490419e2c7b27127f1545839173088ae519
Martin Brabham <optedoblivion@google.com>
2020-10-21 00:41:42 -0700

    GD Security Shim: Make ConfirmationData have strict types
    Bug: 162984360
    Tag: #gd-refactor
    Test: cert/run --host SecurityTest
    Test: atest --host bluetooth_test_gd
    Test: atest --host bluetooth_test_common
    Change-Id: I6684d53b7fe154d80d0869333f1d6d19459d1090

--- main/shim/btm_api.cc
+++ main/shim/btm_api.cc
@@ -240,43 +240,29 @@ namespace {
 std::unordered_map<bluetooth::hci::AddressWithType, bt_bdname_t>
     address_name_map_;
 
-std::unordered_map<std::string, int> gd_legacy_io_caps_map_ = {
-    {bluetooth::hci::IoCapabilityText(
-         bluetooth::hci::IoCapability::DISPLAY_ONLY),
-     BTM_IO_CAP_OUT},
-    {bluetooth::hci::IoCapabilityText(
-         bluetooth::hci::IoCapability::DISPLAY_YES_NO),
-     BTM_IO_CAP_IO},
-    {bluetooth::hci::IoCapabilityText(
-         bluetooth::hci::IoCapability::KEYBOARD_ONLY),
-     BTM_IO_CAP_IN},
-    {bluetooth::hci::IoCapabilityText(
-         bluetooth::hci::IoCapability::NO_INPUT_NO_OUTPUT),
-     BTM_IO_CAP_NONE},
+std::unordered_map<bluetooth::hci::IoCapability, int> gd_legacy_io_caps_map_ = {
+    {bluetooth::hci::IoCapability::DISPLAY_ONLY, BTM_IO_CAP_OUT},
+    {bluetooth::hci::IoCapability::DISPLAY_YES_NO, BTM_IO_CAP_IO},
+    {bluetooth::hci::IoCapability::KEYBOARD_ONLY, BTM_IO_CAP_IN},
+    {bluetooth::hci::IoCapability::NO_INPUT_NO_OUTPUT, BTM_IO_CAP_NONE},
 };
 
-std::unordered_map<std::string, int> gd_legacy_auth_reqs_map_ = {
-    {bluetooth::hci::AuthenticationRequirementsText(
-         bluetooth::hci::AuthenticationRequirements::NO_BONDING),
-     BTM_AUTH_SP_NO},
-    {bluetooth::hci::AuthenticationRequirementsText(
-         bluetooth::hci::AuthenticationRequirements::
-             NO_BONDING_MITM_PROTECTION),
-     BTM_AUTH_SP_YES},
-    {bluetooth::hci::AuthenticationRequirementsText(
-         bluetooth::hci::AuthenticationRequirements::DEDICATED_BONDING),
-     BTM_AUTH_AP_NO},
-    {bluetooth::hci::AuthenticationRequirementsText(
-         bluetooth::hci::AuthenticationRequirements::
-             DEDICATED_BONDING_MITM_PROTECTION),
-     BTM_AUTH_AP_YES},
-    {bluetooth::hci::AuthenticationRequirementsText(
-         bluetooth::hci::AuthenticationRequirements::GENERAL_BONDING),
-     BTM_AUTH_SPGB_NO},
-    {bluetooth::hci::AuthenticationRequirementsText(
-         bluetooth::hci::AuthenticationRequirements::
-             GENERAL_BONDING_MITM_PROTECTION),
-     BTM_AUTH_SPGB_YES},
+std::unordered_map<bluetooth::hci::AuthenticationRequirements, int>
+    gd_legacy_auth_reqs_map_ = {
+        {bluetooth::hci::AuthenticationRequirements::NO_BONDING,
+         BTM_AUTH_SP_NO},
+        {bluetooth::hci::AuthenticationRequirements::NO_BONDING_MITM_PROTECTION,
+         BTM_AUTH_SP_YES},
+        {bluetooth::hci::AuthenticationRequirements::DEDICATED_BONDING,
+         BTM_AUTH_AP_NO},
+        {bluetooth::hci::AuthenticationRequirements::
+             DEDICATED_BONDING_MITM_PROTECTION,
+         BTM_AUTH_AP_YES},
+        {bluetooth::hci::AuthenticationRequirements::GENERAL_BONDING,
+         BTM_AUTH_SPGB_NO},
+        {bluetooth::hci::AuthenticationRequirements::
+             GENERAL_BONDING_MITM_PROTECTION,
+         BTM_AUTH_SPGB_YES},
 };
 }
 
@@ -350,10 +336,9 @@ class ShimUi : public bluetooth::security::UI {
       // Call sp_cback for IO_RSP
       tBTM_SP_IO_RSP io_rsp_evt_data;
       io_rsp_evt_data.bd_addr = bluetooth::ToRawAddress(address.GetAddress());
-      io_rsp_evt_data.io_cap =
-          gd_legacy_io_caps_map_[data.GetExtraData("remote_io_caps")];
+      io_rsp_evt_data.io_cap = gd_legacy_io_caps_map_[data.GetRemoteIoCaps()];
       io_rsp_evt_data.auth_req =
-          gd_legacy_auth_reqs_map_[data.GetExtraData("remote_auth_reqs")];
+          gd_legacy_auth_reqs_map_[data.GetRemoteAuthReqs()];
       io_rsp_evt_data.auth_req = BTM_AUTH_AP_YES;
       io_rsp_evt_data.oob_data = BTM_OOB_NONE;
       (*bta_callbacks_->p_sp_callback)(BTM_SP_IO_RSP_EVT,
