dec7473c2cf624ff460bb298d035e31f1bae1ad8
Hansong Zhang <hsz@google.com>
2020-10-29 20:18:11 -0700

    num_read_pages is unused
    Bug: 159815595
    Tag: #refactor
    Test: compile & verify basic functions working
    Change-Id: I725cac7c4d6ae3801b6f83ea16cc10d9cdb42276

--- stack/btm/btm_sec.cc
+++ stack/btm/btm_sec.cc
@@ -2420,8 +2420,7 @@ void btm_io_capabilities_req(const RawAddress& p) {
   BTM_TRACE_EVENT("%s: State: %s", __func__,
                   btm_pair_state_descr(btm_cb.pairing_state));
 
-  BTM_TRACE_DEBUG("%s:Security mode: %d, Num Read Remote Feat pages: %d",
-                  __func__, btm_cb.security_mode, p_dev_rec->num_read_pages);
+  BTM_TRACE_DEBUG("%s:Security mode: %d", __func__, btm_cb.security_mode);
 
   p_dev_rec->sm4 |= BTM_SM4_TRUE;
 
