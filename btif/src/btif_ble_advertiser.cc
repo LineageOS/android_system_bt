bdbf2ec9c01bad3bdf614ff94d8cee90113ed05b
Sunny Kapdi <sunnyk@codeaurora.org>
2020-11-17 14:35:28 -0800

    BLE Broadcast Isochronous channel support
    Update header files for BLE Broadcast Isochronous
    channel support.
    
    CRs-Fixed: 2803582
    Change-Id: I0c230328f420ac123c5c11f88223c71f0cfc1a5f

--- btif/src/btif_ble_advertiser.cc
+++ btif/src/btif_ble_advertiser.cc
@@ -237,6 +237,16 @@ class BleAdvertiserInterfaceImpl : public BleAdvertiserInterface {
                            std::move(data), jni_thread_wrapper(FROM_HERE, cb)));
   }
 
+  void CreateBIG(int advertiser_id, CreateBIGParameters create_big_params,
+                 CreateBIGCallback cb) override {
+    VLOG(1) << __func__ << " advertiser_id: " << +advertiser_id;
+  }
+
+  void TerminateBIG(int advertiser_id, int big_handle, int reason,
+                    TerminateBIGCallback cb) override {
+    VLOG(1) << __func__ << "big_handle: " << +big_handle;
+  }
+
   void SetPeriodicAdvertisingEnable(int advertiser_id, bool enable,
                                     StatusCallback cb) override {
     VLOG(1) << __func__ << " advertiser_id: " << +advertiser_id
