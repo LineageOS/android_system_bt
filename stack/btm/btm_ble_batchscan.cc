f035871c7c44f6c93e79163b0a5f82a3a701e219
Chris Manton <cmanton@google.com>
2020-11-26 18:09:27 -0800

    Canonicalize include files
    Altering makefiles triggers a significant rebuild of the
    entire Android make system.
    Altering source code does not.
    
    Towards buildable code
    
    Bug: 163134718
    Tag: #refactor
    Test: gd/cert/run --host
    Change-Id: Ibe479c0c0d3cbe413f4c5e624a7fb69a7131d14b

--- stack/btm/btm_ble_batchscan.cc
+++ stack/btm/btm_ble_batchscan.cc
@@ -23,12 +23,12 @@
 #include "bt_target.h"
 
 #include "bt_types.h"
-#include "bt_utils.h"
 #include "btm_ble_api.h"
 #include "btm_int.h"
 #include "btu.h"
 #include "device/include/controller.h"
 #include "hcimsgs.h"
+#include "utils/include/bt_utils.h"
 
 using base::Bind;
 using base::Callback;
