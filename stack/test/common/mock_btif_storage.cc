dcc65ce9732c450f61ed94f2dfe8f0aa16352085
Marco Ballesio <balejs@google.com>
2020-12-01 00:37:48 +0000

    Revert "Add various stack/test/common/mock_"
    Revert submission 1512835
    
    Reason for revert: DroidMonitor-triggered revert due to breakages:
    
    https://android-build.googleplex.com/builds/quarterdeck?branch=aosp-master&target=aosp_cf_x86_phone-userdebug&lkgb=7002541&lkbb=7002623&fkbb=7002623
    https://android-build.googleplex.com/builds/quarterdeck?branch=aosp-master&target=sdk_x86_64-sdk&lkgb=7002541&lkbb=7002623&fkbb=7002623
    https://android-build.googleplex.com/builds/quarterdeck?branch=aosp-master&target=full-eng&lkgb=7002541&lkbb=7002623&fkbb=7002623
    https://android-build.googleplex.com/builds/quarterdeck?branch=aosp-master&target=aosp_cf_x86_64_phone-userdebug&lkgb=7002434&lkbb=7002649&fkbb=7002649
    https://android-build.googleplex.com/builds/quarterdeck?branch=git_rvc-d1-dev-plus-aosp&target=aosp_cf_x86_phone-userdebug&lkgb=7002643&lkbb=7002696&fkbb=7002696
    https://android-build.googleplex.com/builds/quarterdeck?branch=git_rvc-d1-dev-plus-aosp&target=docs&lkgb=7002604&lkbb=7002703&fkbb=7002696
    
    
    bug http://b/174517408
    Reverted Changes:
    Ieb002c783:Canonicalize include files
    I69d5dff4f:enum-ify fixed_cid stack/include/l2cdefs::L2CAP_
    I5a52498ad:Introduce net_test_stack_btm
    I6a637b5e2:Explicit use base::Location stack/btu/btu_hcif::
    I23c831a61:Add various stack/test/common/mock_
    
    Change-Id: I20453ad181475add66594787b01649f5b687ab21

--- stack/test/common/mock_btif_storage.cc
+++ stack/test/common/mock_btif_storage.cc
@@ -29,5 +29,3 @@ void bluetooth::manager::SetMockBtifStorageInterface(
 void btif_storage_load_bonded_eatt(void) {
   btif_storage_interface->LoadBondedEatt();
 }
-
-uint8_t btif_storage_get_local_io_caps() { return 0; }
