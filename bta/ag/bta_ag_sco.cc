a18b299c5a3daba57e904d6768d7523a93aa4577
Chris Manton <cmanton@google.com>
2020-12-14 15:27:21 -0800

    Re-log bta/ag/bta_ag_sco::bta_ag_sco_disc_cback
    Towards loggable code
    
    Bug: 163134718
    Tag: #refactor
    Test: gd/cert/run --host
    
    Change-Id: Idd8ec314c9fd1109f43f0aed4e0795d1281a97a9

--- bta/ag/bta_ag_sco.cc
+++ bta/ag/bta_ag_sco.cc
@@ -31,8 +31,10 @@
 #include "btm_api.h"
 #include "device/include/controller.h"
 #include "device/include/esco_parameters.h"
+#include "main/shim/dumpsys.h"
 #include "osi/include/log.h"
 #include "osi/include/osi.h"
+#include "stack/btm/btm_sco.h"
 #include "stack/include/btu.h"
 #include "utl.h"
 
@@ -159,21 +161,17 @@ static void bta_ag_sco_conn_cback(uint16_t sco_idx) {
 static void bta_ag_sco_disc_cback(uint16_t sco_idx) {
   uint16_t handle = 0;
 
-  APPL_TRACE_DEBUG(
-      "bta_ag_sco_disc_cback(): sco_idx: 0x%x  p_cur_scb: 0x%08x  sco.state: "
-      "%d",
-      sco_idx, bta_ag_cb.sco.p_curr_scb, bta_ag_cb.sco.state);
-
-  APPL_TRACE_DEBUG(
-      "bta_ag_sco_disc_cback(): scb[0] addr: 0x%08x  in_use: %u  sco_idx: 0x%x "
-      " sco state: %u",
-      &bta_ag_cb.scb[0], bta_ag_cb.scb[0].in_use, bta_ag_cb.scb[0].sco_idx,
-      bta_ag_cb.scb[0].state);
-  APPL_TRACE_DEBUG(
-      "bta_ag_sco_disc_cback(): scb[1] addr: 0x%08x  in_use: %u  sco_idx: 0x%x "
-      " sco state: %u",
-      &bta_ag_cb.scb[1], bta_ag_cb.scb[1].in_use, bta_ag_cb.scb[1].sco_idx,
-      bta_ag_cb.scb[1].state);
+  LOG_DEBUG(
+      "sco_idx: 0x%x sco.state:%s", sco_idx,
+      sco_state_text(static_cast<tSCO_STATE>(bta_ag_cb.sco.state)).c_str());
+  LOG_DEBUG(
+      "  scb[0] in_use:%s sco_idx: 0x%x sco state:%s",
+      logbool(bta_ag_cb.scb[0].in_use).c_str(), bta_ag_cb.scb[0].sco_idx,
+      sco_state_text(static_cast<tSCO_STATE>(bta_ag_cb.scb[0].state)).c_str());
+  LOG_DEBUG(
+      "  scb[1] in_use:%s sco_idx:0x%x sco state:%s",
+      logbool(bta_ag_cb.scb[1].in_use).c_str(), bta_ag_cb.scb[1].sco_idx,
+      sco_state_text(static_cast<tSCO_STATE>(bta_ag_cb.scb[1].state)).c_str());
 
   /* match callback to scb */
   if (bta_ag_cb.sco.p_curr_scb != nullptr && bta_ag_cb.sco.p_curr_scb->in_use) {
