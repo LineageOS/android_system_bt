c8ac20e6348459c6b9657683b21a620f8a4d2049
Chris Manton <cmanton@google.com>
2020-12-16 14:23:20 -0800

    Add BTM_LogHistory to BTM_RemoveSco
    Towards loggable code
    
    Bug: 163134718
    Tag: #refactor
    Test: gd/cert/run --host
    
    Change-Id: Ifabbe8cf65fc56dbe43105d9cd133a7b6ae97025

--- stack/btm/btm_sco.cc
+++ stack/btm/btm_sco.cc
@@ -772,6 +772,8 @@ tBTM_STATUS BTM_RemoveSco(uint16_t sco_inx) {
   p->state = SCO_ST_DISCONNECTING;
 
   acl_disconnect_from_handle(p->hci_handle, HCI_ERR_PEER_USER);
+  BTM_LogHistory("SCO", p->esco.data.bd_addr, "Disconnecting",
+                 "local initiated");
 
   return (BTM_CMD_STARTED);
 }
