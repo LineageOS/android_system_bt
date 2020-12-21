c30a428c51a98de7d4e169a38c0ccd152849d77e
Zach Johnson <zachoverflow@google.com>
2020-12-21 14:34:40 -0800

    rusty-gd: implement custom types & read address in controller
    Bug: 171749953
    Tag: #gd-refactor
    Test: gd/cert/run --rhost SimpleHalTest
    Change-Id: Ic8ddcac8fa2738e1282ca853a27450a56f1b54c7

--- gd/packet/parser/fields/custom_field_fixed_size.h
+++ gd/packet/parser/fields/custom_field_fixed_size.h
@@ -43,5 +43,13 @@ class CustomFieldFixedSize : public ScalarField {
 
   virtual void GenStringRepresentation(std::ostream& s, std::string accessor) const override;
 
+  void GenRustGetter(std::ostream& s, Size start_offset, Size end_offset) const override;
+
+  void GenRustWriter(std::ostream& s, Size start_offset, Size end_offset) const override;
+
+  virtual std::string GetRustDataType() const override;
+
+  virtual std::string GetRustParseDataType() const override;
+
   std::string type_name_;
 };
