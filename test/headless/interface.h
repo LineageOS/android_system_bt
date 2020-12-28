716caa982a1551f66418cdbe89dda5a38c1be9be
Chris Manton <cmanton@google.com>
2020-12-28 12:45:50 -0800

    Add headless::connect test
    Bug: 147316415
    Test: cert
    Tag: #refactor
    
    Change-Id: Ibe2a88aa832bd9a461c02158a6546d964828e295

---
+++ test/headless/interface.h
@@ -0,0 +1,35 @@
+
+
+#pragma once
+
+#include <list>
+#include <map>
+#include <string>
+
+#include "include/hardware/bluetooth.h"
+
+using acl_state_changed_params_t = struct {
+  bt_status_t status;
+  RawAddress* remote_bd_addr;
+  bt_acl_state_t state;
+};
+
+using callback_params_t = union {
+  acl_state_changed_params_t acl_state_changed;
+};
+
+using interface_data_t = struct {
+  std::string name;
+  callback_params_t params;
+};
+
+using callback_function_t = void (*)(interface_data_t);
+using interface_callback_t = struct {
+  std::string name;
+  callback_function_t function;
+};
+
+void headless_add_callback(const std::string interface_name,
+                           callback_function_t function);
+void headless_remove_callback(const std::string interface_name,
+                              callback_function_t function);
