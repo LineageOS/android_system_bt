1353c4732817a4d362e2322202295f03e153ac62
Hansong Zhang <hsz@google.com>
2021-01-11 21:49:01 -0800

    L2cap: Pass remote features to btm_sec
    Test: cert/run
    Tag: #gd-refactor
    Bug: 141555841
    Change-Id: I0b8de4ce270caf18b1b07930f04e9905adebf622

--- gd/l2cap/classic/internal/link.cc
+++ gd/l2cap/classic/internal/link.cc
@@ -433,7 +433,7 @@ void Link::OnReadRemoteVersionInformationComplete(
 }
 void Link::OnReadRemoteExtendedFeaturesComplete(uint8_t page_number, uint8_t max_page_number, uint64_t features) {
   LOG_INFO(
-      "UNIMPLEMENTED page_number:%hhu max_page_number:%hhu sub_version:0x%lx",
+      "page_number:%hhu max_page_number:%hhu sub_version:0x%lx",
       page_number,
       max_page_number,
       static_cast<unsigned long>(features));
