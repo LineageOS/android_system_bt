df935f7c8223ffaa08969acc029e7948337c9d2f
Łukasz Rymanowski <lukasz.rymanowski@codecoup.pl>
2021-03-26 17:27:33 +0000

    Revert "Remove unused API BTM_GetPeerSCA"
    Reason for revert: This is needed for LE Audio
    
    This reverts commit ab4932be3bad052d5a0f7b05cd59b21a88fe26e6.
    
    Change-Id: I0a562e1bee8d3c4f3f7170e34ea92fe657123b96

--- stack/include/btm_api.h
+++ stack/include/btm_api.h
@@ -752,6 +752,19 @@ uint16_t BTM_GetHCIConnHandle(const RawAddress& remote_bda,
 
 /*******************************************************************************
  *
+ * Function         BTM_GetPeerSCA
+ *
+ * Description      This function is called to get peer sleep clock accuracy
+ *
+ * Returns          SCA or 0xFF if SCA was never previously requested, request
+ *                  is not supported by peer device or ACL does not exist
+ *
+ ******************************************************************************/
+extern uint8_t BTM_GetPeerSCA(const RawAddress& remote_bda,
+                              tBT_TRANSPORT transport);
+
+/*******************************************************************************
+ *
  * Function         BTM_DeleteStoredLinkKey
  *
  * Description      This function is called to delete link key for the specified
