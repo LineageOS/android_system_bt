0d5215fa2c9b7ddbbfce9621cf4b2c3d844d9b2e
Josh Wu <joshwu@google.com>
2022-04-29 00:02:23 -0700

    Security: Fix out of bound read in AT_SKIP_REST
    Bug: 220732646
    Test: build
    Tag: #security
    Ignore-AOSP-First: Security bug
    Change-Id: Ia49f26e4979f9e57c448190a52d0d01b70e342c4
    (cherry picked from commit 863a0f417f6358892783860e08bf093d027764cf)
    Merged-In: Ia49f26e4979f9e57c448190a52d0d01b70e342c4

--- bta/hf_client/bta_hf_client_at.cc
+++ bta/hf_client/bta_hf_client_at.cc
@@ -797,9 +797,9 @@ void bta_hf_client_binp(tBTA_HF_CLIENT_CB* client_cb, char* number) {
   } while (0)
 
 /* skip rest of AT string up to <cr> */
-#define AT_SKIP_REST(buf)           \
-  do {                              \
-    while (*(buf) != '\r') (buf)++; \
+#define AT_SKIP_REST(buf)                             \
+  do {                                                \
+    while (*(buf) != '\r' && *(buf) != '\0') (buf)++; \
   } while (0)
 
 static char* bta_hf_client_parse_ok(tBTA_HF_CLIENT_CB* client_cb,
