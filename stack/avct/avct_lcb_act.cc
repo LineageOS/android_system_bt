db7117930064ff8a01f31d105a98f3862d255f98
Brian Delwiche <delwiche@google.com>
2022-08-25 18:52:28 +0000

    Added max buffer length check
    Bug: 230867224
    Test: Manual -- paired Bluetooth headset and played audio
    Tags: #security
    Ignore-AOSP-First: Security
    Change-Id: I740038288143715a1c06db781efd674b269a7f3e
    (cherry picked from commit 2992109ab975def57192c5e3d40078e69b1e8717)
    Merged-In: I740038288143715a1c06db781efd674b269a7f3e

--- stack/avct/avct_lcb_act.cc
+++ stack/avct/avct_lcb_act.cc
@@ -67,7 +67,12 @@ static BT_HDR* avct_lcb_msg_asmbl(tAVCT_LCB* p_lcb, BT_HDR* p_buf) {
   pkt_type = AVCT_PKT_TYPE(p);
 
   /* quick sanity check on length */
-  if (p_buf->len < avct_lcb_pkt_type_len[pkt_type]) {
+  if (p_buf->len < avct_lcb_pkt_type_len[pkt_type] ||
+      (sizeof(BT_HDR) + p_buf->offset + p_buf->len) > BT_DEFAULT_BUFFER_SIZE) {
+    if ((sizeof(BT_HDR) + p_buf->offset + p_buf->len) >
+        BT_DEFAULT_BUFFER_SIZE) {
+      android_errorWriteWithInfoLog(0x534e4554, "230867224", -1, NULL, 0);
+    }
     osi_free(p_buf);
     AVCT_TRACE_WARNING("Bad length during reassembly");
     p_ret = NULL;
