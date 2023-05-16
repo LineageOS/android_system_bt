b79f69369b0e2a60d8861258838cc617750eab07
Hui Peng <phui@google.com>
2023-05-16 21:24:07 +0000

    Fix an integer overflow bug in avdt_msg_asmbl
    This is a backport of
    Iaa4d603921fc4ffb8cfb5783f99ec0963affd6a2
    to rvc-dev
    
    Bug: 280633699
    Test: manual
    Ignore-AOSP-First: security
    Tag: #security
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:26347d4bdba646bbba4d27337d2888a04de42639)
    Merged-In: Iaa4d603921fc4ffb8cfb5783f99ec0963affd6a2
    Change-Id: Iaa4d603921fc4ffb8cfb5783f99ec0963affd6a2

--- stack/avdt/avdt_msg.cc
+++ stack/avdt/avdt_msg.cc
@@ -1289,14 +1289,14 @@ BT_HDR* avdt_msg_asmbl(AvdtpCcb* p_ccb, BT_HDR* p_buf) {
        * NOTE: The buffer is allocated above at the beginning of the
        * reassembly, and is always of size BT_DEFAULT_BUFFER_SIZE.
        */
-      uint16_t buf_len = BT_DEFAULT_BUFFER_SIZE - sizeof(BT_HDR);
+      size_t buf_len = BT_DEFAULT_BUFFER_SIZE - sizeof(BT_HDR);
 
       /* adjust offset and len of fragment for header byte */
       p_buf->offset += AVDT_LEN_TYPE_CONT;
       p_buf->len -= AVDT_LEN_TYPE_CONT;
 
       /* verify length */
-      if ((p_ccb->p_rx_msg->offset + p_buf->len) > buf_len) {
+      if (((size_t) p_ccb->p_rx_msg->offset + (size_t) p_buf->len) > buf_len) {
         /* won't fit; free everything */
         AVDT_TRACE_WARNING("%s: Fragmented message too big!", __func__);
         osi_free_and_reset((void**)&p_ccb->p_rx_msg);
