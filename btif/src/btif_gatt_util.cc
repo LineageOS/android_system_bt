c04e1233df468689ab2eb90308c46c2296143a73
Hui Peng <phui@google.com>
2023-11-29 00:53:33 +0000

    Fix an OOB bug in btif_to_bta_response and attp_build_value_cmd
    this is a backport of Iefa66f3a293ac2072ba79853a9ec23cdfe4c1368
    
    Bug: 276898739
    Test: manual
    Tag: #security
    Ignore-AOSP-First: security
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:138120c65f9b5a03d462d01da9c5c7f71c875e1e)
    Merged-In: Ia13e47e416d43243e90fb1430f65ae68c50f9ff3
    Change-Id: Ia13e47e416d43243e90fb1430f65ae68c50f9ff3

--- btif/src/btif_gatt_util.cc
+++ btif/src/btif_gatt_util.cc
@@ -18,6 +18,8 @@
 
 #define LOG_TAG "bt_btif_gatt"
 
+#include <algorithm>
+
 #include "btif_gatt_util.h"
 
 #include <errno.h>
@@ -50,9 +52,9 @@ using bluetooth::Uuid;
 void btif_to_bta_response(tGATTS_RSP* p_dest, btgatt_response_t* p_src) {
   p_dest->attr_value.auth_req = p_src->attr_value.auth_req;
   p_dest->attr_value.handle = p_src->attr_value.handle;
-  p_dest->attr_value.len = p_src->attr_value.len;
+  p_dest->attr_value.len = std::min<uint16_t>(p_src->attr_value.len, GATT_MAX_ATTR_LEN);
   p_dest->attr_value.offset = p_src->attr_value.offset;
-  memcpy(p_dest->attr_value.value, p_src->attr_value.value, GATT_MAX_ATTR_LEN);
+  memcpy(p_dest->attr_value.value, p_src->attr_value.value, p_dest->attr_value.len);
 }
 
 /*******************************************************************************
