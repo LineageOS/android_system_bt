9b56b1c5159b22197b99d0e7324ce59ce92939a4
Brian Delwiche <delwiche@google.com>
2024-04-22 21:14:56 +0000

    Fix an authentication bypass bug in SMP
    When pairing with BLE legacy pairing initiated
    from remote, authentication can be bypassed.
    This change fixes it.
    
    Bug: 251514170
    Test: m com.android.btservices
    Test: manual run against PoC
    Ignore-AOSP-First: security
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:8a3dbadc71428a30b172a74343be08498c656747)
    Merged-In: I66b1f9a80060f48a604001829db8ea7c96c7b7f8
    Change-Id: I66b1f9a80060f48a604001829db8ea7c96c7b7f8

--- stack/smp/smp_act.cc
+++ stack/smp/smp_act.cc
@@ -275,6 +275,7 @@ void smp_send_pair_rsp(tSMP_CB* p_cb, tSMP_INT_DATA* p_data) {
 void smp_send_confirm(tSMP_CB* p_cb, tSMP_INT_DATA* p_data) {
   SMP_TRACE_DEBUG("%s", __func__);
   smp_send_cmd(SMP_OPCODE_CONFIRM, p_cb);
+  p_cb->flags |= SMP_PAIR_FLAGS_CMD_CONFIRM_SENT;
 }
 
 /*******************************************************************************
@@ -608,6 +609,16 @@ void smp_proc_init(tSMP_CB* p_cb, tSMP_INT_DATA* p_data) {
   SMP_TRACE_DEBUG("%s", __func__);
 
   if (smp_command_has_invalid_parameters(p_cb)) {
+    smp_sm_event(p_cb, SMP_AUTH_CMPL_EVT, &reason);
+    return;
+  }
+
+  if (!((p_cb->loc_auth_req & SMP_SC_SUPPORT_BIT) &&
+    (p_cb->peer_auth_req & SMP_SC_SUPPORT_BIT)) &&
+    !(p_cb->flags & SMP_PAIR_FLAGS_CMD_CONFIRM_SENT))
+  {
+    // in legacy pairing, the peer should send its rand after
+    // we send our confirm
     smp_sm_event(p_cb, SMP_AUTH_CMPL_EVT, &reason);
     return;
   }
