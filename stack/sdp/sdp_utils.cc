e1325abda7d7407615ef6bfdf6ea336386331d50
Brian Delwiche <delwiche@google.com>
2024-04-22 16:43:29 +0000

    Fix heap-buffer overflow in sdp_utils.cc
    Fuzzer identifies a case where sdpu_compare_uuid_with_attr crashes with
    an out of bounds comparison.  Although the bug claims this is due to a
    comparison of a uuid with a smaller data field thana the discovery
    attribute, my research suggests that this instead stems from a
    comparison of a 128 bit UUID with a discovery attribute of some other,
    invalid size.
    
    Add checks for discovery attribute size.
    
    Bug: 287184435
    Test: atest bluetooth_test_gd_unit, net_test_stack_sdp
    Tag: #security
    Ignore-AOSP-First: Security
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:99210e2f251e2189c1eede15942c832e017404c2)
    Merged-In: Ib536cbeac454efbf6af3d713c05c8e3e077e069b
    Change-Id: Ib536cbeac454efbf6af3d713c05c8e3e077e069b

--- stack/sdp/sdp_utils.cc
+++ stack/sdp/sdp_utils.cc
@@ -729,12 +729,19 @@ bool sdpu_compare_uuid_with_attr(tBT_UUID* p_btuuid, tSDP_DISC_ATTR* p_attr) {
   uint16_t attr_len = SDP_DISC_ATTR_LEN(p_attr->attr_len_type);
 
   /* Since both UUIDs are compressed, lengths must match  */
-  if (p_btuuid->len != attr_len) return (false);
+  if (p_btuuid->len != attr_len) {
+    SDP_TRACE_ERROR("invalid length for discovery attribute");
+    return (false);
+  }
 
   if (p_btuuid->len == 2)
     return (bool)(p_btuuid->uu.uuid16 == p_attr->attr_value.v.u16);
   else if (p_btuuid->len == 4)
     return (bool)(p_btuuid->uu.uuid32 == p_attr->attr_value.v.u32);
+  else if (attr_len != MAX_UUID_SIZE) {
+    SDP_TRACE_ERROR("invalid length for discovery attribute");
+    return (false);
+  }
   else if (!memcmp(p_btuuid->uu.uuid128, (void*)p_attr->attr_value.v.array,
                    MAX_UUID_SIZE))
     return (true);
