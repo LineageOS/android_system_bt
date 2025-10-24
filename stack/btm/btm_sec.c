8b428a553a7715fb593d15143aa1716eda5ae764
Brian Delwiche <delwiche@google.com>
2025-10-23 17:25:48 -0700

    Upgrade security If it is on temp bonding & authentication is requirement
    This is a backport of ag/31042219.
    
    Bug: 380091558
    Test: mmm packages/modules/Bluetooth
    Ignore-AOSP-First: security
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:62eccbf042166aee4ec45611f2344737a3fbd012
    Merged-In: I4169359f8a1691cc35922b86bf37b4e6b795312a
    Change-Id: I4169359f8a1691cc35922b86bf37b4e6b795312a

--- stack/btm/btm_sec.c
+++ stack/btm/btm_sec.c
@@ -2049,6 +2049,14 @@ static BOOLEAN btm_sec_is_upgrade_possible(tBTM_SEC_DEV_REC  *p_dev_rec, BOOLEAN
              * it probably would not want to upgrade the link key based on the security level database */
             is_possible = TRUE;
         }
+
+        /*if authentication is requirement & currently on temp bonding
+         * trigger pairing */
+        if ((p_dev_rec->security_required &
+            (is_originator ? BTM_SEC_OUT_AUTHENTICATE : BTM_SEC_IN_AUTHENTICATE)) &&
+             p_dev_rec->bond_type == BOND_TYPE_TEMPORARY) {
+            is_possible = TRUE;
+        }
     }
     BTM_TRACE_DEBUG ("%s() is_possible: %d sec_flags: 0x%x", __func__, is_possible, p_dev_rec->sec_flags);
     return is_possible;
