c1b8c82ff34f58edfa7b7920251215f9ad0797cc
Brian Delwiche <delwiche@google.com>
2026-03-17 10:36:01 -0700

    [BACKPORT] Handle LE Secure Connections Passkey Display/Notification
    This change improves the handling of LE Secure Connections when using
    Passkey Display or Key Notification models. It introduces state tracking
    for user approval and remote confirmation of the passkey. The SMP layer
    now waits for both user approval via SMP_SecurityGrant and remote
    confirmation before proceeding with key generation. Additionally, LE
    pairing is now cancelled if authentication times out. Default LE pairing
    flags are also initialized in btif_dm.cc.
    
    This is a combined backport of the following CLs on main: ag/35861798
    ag/35914708 ag/35861802
    
    Test: mmm packages/modules/Bluetooth
    Flag: com.android.bluetooth.flags.passkey_entry_pairing_approval
    Bug: 446268763
    Bug: 446114623
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:f075dbd5fc47004eb57eca7d6314cf549c496198
    Merged-In: I7f8d0ffea6e6017b338d316cc5cfa31363305f6b
    Change-Id: I7f8d0ffea6e6017b338d316cc5cfa31363305f6b

--- stack/btm/btm_sec.c
+++ stack/btm/btm_sec.c
@@ -5163,7 +5163,7 @@ static void btm_sec_pairing_timeout(UNUSED_ATTR void *data)
 */
     p_dev_rec = btm_find_dev (p_cb->pairing_bda);
 
-    BTM_TRACE_EVENT ("%s  State: %s   Flags: %u", __func__,
+    BTM_TRACE_WARNING ("%s  State: %s   Flags: %u", __func__,
                       btm_pair_state_descr(p_cb->pairing_state), p_cb->pairing_flags);
 
     switch (p_cb->pairing_state)
@@ -5239,6 +5239,10 @@ static void btm_sec_pairing_timeout(UNUSED_ATTR void *data)
             break;
 
         case BTM_PAIR_STATE_WAIT_AUTH_COMPLETE:
+            if (btm_cb.pairing_flags & BTM_PAIR_FLAGS_LE_ACTIVE) {
+                SMP_PairCancel(p_cb->pairing_bda);
+            }
+            /* Fallthrough intended */
         case BTM_PAIR_STATE_GET_REM_NAME:
             /* We need to notify the UI that timeout has happened while waiting for authentication*/
             btm_sec_change_pairing_state (BTM_PAIR_STATE_IDLE);
